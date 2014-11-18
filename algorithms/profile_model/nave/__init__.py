590974cfd9675127e2c527c921795c51d9eda264
James Parkhurst <james.parkhurst@diamond.ac.uk>
2014-11-18 16:27:58 +0000

    First commit of nave profile model

---
+++ algorithms/profile_model/nave/__init__.py
@@ -0,0 +1,6 @@
+from __future__ import division
+from dials.array_family import flex # import dependency
+
+from model import phil_scope        # implicit dependency
+from model import ProfileModel      # implicit dependency
+from factory import Factory
---
+++ algorithms/profile_model/nave/model.py
@@ -0,0 +1,70 @@
+#
+# model.py
+#
+#  Copyright (C) 2013 Diamond Light Source
+#
+#  Author: James Parkhurst
+#
+#  This code is distributed under the BSD license, a copy of which is
+#  included in the root directory of this package.
+
+from __future__ import division
+from libtbx.phil import parse
+from dials.algorithms.profile_model.interface import ProfileModelIface
+
+phil_scope = parse('''
+
+''')
+
+class ProfileModel(ProfileModelIface):
+  ''' A class to encapsulate the profile model. '''
+
+  def __init__(self, s, da, w):
+    ''' Initialise with the parameters. '''
+    from math import pi
+    self._s = s
+    self._da = da
+    self._w = w
+    assert(self._s > 0)
+    assert(self._da >= 0)
+    assert(self._w >= 0)
+
+  def s(self):
+    ''' Return the spread in mosaic block size. '''
+    return self._s
+
+  def da(self):
+    ''' Return the spread of unitcell dimensions. '''
+    return self._da
+
+  def w(self):
+    ''' Return the angular spread of mosaic blocks. '''
+    return self._w
+
+  def predict_reflections(self, experiment, dmin=None, dmax=None, margin=1,
+                          force_static=False, **kwargs):
+    ''' Predict the reflections. '''
+    from dials.array_family import flex
+    return flex.reflection_table.from_predictions(
+      experiment,
+      dmin=dmin,
+      dmax=dmax,
+      margin=margin,
+      force_static=force_static)
+
+  def compute_bbox(self, experiment, reflections, **kwargs):
+    ''' Compute the bounding box. '''
+    raise RuntimeError("Not implemented")
+
+  def compute_partiality(self, experiment, reflections, **kwargs):
+    ''' Compute the partiality. '''
+    raise RuntimeError("Not implemented")
+
+  def compute_mask(self, experiment, reflections, **kwargs):
+    ''' Compute the shoebox mask. '''
+    raise RuntimeError("Not implemented")
+
+  def dump(self):
+    ''' Dump the profile model to phil parameters. '''
+    raise RuntimeError("Not implemented")
+

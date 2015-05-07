296229f7eb068b7f5f85c7664286dd904a4d1162
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2015-05-07 08:51:07 +0100

    loader test

--- scratch/mg/qa/tst_loader.py
+++ scratch/mg/qa/tst_loader.py
@@ -1,16 +1,26 @@
 import mock
-import unittest
+import os
 import tests
+import unittest
 
-class LoaderTests(unittest.TestCase):
+class LoaderTests_for_tests__init__py(unittest.TestCase):
+
+  def test_loader_file_enumeration(self):
+    test_directory = os.path.dirname(os.path.abspath(tests.__file__))
+    expected = set()
+    for f in os.listdir(test_directory):
+      if f.endswith('.py') and not f.startswith('__'):
+        expected.add(os.path.join(test_directory, f))
+
+    actual = set(tests.list_all_modules().itervalues())
+
+    self.assertEqual(expected, actual)
+
+class LoaderTests_for_loader_py(unittest.TestCase):
 
   @unittest.skip('not implemented yet')
-  @mock.patch('tests.os')
-  def test_loader_file_enumeration(self, mock_os):
-    test_list = tests.list_all_modules()
-    print test_list
-#    self.assertTrue(C(5))
-#    self.assertFalse(C(6))
+  def test_for_loader_py_still_missing(self):
+    pass
 
 if __name__ == '__main__':
   unittest.main()

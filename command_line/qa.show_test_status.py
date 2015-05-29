ef6d9fd4e6e661cd155ce095043e0946ca16c401
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2015-05-29 21:40:33 +0100

    correct times, fail if db not present

--- command_line/qa.show_test_status.py
+++ command_line/qa.show_test_status.py
@@ -1,16 +1,23 @@
 #!/bin/env python
 
+from datetime import datetime
 import dlstbx.qa.database as db
 from dlstbx.qa.term import color
 import dlstbx.qa.units as units
+import os.path
 import sys
 
 def run(params):
   if len(params) < 1:
     print "usage: python dlstbx.qa.show_test_status.py database.db"
     return
-  sql = db.DB(params[0])
+  dbfile = params[0]
+  if not os.path.exists(dbfile):
+    print "Database file %s not found" % dbfile
+    return
+  sql = db.DB(dbfile)
 
+  epoch = (datetime.now() - datetime(1970, 1, 1)).total_seconds()
   lastdataset = None
   for t in sql.get_tests(order_by_name=True):
     if lastdataset != t['dataset']:
@@ -28,7 +35,7 @@ def run(params):
       print " [FAIL]",
     print "%-30s" % t['test'],
     color()
-    print " (%s ago)" % units.readable_time(t['lastseen'])
+    print " (%s ago)" % units.readable_time(epoch - t['lastseen'])
 
 if __name__ == '__main__':
   run(sys.argv[1:])

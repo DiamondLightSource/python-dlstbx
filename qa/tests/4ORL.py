e9f4880e29090e166da5057739b1d094d3b52b2b
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2015-09-08 11:55:54 +0100

    add test parameters for 4ORL

--- qa/tests/4ORL.py
+++ qa/tests/4ORL.py
@@ -6,12 +6,12 @@ def check_that_data_is_complete():
 
 @Test(timeout=minutes(20))
 def run_with_dials():
-  xia2('-dials')
+  xia2('-dials', '-atom', 'Se')
 
   runtime(minutes(8))
   spacegroup("C 2 2 2")
   unitcell(between(45.63, 49.63), between(101.23, 105.23), between(41.23, 45.23))
-  resolution(1.4, 27)
+  resolution(1.35, 30)
 
 @Test(timeout=minutes(20))
 def run_with_3dii():

82676750bb590ecf940cc9efbf71e20bda8ec203
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2015-11-18 08:53:59 +0000

    Add insulin test data from Australian Synchrotron beamline MX2 via Jun Aishima

---
+++ qa/tests/insulin_reversephi.py
@@ -0,0 +1,29 @@
+from qa import *
+
+@Data
+def data_are_complete():
+  images(180)
+
+@Test(timeout=minutes(60))
+def run_with_dials():
+  xia2('-dials')
+
+  runtime(minutes(10))
+#  spacegroup("P 21 21 21")
+#  unitcell(between(4,7), between(5,8), between(10,20))
+#  resolution(0.7, 10)
+#  completeness(90)
+#  multiplicity(12)
+#  uniquereflections(3000)
+
+@Test(timeout=minutes(60))
+def run_with_3dii():
+  xia2('-3dii')
+
+  runtime(minutes(10))
+#  spacegroup("P 21 21 21")
+#  unitcell(between(4,7), between(5,8), between(10,20))
+#  resolution(0.7, 10)
+#  completeness(90)
+#  multiplicity(12)
+#  uniquereflections(3000)

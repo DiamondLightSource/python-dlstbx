ed3482d76147493890940718ba7761928ca8995e
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2016-06-10 12:58:11 +0100

    mock transport layer for frontend tests

--- workflow/test_frontend.py
+++ workflow/test_frontend.py
@@ -1,11 +1,13 @@
-import frontend
-import pytest
 import dlstbx.workflow.services.sample_service
+import frontend
+import mock
 
-def test_instantiate_new_frontend():
+@mock.patch('dlstbx.workflow.frontend.dlstbx.workflow.transport')
+def test_instantiate_new_frontend(mock_transport):
   '''Create a new frontend.'''
   frontend.Frontend()
 
-def test_start_service_in_frontend():
+@mock.patch('dlstbx.workflow.frontend.dlstbx.workflow.transport')
+def test_start_service_in_frontend(mock_transport):
   fe = frontend.Frontend()
   fe.switch_service(dlstbx.workflow.services.sample_service.Waiter)

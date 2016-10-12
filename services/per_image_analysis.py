65e3b50cdc5099f2ae247ccdfa24f147294bf7cf
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2016-10-12 22:09:25 +0100

    Do per-image-analysis within a transaction. Messages are no longer lost on exiting.

--- services/per_image_analysis.py
+++ services/per_image_analysis.py
@@ -3,6 +3,9 @@ import os
 import time
 from workflows.services.common_service import CommonService
 
+from dials.command_line.find_spots_client import response_to_xml
+from dials.command_line.find_spots_server import work
+
 class DLSPerImageAnalysis(CommonService):
   '''A service that analyses individual images.'''
 
@@ -10,28 +13,37 @@ class DLSPerImageAnalysis(CommonService):
   _service_name = "DLS Per-Image-Analysis"
 
   def initializing(self):
-    '''Subscribe to a channel.'''
-    self._transport.subscribe('per_image_analysis', self.per_image_analysis)
+    '''Subscribe to the per_image_analysis queue. Received messages must be acknowledged.'''
+    # TODO: Limit the number of messages in flight
+    self._transport.subscribe('per_image_analysis', self.per_image_analysis, acknowledgement=True)
 
   def per_image_analysis(self, header, message):
     '''Run PIA on one image.'''
 
-    # This is a bug in the message handling API. This function should receive the deserialized message.
+    #################
+    # This is a bug in the message handling API.
+    # The function should have receive a deserialized message.
     import json
     message = json.loads(message)
+    #################
 
+    # Conditionally acknowledge receipt of the message
+    txn = self._transport.transaction_begin()
+    self._transport.ack(header['message-id'], transaction=txn)
+
+    # Extract the filename
     filename = message['file']
     print "Running PIA on", filename
 
-    from dials.command_line.find_spots_client import response_to_xml
-    from dials.command_line.find_spots_server import work
-
+    # Do the per-image-analysis
     cl = ['d_max=40']
     results = work(filename, cl=cl)
     results['image'] = filename
     xml_response = response_to_xml(results)
     print results
-    print xml_response()
+    print xml_response
 
-    self._transport.send('transient.destination', results)
+    # Send results onwards
+    self._transport.send('transient.destination', results, transaction=txn)
+    self._transport.transaction_commit(txn)
     print "PIA completed on", filename

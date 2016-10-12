b3f9196cefad6f1970fa94f318f89caf2e1c9ad0
rjgildea <richard.gildea@diamond.ac.uk>
2016-10-12 17:20:47 +0100

    PIA-magic

--- services/per_image_analysis.py
+++ services/per_image_analysis.py
@@ -23,20 +23,15 @@ class DLSPerImageAnalysis(CommonService):
     filename = message['file']
     print "Running PIA on", filename
 
-    # Begin PIA-magic ============
-    import time
-    time.sleep(5)
-    spot_count = 20
-    resolution = 'magenta'
-    strong_spots = 3
-    # End PIA-magic ==============
+    from dials.command_line.find_spots_client import response_to_xml
+    from dials.command_line.find_spots_server import work
 
-    results = { 'file': filename,
-                'spots': spot_count,
-                'strong_spots': strong_spots,
-                'resolution': resolution,
-                'These are': 'per-image-analysis results', # not required
-              }
+    cl = ['d_max=40']
+    results = work(filename, cl=cl)
+    results['image'] = filename
+    xml_response = response_to_xml(results)
+    print results
+    print xml_response()
 
     self._transport.send('transient.destination', results)
     print "PIA completed on", filename

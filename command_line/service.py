067cd580e982292975432c0abe102da076ae869d
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2016-10-12 08:41:45 +0100

    Directory for dlstbx services

--- command_line/service.py
+++ command_line/service.py
@@ -5,10 +5,14 @@
 
 from __future__ import division
 from dlstbx.util.version import dlstbx_version
+import os.path
 import sys
+import workflows
 import workflows.contrib.start_service
 
 if __name__ == '__main__':
+  dlstbx = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+  workflows.load_plugins([os.path.join(dlstbx, 'services')])
   workflows.contrib.start_service.run(sys.argv[1:],
                                       program_name='dlstbx.service',
                                       version=dlstbx_version())
---
+++ services/sample_dummy.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import, division
+from workflows.services.common_service import CommonService
+
+class DLSDummy(CommonService):
+  '''A dlstbx dummy service that does nothing.'''
+
+  # Human readable service name
+  _service_name = "DLS Dummy service"

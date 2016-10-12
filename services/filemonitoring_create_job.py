df20cc06fe3eb16b2a3c4f29c52f079a58e95c95
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2016-10-12 17:02:28 +0100

    DLSFileMonitoringJob to create a new file monitoring job every 5 minutes

--- services/filemonitoring_create_job.py
+++ services/filemonitoring_create_job.py
@@ -1,4 +1,5 @@
 from __future__ import absolute_import, division
+import time
 from workflows.services.common_service import CommonService
 
 class DLSFileMonitoringJob(CommonService):
@@ -8,8 +9,13 @@ class DLSFileMonitoringJob(CommonService):
   _service_name = "DLS File monitoring job generator"
 
   def initializing(self):
-    '''Subscribe to a channel.'''
+    '''Create a new job every 5 minutes.'''
+    self.generate_job()
+    self._register_idle(5 * 60, self.generate_job)
+
+  def generate_job(self):
+    '''Generate a new monitoring job.'''
     list_of_files = \
       [ "/dls/mx-scratch/dials/example_data/wide_rotation/X4_wide_M1S4_1_%04d.cbf" % x for x in range(1, 91) ]
     self._transport.send('transient.file_monitor', { 'files': list_of_files })
-    print "Job created."
+    print "Job created at", time.ctime()

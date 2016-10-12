9fc0bd8087e7606aa0305e8d26ff62772644f142
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2016-10-12 08:06:28 +0100

    Import instead of replicate code

--- command_line/service.py
+++ command_line/service.py
@@ -5,29 +5,11 @@
 
 from __future__ import division
 from dlstbx.util.version import dlstbx_version
-import workflows.transport
-import workflows.frontend
-from optparse import OptionParser, SUPPRESS_HELP
 import sys
-
-def run(cmdline_args):
-  parser = OptionParser(
-    usage='dlstbx.service [options]',
-    version=dlstbx_version()
-  )
-  parser.add_option("-?", action="help", help=SUPPRESS_HELP)
-  parser.add_option("-s", "--service", dest="service", metavar="SVC",
-      default=None, help="Name of the service to start, default '%default'")
-  parser.add_option("-t", "--transport", dest="transport", metavar="TRN",
-      default="stomp", help="Transport mechanism, default '%default'")
-  workflows.transport.add_command_line_options(parser)
-  (options, args) = parser.parse_args(cmdline_args)
-
-  workflows.frontend.Frontend(
-      service=options.service,
-      transport=options.transport,
-    ).run()
+import workflows.contrib.start_service
 
 if __name__ == '__main__':
-  run(sys.argv[1:])
+  workflows.contrib.start_service.run(sys.argv[1:],
+                                      program_name='dlstbx.service',
+                                      version=dlstbx_version())
 

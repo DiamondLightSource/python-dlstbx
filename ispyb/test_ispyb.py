c7904460692c48772d931e295820be00b95638a2
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2016-11-02 15:25:21 +0000

    Move test to test file

--- ispyb/test_ispyb.py
+++ ispyb/test_ispyb.py
@@ -1,6 +1,14 @@
 from __future__ import absolute_import, division
 
-import dlstbx.ispyb.ispyb
+from dlstbx.ispyb.ispyb import ispyb
 
-def test_ispyb_api():
-  dlstbx.ispyb.ispyb.test()
+def test_fetch_datacollect_group_from_ispyb():
+  i = ispyb()
+  dc_id = 1397955
+  res = i.get_dc_info(dc_id)
+  # this was not recorded as a data collection group
+  whole_group = i.get_dc_group(dc_id)
+  assert len(whole_group) == 1
+  # however there are four data collections
+  whole_group = i.get_matching_folder(dc_id)
+  assert len(whole_group) == 4

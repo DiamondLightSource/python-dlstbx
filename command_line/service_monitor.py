a82c683c56ea9d8480caed32aa951cc05faa6156
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2016-11-03 13:56:06 +0000

    Add text mode for monitor

--- command_line/service_monitor.py
+++ command_line/service_monitor.py
@@ -21,14 +21,19 @@ def run(cmdline_args):
     version=dlstbx_version()
   )
   parser.add_option("-?", action="help", help=SUPPRESS_HELP)
+  parser.add_option("-n", action="store_true", dest="nofancy",
+      default=False, help="Do not draw fancy borders")
   parser.add_option("-t", "--transport", dest="transport", metavar="TRN",
       default="stomp", help="Transport mechanism, default '%default'")
   workflows.transport.add_command_line_options(parser)
   (options, args) = parser.parse_args(cmdline_args)
 
-  workflows.contrib.status_monitor.Monitor(
+  monitor = workflows.contrib.status_monitor.Monitor(
       transport=options.transport,
-    ).run()
+    )
+  if options.nofancy:
+    monitor.border_chars = monitor.border_chars_text
+  monitor.run()
 
 if __name__ == '__main__':
   run(sys.argv[1:])

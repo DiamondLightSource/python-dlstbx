f0c06cb29d0dcc2646fc6fcc3654a18eb347d420
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2016-11-04 15:45:35 +0000

    dlstbx.go to trigger xia2 processing after end of data collection

---
+++ command_line/go.py
@@ -0,0 +1,25 @@
+#
+# dlstbx.go
+#   Process a datacollection
+#
+
+from __future__ import division
+import workflows
+import workflows.contrib.start_service
+
+if __name__ == '__main__':
+  from workflows.transport.stomp_transport import StompTransport
+  StompTransport.defaults['--stomp-host'] = 'ws154.diamond.ac.uk'
+  StompTransport.defaults['--stomp-prfx'] = 'pd'
+
+  stomp = StompTransport()
+  stomp.connect()
+
+  dcid = 1397955
+  stomp.send(
+    'processing_recipe',
+    {
+      'recipes': [ 'example-xia2' ],
+      'parameters': { 'ispyb_dcid': dcid }
+    }
+  )

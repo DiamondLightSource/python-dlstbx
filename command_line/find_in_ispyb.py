4b230bc0dafcdcf5fbe23f43d760b41d2bb48c3b
graeme-winter <graeme.winter@gmail.com>
2016-11-16 15:31:23 +0000

    More useful to have it print a fully populated param document

--- command_line/find_in_ispyb.py
+++ command_line/find_in_ispyb.py
@@ -1,12 +1,19 @@
 def main(args):
-  from dlstbx.ispyb.ispyb import ispyb
+  from dlstbx.ispyb.ispyb import ispyb, ispyb_filter
+  import json
   i = ispyb()
 
   for arg in args:
     dc_id = int(arg)
     dc_info = i.get_dc_info(dc_id)
     start, end = i.dc_info_to_start_end(dc_info)
-    print '%s:%d:%d' % (i.dc_info_to_filename(dc_info), start, end)
+
+    message = { }
+    parameters = {'ispyb_dcid': dc_id}
+
+    message, parameters = ispyb_filter(message, parameters)
+
+    print json.dumps(parameters)
 
 if __name__ == '__main__':
   import sys

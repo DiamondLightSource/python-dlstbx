31b55f3ffca0970a091c28be8b6fdded07f5c180
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2016-12-02 17:38:27 +0000

    Basic tool to monitor queue activity

---
+++ command_line/queue_monitor.py
@@ -0,0 +1,316 @@
+from __future__ import absolute_import, division
+
+#
+# dlstbx.queue_monitor
+#   Monitor queue utilization
+#
+
+import copy
+import curses
+import locale
+import math
+from optparse import OptionParser, SUPPRESS_HELP
+import re
+import threading
+import time
+import uuid
+import workflows
+import workflows.transport
+
+locale.setlocale(locale.LC_ALL, '') 
+
+class QueueStatus():
+  '''Monitor ActiveMQ queue activity.'''
+
+  # Dictionary of all known queues
+  last_gather = 0
+  gather_interval = 5
+  status = {}
+  lock = threading.Lock()
+
+  # Unique ID for this queue status monitor
+  uuid = str(uuid.uuid4())
+  report_queue = 'transient.report.' + uuid
+
+  def __init__(self, transport=None):
+    '''Set up monitor and connect to the network transport layer'''
+    if transport is None or isinstance(transport, basestring):
+      self._transport = workflows.transport.lookup(transport)()
+    else:
+      self._transport = transport()
+    assert self._transport.connect(), "Could not connect to transport layer"
+    self._transport.subscribe(self.report_queue, self.process_report, transformation=True)
+#   self._transport.subscribe_broadcast('ActiveMQ.Advisory.>', self.activemq_advisory, ignore_namespace=True)
+
+  def run(self):
+    '''A wrapper for the real _run() function to cleanly enable/disable the
+       curses environment.'''
+    curses.wrapper(self._run)
+
+  def gather(self):
+    self.last_gather = time.time()
+#   self._transport.send('ActiveMQ.Statistics.Broker', '', headers = { 'JMSReplyTo': 'zocdev.' + self.report_queue }, ignore_namespace=True )
+    self._transport.broadcast('ActiveMQ.Statistics.Destination.zocdev.transient.status', '', headers = { 'JMSReplyTo': 'zocdev.' + self.report_queue }, ignore_namespace=True )
+    self._transport.send('ActiveMQ.Statistics.Destination.>', '', headers = { 'JMSReplyTo': 'zocdev.' + self.report_queue }, ignore_namespace=True )
+
+  @staticmethod
+  def formatnumber(stdscr, number):
+    stdscr.addstr(str(number))
+
+  def _run(self, stdscr):
+    '''Main display function'''
+    curses.use_default_colors()
+    curses.curs_set(False)
+    curses.init_pair(1, curses.COLOR_RED, -1)
+    curses.init_pair(2, curses.COLOR_BLACK, -1)
+    curses.init_pair(3, curses.COLOR_GREEN, -1)
+    curses.init_pair(4, curses.COLOR_YELLOW, -1)
+    curses.init_pair(5, curses.COLOR_BLUE, -1)
+    curs_y, curs_x = stdscr.getmaxyx()
+
+    def arrow(change):
+      if change >= 3:
+        stdscr.addstr(u'\u2b08'.encode('utf-8'), curses.color_pair(3) + curses.A_BOLD)
+      elif change >= 2:
+        stdscr.addstr(u'\u2197'.encode('utf-8'), curses.color_pair(3) + curses.A_BOLD)
+      elif change >= 1:
+        stdscr.addstr(u'\u2197'.encode('utf-8'), curses.color_pair(3))
+      elif change <= -3:
+        stdscr.addstr(u'\u2b0a'.encode('utf-8'), curses.color_pair(1) + curses.A_BOLD)
+      elif change <= -2:
+        stdscr.addstr(u'\u2198'.encode('utf-8'), curses.color_pair(1) + curses.A_BOLD)
+      elif change <= -1:
+        stdscr.addstr(u'\u2198'.encode('utf-8'), curses.color_pair(1))
+
+    try:
+      while True:
+        if self.last_gather + self.gather_interval < time.time():
+          self.gather()
+
+        # Check if screen was re-sized (True or False)
+        resize = curses.is_term_resized(curs_y, curs_x)
+
+        # Action in loop if resize is True:
+        stdscr.clear()
+        stdscr.scrollok(True)
+        if resize is True:
+          curs_y, curs_x = stdscr.getmaxyx()
+          curses.resizeterm(curs_y, curs_x)
+        rows = curses.LINES
+        with self.lock:
+          status_list = self.status.values()
+        status_list.sort(key=lambda s:s['relevance'], reverse=True)
+
+        stdscr.addstr(0, 0, 'DLS Zocalo queue monitor', curses.color_pair(4))
+        if curses.COLS > 55:
+          stdscr.addstr(0, 30, 'queues:', curses.color_pair(4))
+          stdscr.addstr(0, 38, str(len(status_list)), curses.color_pair(3))
+        if curses.COLS > 67:
+          stdscr.addstr(0, 47, 'messages:', curses.color_pair(4))
+          stdscr.addstr(0, 57, str(sum(s['size'] for s in status_list)), curses.color_pair(3))
+#       stdscr.addstr(0, 0, 'DLS', curses.A_BOLD + curses.color_pair(4))
+
+        reserved_rows = 1
+        status_list = copy.deepcopy(status_list[:(rows - reserved_rows)])
+        if not status_list:
+          stdscr.refresh()
+          time.sleep(0.2)
+          continue
+
+        for s in status_list:
+          if s['destinationName'].startswith('topic://'):
+            s['shortdestlen'] = len(s['shortdest']) + 1
+            s['shortdest'] = s['shortdest'].encode('utf-8') + u'\u29d3'.encode('utf-8')
+
+        longestname = max(s.get('shortdestlen', len(s['shortdest'])) for s in status_list)
+        for n, s in enumerate(status_list):
+          stdscr.move(reserved_rows + n, longestname - s.get('shortdestlen', len(s['shortdest'])))
+          stdscr.addstr(s['shortdest'])
+
+        col = longestname + 1
+        longestenq = max(len(str(s.get('change-enqueueCount', 0))) for s in status_list)
+        for n, s in enumerate(status_list):
+          stdscr.move(reserved_rows + n, col + longestenq - len(str(s.get('change-enqueueCount', 0))))
+          enqueued = s.get('change-enqueueCount')
+          if enqueued > 0:
+            stdscr.addstr(str(enqueued), curses.color_pair(3))
+          elif enqueued == 0:
+            stdscr.addstr('0', curses.color_pair(2))
+          if s.get('changediffhist-enqueueCount'):
+            stdscr.move(reserved_rows + n, col + longestenq)
+            arrow(s.get('changediffhist-enqueueCount', 0))
+#          stdscr.move(reserved_rows + n, col + longestenq + 5)
+#          stdscr.addstr(str(s.get('changediffhist-enqueueCount', '')))
+
+        col += longestenq + 1
+        longestsize = max(len(str(s.get('size', 0))) for s in status_list)
+        for n, s in enumerate(status_list):
+          stdscr.move(reserved_rows + n, col)
+          if s.get('change-enqueueCount') > 0:
+            stdscr.addstr(u'\u25b6'.encode('utf-8'), curses.color_pair(3))
+          else:
+            stdscr.addstr(u'\u25b6'.encode('utf-8'), curses.color_pair(2))
+          if s.get('size') > 0:
+            stdscr.addstr(u'\u2772'.encode('utf-8'), curses.color_pair(5) + curses.A_BOLD)
+            stdscr.move(reserved_rows + n, col + 2 + longestsize - len(str(s['size'])))
+            if s.get('change-enqueueCount') > 0 and s.get('change-dequeueCount') == 0:
+              stdscr.addstr(str(s['size']), curses.color_pair(1) + curses.A_BOLD)
+            else:
+              stdscr.addstr(str(s['size']), curses.color_pair(5))
+            arrow(s.get('changehist-size', 0))
+          else:
+            stdscr.addstr(u'\u2772'.encode('utf-8'), curses.color_pair(2) + curses.A_BOLD)
+
+        col += longestsize + 3
+        longestflight = max(len(str(s.get('inflightCount', 0))) for s in status_list)
+        for n, s in enumerate(status_list):
+          stdscr.move(reserved_rows + n, col)
+          if s.get('size') > 0:
+            stdscr.addstr('|', curses.color_pair(5) + curses.A_BOLD)
+          else:
+            stdscr.addstr('|', curses.color_pair(2) + curses.A_BOLD)
+          if s.get('inflightCount', 0) > 0:
+            stdscr.move(reserved_rows + n, col + 1 + longestflight - len(str(s['inflightCount'])))
+            stdscr.addstr(str(s['inflightCount']), curses.color_pair(5))
+            arrow(s.get('changehist-inflightCount'))
+          stdscr.move(reserved_rows + n, col + longestflight + 2)
+          if s.get('size') > 0:
+            stdscr.addstr(u'\u2773'.encode('utf-8'), curses.color_pair(5) + curses.A_BOLD)
+          else:
+            stdscr.addstr(u'\u2773'.encode('utf-8'), curses.color_pair(2) + curses.A_BOLD)
+
+        col += longestflight + 3
+        for n, s in enumerate(status_list):
+          stdscr.move(reserved_rows + n, col)
+          dequeued = s.get('change-dequeueCount')
+          if not dequeued:
+            stdscr.addstr(u'\u25b6'.encode('utf-8'), curses.color_pair(2))
+          elif dequeued > 0:
+            stdscr.addstr(u'\u25b6'.encode('utf-8'), curses.color_pair(3))
+            stdscr.addstr(str(dequeued), curses.color_pair(3))
+            arrow(s.get('changediffhist-dequeueCount', 0))
+          elif dequeued == 0:
+            stdscr.addstr(u'\u25b60'.encode('utf-8'), curses.color_pair(2))
+
+          stdscr.addstr(reserved_rows + n, curses.COLS - 7, "%6.1f" % s['relevance'])
+        stdscr.refresh()
+        time.sleep(0.4)
+    except KeyboardInterrupt:
+      return
+
+  def process_report(self, header, message):
+    report = {}
+    for entry in message['map']['entry']:
+      if 'string' in entry:
+        if isinstance(entry['string'], list):
+          name = entry['string'].pop(0)
+        else:
+          name = entry['string']
+          del(entry['string'])
+      if len(entry) == 1:
+        value_type = entry.iterkeys().next()
+        report[name] = entry.itervalues().next()
+        if value_type in ('long', 'int'):
+          report[name] = int(report[name])
+        if isinstance(report[name], list) and len(report[name]) == 1:
+          report[name] = report[name][0]
+      else:
+        report[name] = entry
+    destination = report.get('destinationName')
+    if not destination: return
+    if destination.endswith(self.report_queue): return
+
+    self.last_gather = time.time()
+
+    shortdest = destination.replace('queue://zocdev.', '').replace('topic://zocdev.', '').replace('uk.ac.diamond.', 'u.a.d.').replace('transient.', 't.')
+    shortdest = re.sub('([0-9a-f]{8})-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}', '\\1-(..)', shortdest)
+    report['shortdest'] = shortdest
+    report['last-seen'] = time.time()
+
+    with self.lock:
+      last_status = self.status.get(destination, {})
+      for key in ('size', 'enqueueCount', 'dequeueCount', 'consumerCount', 'producerCount', 'inflightCount'):
+        if key in last_status:
+          report['change-' + key] = report.get(key, 0) - last_status[key]
+
+        # Find change trend (2nd derivative)
+        report['changehist-' + key] = last_status.get('changehist-' + key, 0)
+        if 'change-' + key in report:
+          change = report['change-' + key]
+          # This is the absolute change in the value since the last update
+          if change < 0:
+            report['changehist-' + key] = max(report['changehist-' + key] - 1, -3)
+          elif change > 0:
+            report['changehist-' + key] = min(report['changehist-' + key] + 1, 3)
+          elif report['changehist-' + key] > 0.5:
+            report['changehist-' + key] -= 0.5
+          elif report['changehist-' + key] < -0.5:
+            report['changehist-' + key] += 0.5
+          else:
+            report['changehist-' + key] = 0
+        # Find change trend trend (3nd derivative)
+        report['changediffhist-' + key] = last_status.get('changediffhist-' + key, 0)
+        if 'change-' + key in last_status:
+          change = report['change-' + key] - last_status['change-' + key]
+          # This is the change in the (change in the value) since the last update
+          if change < 0:
+            report['changediffhist-' + key] = max(report['changediffhist-' + key] - 1, -3)
+          elif change > 0:
+            report['changediffhist-' + key] = min(report['changediffhist-' + key] + 1, 3)
+          elif report['changediffhist-' + key] > 0.5:
+            report['changediffhist-' + key] -= 0.5
+          elif report['changediffhist-' + key] < -0.5:
+            report['changediffhist-' + key] += 0.5
+          else:
+            report['changediffhist-' + key] = 0
+
+      # Define a sort order by traffic levels (large numbers, large changes == important)
+      relevance = last_status.get('relevance', 0) / 2
+      for key in ('size', 'enqueueCount', 'dequeueCount'):
+        for qualifier in ('', 'change-'):
+          relevance = relevance + math.log(1 + abs(report.get(qualifier + key, 0)))
+        for qualifier in ('changehist-', 'changediffhist-'):
+          relevance = relevance + (abs(report.get(qualifier + key, 0)))
+      report['relevance'] = relevance
+
+      self.status[destination] = report
+#     change = { key: report[key] - last_status[key] for key in ('size', 'enqueueCount', 'dequeueCount', 'inflightCount') }
+#      if any(change.itervalues()) or report['size'] or change['enqueueCount'] or change['dequeueCount']:
+#        print '%60s: %3d -> %3d (%+3d) -> %3d [ %3d %3d ]' % (destination, change['enqueueCount'], report['size'], change['size'], change['dequeueCount'], report['inflightCount'], change['inflightCount'])
+
+#averageEnqueueTime :  0
+#averageMessageSize :  0
+#brokerId :  [u'ID:cs04r-sc-vserv-128.diamond.ac.uk-45333-1480578321703-0:1']
+#brokerName :  [u'localhost']
+#consumerCount :  1
+#dequeueCount :  1
+#destinationName :  [u'queue://zocdev.transient.system_test.c01d6377-2e4c-4bbb-8e7e-c93bcd839568.6']
+#dispatchCount :  0
+#enqueueCount :  1
+#expiredCount :  1
+#inflightCount :  0
+#maxEnqueueTime :  0
+#memoryLimit :  668309914
+#memoryPercentUsage :  0
+#memoryUsage :  0
+#messagesCached :  0
+#minEnqueueTime :  0
+#producerCount :  0
+#size :  0
+
+if __name__ == '__main__':
+  # override default stomp host
+  from workflows.transport.stomp_transport import StompTransport
+  StompTransport.load_configuration_file(
+    '/dls_sw/apps/zocalo/secrets/credentials-testing.cfg')
+
+  parser = OptionParser(
+    usage='dlstbx.queue_monitor [options]'
+  )
+  parser.add_option("-?", action="help", help=SUPPRESS_HELP)
+  parser.add_option("-t", "--transport", dest="transport", metavar="TRN",
+      default="stomp", help="Transport mechanism, default '%default'")
+  workflows.transport.add_command_line_options(parser)
+  (options, args) = parser.parse_args()
+
+  QueueStatus(transport=options.transport).run()

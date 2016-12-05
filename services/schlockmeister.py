1899e67ba222da87a64ca5f98667c7c7e697a6ca
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2016-12-05 11:00:40 +0000

    subscribe to a marker queue so other instances can be identified

--- services/schlockmeister.py
+++ services/schlockmeister.py
@@ -1,4 +1,5 @@
 from __future__ import absolute_import, division
+import uuid
 from workflows.services.common_service import CommonService
 
 class DLSSchlockmeister(CommonService):
@@ -29,6 +30,16 @@ class DLSSchlockmeister(CommonService):
        Only receive messages that have been delivered 10 times in the past.
        Get messages without pre-processing, as this may have caused the crash.
     '''
+
+    # Generate a unique ID in order to identify myself and other instances of
+    # myself in the list of active subscribers/consumers
+    self.uuid = str(uuid.uuid4())
+
+    # Listen to a specific queue in the namespace. There will be no messages
+    # in that queue. It only acts as a marker to identify service instances.
+    self._markerqueue = 'transient.schlockmeister.' + self.uuid
+    self._transport.subscribe(self._markerqueue, self.ignore, disable_mangling=True)
+
     self._transport.subscribe('>', self.quarantine, acknowledgement=True,
         selector="JMSXDeliveryCount>10", disable_mangling=True)
     self.log.info("Schlockmeister ready for work")
@@ -43,3 +54,7 @@ class DLSSchlockmeister(CommonService):
 
     # The actual quarantining magic happens on the broker
     self._transport.nack(header)
+
+  @staticmethod
+  def ignore(header, message):
+    '''Ignore any messages on this subscription.'''

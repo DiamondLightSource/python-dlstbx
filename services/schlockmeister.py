5a712543e8223cd0e7ad46407a65bbf1302cf1fb
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2016-12-05 12:19:36 +0000

    Subscribe to relevant queues individually rather than a blanket subscription on all queues. Unsubscribe when there are no other non-Schlockmeister subscribers. With this logic the garbage collector on the broker can now again remove unused queues.

--- services/schlockmeister.py
+++ services/schlockmeister.py
@@ -1,4 +1,5 @@
 from __future__ import absolute_import, division
+import collections
 import uuid
 from workflows.services.common_service import CommonService
 
@@ -25,11 +26,17 @@ class DLSSchlockmeister(CommonService):
   # Logger name
   _logger_name = 'dlstbx.services.schlockmeister'
 
+  known_queues = {}
+  known_consumers = {}
+  known_instances = set()
+
   def initializing(self):
     '''Subscribe to all queues. Received messages must be acknowledged.
        Only receive messages that have been delivered 10 times in the past.
        Get messages without pre-processing, as this may have caused the crash.
     '''
+
+    self.log.info("Collecting broker information")
 
     # Generate a unique ID in order to identify myself and other instances of
     # myself in the list of active subscribers/consumers
@@ -40,9 +47,113 @@ class DLSSchlockmeister(CommonService):
     self._markerqueue = 'transient.schlockmeister.' + self.uuid
     self._transport.subscribe(self._markerqueue, self.ignore, disable_mangling=True)
 
-    self._transport.subscribe('>', self.quarantine, acknowledgement=True,
-        selector="JMSXDeliveryCount>10", disable_mangling=True)
-    self.log.info("Schlockmeister ready for work")
+    # Look through list of all subscriptions to identify the subscription that
+    # was just set up. From this we can infer the relevant namespace.
+    self._namespace = None
+    self._subid_watch_global = self._transport.subscribe_broadcast( \
+        'ActiveMQ.Advisory.Consumer.Queue.>', self.watch_global, ignore_namespace=True)
+
+    # Add a fallback in case the namespace could not be determined.
+    self._register_idle(90, self._namespace_determination_failure)
+
+  @staticmethod
+  def ignore(header, message):
+    '''Ignore any messages on this subscription.'''
+
+  def _namespace_determination_failure(self):
+    '''Report namespace determination failure.'''
+    self.log.error('Could not determine namespace within allocated time. Shutting down.')
+    self._shutdown()
+
+  def watch_global(self, header, message):
+    '''Received information on one existing queue subscription.
+       Try to identify the relevant transport namespace.
+    '''
+    if self._namespace or not isinstance(message, dict):
+      return
+    if self._markerqueue in message.get('ConsumerInfo', {}).get('destination', {}).get('string', ''):
+      self._namespace = message['ConsumerInfo']['destination']['string'][:-len(self._markerqueue)]
+      self.log.info("Identified namespace as '%s'", self._namespace)
+
+      # With the namespace now identified, can drop the global subscription watch and look only at relevant queues
+      self._transport.unsubscribe(self._subid_watch_global)
+
+      # Disable fallback function. Enable garbage collection instead.
+      self._register_idle(15, self.garbage_collect)
+
+      self._transport.subscribe_broadcast('ActiveMQ.Advisory.Consumer.Queue.%s>' % self._namespace, self.watch_local, ignore_namespace=True)
+
+  def watch_local(self, header, message):
+    '''Keep track of queue consumers to identify active queues and topics.'''
+    if not isinstance(message, dict):
+      return
+
+    if 'ConsumerInfo' in message:
+      destination = message['ConsumerInfo']['destination']['string']
+      if not destination.startswith(self._namespace):
+        self.log.warn("Subscription to %s detected, which is not within namespace", self._namespace)
+        return
+      destination = destination.replace(self._namespace, '')
+
+      subscriber = message['ConsumerInfo']['consumerId']['connectionId']
+
+      consumer_triple = (subscriber,
+                         message['ConsumerInfo']['consumerId']['sessionId'],
+                         message['ConsumerInfo']['consumerId']['value'])
+      if consumer_triple in self.known_consumers:
+        self.log.error("Consumer triple %s has already been seen!", str(consumer_triple))
+      self.known_consumers[consumer_triple] = destination
+
+      self.log.debug("Seen new subscriber %s to %s", subscriber, destination)
+      if destination not in self.known_queues:
+        self.known_queues[destination] = { 'subscribers': collections.Counter() }
+      self.known_queues[destination]['subscribers'].update({subscriber: 1})
+
+      if destination.startswith('transient.schlockmeister.'):
+        self.log.info("Ignoring subscriptions by client %s", subscriber)
+        self.known_instances.add(subscriber)
+    elif 'RemoveInfo' in message:
+      subscriber = message['RemoveInfo']['objectId']['connectionId']
+      consumer_triple = (subscriber,
+                         message['RemoveInfo']['objectId']['sessionId'],
+                         message['RemoveInfo']['objectId']['value'])
+      if consumer_triple not in self.known_consumers:
+        self.log.error("Consumer triple %s unknown for removal!", str(consumer_triple))
+        return
+      destination = self.known_consumers[consumer_triple]
+      del(self.known_consumers[consumer_triple])
+
+      if destination not in self.known_queues:
+        self.log.error("Queue %s unknown for removal", destination)
+      self.log.debug("Seen subscriber %s leaving %s", subscriber, destination)
+      self.known_queues[destination]['subscribers'].update({subscriber: -1})
+      if self.known_queues[destination]['subscribers'][subscriber] == 0:
+        del(self.known_queues[destination]['subscribers'][subscriber])
+    else:
+      self.log.warn('Received unknown message type\n%s', str(message))
+    self.update_subscriptions()
+
+  def update_subscriptions(self):
+    '''Match up subscriptions to list of queues with real subscribers.'''
+    for destination in self.known_queues:
+      real_subscriber_count = sum(map(lambda k: k not in self.known_instances, self.known_queues[destination]['subscribers']))
+      if real_subscriber_count and not self.known_queues[destination].get('subscription'):
+        self.log.debug("subscribing to %s", destination)
+        self.known_queues[destination]['subscription'] = \
+            self._transport.subscribe(destination, self.quarantine, acknowledgement=True,
+                                      selector="JMSXDeliveryCount>10", disable_mangling=True)
+      if not real_subscriber_count and self.known_queues[destination].get('subscription'):
+        self.log.debug("unsubscribing from %s", destination)
+        self._transport.unsubscribe(self.known_queues[destination]['subscription'])
+        del(self.known_queues[destination]['subscription'])
+
+  def garbage_collect(self):
+    '''Regularly clean up list of known queues.'''
+    entries = list(self.known_queues)
+    for entry in entries:
+      if len(self.known_queues[entry]) == 1 and not any(self.known_queues[entry]['subscribers']):
+        del(self.known_queues[entry])
+        self.log.info("collecting stale queue %s, leaving %d queues, %d consumers, %d peers", entry, len(self.known_queues), len(self.known_consumers), len(self.known_instances)-1)
 
   def quarantine(self, header, message):
     '''Quarantine this message.'''
@@ -54,7 +165,3 @@ class DLSSchlockmeister(CommonService):
 
     # The actual quarantining magic happens on the broker
     self._transport.nack(header)
-
-  @staticmethod
-  def ignore(header, message):
-    '''Ignore any messages on this subscription.'''

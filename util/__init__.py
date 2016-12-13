295949d837e71b6e075b84254f15c409cd553558
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2016-12-13 10:02:55 +0000

    function to obtain process memory information

--- util/__init__.py
+++ util/__init__.py
@@ -1,5 +1,6 @@
 from __future__ import absolute_import, division
 import errno
+import re
 import os
 import stat
 
@@ -19,3 +20,25 @@ def dls_tmp_folder():
     if exception.errno != errno.EPERM:
       raise
   return tmp_folder
+
+_proc_getnumber = re.compile(':\s+([0-9]+)\s')
+def get_process_uss(pid = None):
+  '''Get the unique set size of a process in bytes.
+     The unique set size is the amount of memory that would be freed if that
+     process was terminated.
+     Note that this will only work on linux.
+  '''
+  if not pid:
+    pid = os.getpid() # Don't cache this. Multiprocessing would copy value.
+  with open('/proc/%s/smaps' % str(pid), 'r') as fh:
+    return 1024 * sum( int(_proc_getnumber.search(x).group(1))
+                       for x in fh
+                       if x.startswith('Private') )
+try:
+  get_process_uss()
+except IOError as exception:
+  if exception.errno == 2:
+    # /proc not available on this platform
+    get_process_uss = lambda pid=None: None
+  else:
+    raise

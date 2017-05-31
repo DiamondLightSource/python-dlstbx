dd7dc1060eae70bef00179b59c925e2b0d6a94d0
graeme-winter <graeme.winter@gmail.com>
2017-05-31 11:48:52 +0100

    Recipe generation

---
+++ command_line/recipe.py
@@ -0,0 +1,26 @@
+#
+# dlstbx.recipe
+#   Propose recipes to be run on $DCID
+#
+
+from __future__ import division
+from optparse import OptionParser, SUPPRESS_HELP
+import json
+import sys
+
+# Example: dlstbx.recipe 527189
+
+if __name__ == '__main__':
+  if len(sys.argv) != 2:
+    raise RuntimeError, '%s $DCID' % sys.argv[0]
+
+  dcid = int(sys.argv[1])
+
+  from dlstbx.ispyb.ispyb import ispyb_filter
+
+  message = { }
+  parameters = {'ispyb_dcid':dcid}
+
+  message, parameters = ispyb_filter(message, parameters)
+
+  print ' '.join(message['default_recipe'])

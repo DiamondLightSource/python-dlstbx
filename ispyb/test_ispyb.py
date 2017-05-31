615599afee1028f1e9d9742b76448d513d5965ba
graeme-winter <graeme.winter@gmail.com>
2017-05-31 15:14:25 +0100

    Putative fix

--- ispyb/test_ispyb.py
+++ ispyb/test_ispyb.py
@@ -26,7 +26,7 @@ def test_ispyb_recipe_filtering_does_read_datacollection_information():
 
   message, parameters = ispyb_filter(message, parameters)
 
-  assert message == { 'dummy_msg': mock.sentinel.dummy_msg, 'default_recipe': ['per-image-analysis-rotation', 'processing-fast-dp', 'processing-xia2-3dii', 'processing-xia2-dials', 'processing-multi-xia2'] }
+  assert message == { 'dummy_msg': mock.sentinel.dummy_msg, 'default_recipe': ['per-image-analysis-rotation', 'processing-fast-dp', 'processing-xia2-3dii', 'processing-xia2-dials', 'processing-multi-xia2-dials', 'processing-multi-xia2-3dii'] }
   assert parameters['ispyb_dcid'] == ds['gphl_C2']
   assert isinstance(parameters['ispyb_dc_class'], dict)
   assert isinstance(parameters['ispyb_dc_info'], dict)

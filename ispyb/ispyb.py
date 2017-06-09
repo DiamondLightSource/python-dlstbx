aa35bf3250b160f20f444af1109e8c7920820e20
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2017-06-09 10:28:35 +0100

    clean clutter

--- ispyb/ispyb.py
+++ ispyb/ispyb.py
@@ -488,13 +488,13 @@ def ispyb_filter(message, parameters):
 
   if parameters['ispyb_images']:
     message['default_recipe'] = [
-      'per-image-analysis-rotation', 'processing-fast-dp', 
+      'per-image-analysis-rotation', 'processing-fast-dp',
       'processing-xia2-3dii', 'processing-xia2-dials',
       'processing-multi-xia2-dials', 'processing-multi-xia2-3dii',
       'processing-autoproc']
   else:
     message['default_recipe'] = [
-      'per-image-analysis-rotation', 'processing-fast-dp', 
+      'per-image-analysis-rotation', 'processing-fast-dp',
       'processing-xia2-3dii', 'processing-xia2-dials',
       'processing-autoproc']
 

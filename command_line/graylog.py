422b355c33bd6f14cbf7a7318de4a1aa0a1b3c2e
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2017-07-05 09:38:02 +0100

    refactor Graylog API out, add formatting options

--- command_line/graylog.py
+++ command_line/graylog.py
@@ -4,106 +4,16 @@
 #
 
 from __future__ import absolute_import, division
+import base64
 import ConfigParser
 from dlstbx.util.colorstreamhandler import ColorStreamHandler
-import base64
+from dlstbx.util.graylog import GraylogAPI
 import json
 from optparse import OptionParser, SUPPRESS_HELP
+import string
 import sys
 import time
 import urllib2
-
-class GraylogAPI():
-  last_seen_message = None
-  last_seen_timestamp = None
-
-  def __init__(self, configfile):
-    cfgparser = ConfigParser.ConfigParser(allow_no_value=True)
-    self.level = 6 # INFO
-    if not cfgparser.read(configfile):
-      raise RuntimeError('Could not read from configuration file %s' % filename)
-    self.url = cfgparser.get('graylog', 'url')
-    if not self.url.endswith('/'):
-      self.url += '/'
-    self.authstring = "Basic " \
-                    + base64.b64encode(cfgparser.get('graylog', 'username')
-                                                                 + ':' +
-                                       cfgparser.get('graylog', 'password'))
-    self.stream = cfgparser.get('graylog', 'stream')
-
-  def _get(self, url):
-    complete_url = self.url + url
-    req = urllib2.Request(complete_url, headers={"Accept": "application/json"})
-    req.add_header("Authorization", self.authstring)
-    handler = urllib2.urlopen(req)
-
-    returncode = handler.getcode()
-    success = returncode == 200
-    headers = { k: v for k, v in handler.headers.items() }
-    body = handler.read()
-    if success:
-      parsed = json.loads(body)
-    else:
-      parsed = None
-
-    return {
-      "success": success,
-      "returncode": returncode,
-      "headers": headers,
-      "body": body,
-      "parsed": parsed
-    }
-
-  def get_messages(self, time=600):
-    if self.last_seen_timestamp:
-      update = self.absolute_update()
-    else:
-      update = self.relative_update(time=time)
-      self.last_seen_timestamp = update['parsed']['to']
-    if not update['success']:
-      return
-    update = update['parsed']
-    messages = [ x.get('message', {}) for x in update.get('messages', []) ]
-    if self.last_seen_message:
-      message_ids = [ m['_id'] for m in messages ]
-      try:
-        seen_marker = message_ids.index(self.last_seen_message)
-        messages = messages[seen_marker + 1:] # skip previously seen message and all preceeding
-      except ValueError:
-        pass # last seen message not in selection
-    if messages:
-      self.last_seen_message = messages[-1]['_id']
-      self.last_seen_timestamp = messages[-1]['timestamp']
-    return messages
-
-  def get_all_messages(self, time=600):
-    messages = True
-    while messages:
-      messages = g.get_messages(time=options.time)
-      for message in messages:
-        yield message
-
-  def relative_update(self, time=600):
-    return self._get("search/universal/relative?"
-                     "query=level:%3C={level}&"
-                     "range={time}&"
-                     "filter=streams%3A{stream}&"
-                     "sort=timestamp%3Aasc"
-                     .format(time=time, stream=self.stream, level=self.level)
-        )
-
-  def absolute_update(self, from_time=None):
-    if not from_time:
-      from_time = self.last_seen_timestamp
-    from_time = from_time.replace(':', '%3A')
-    return self._get("search/universal/absolute?"
-                     "query=level:%3C={level}&"
-                     "from={from_time}&"
-                     "to=2031-01-01%2012%3A00%3A00&"
-                     "filter=streams%3A{stream}&"
-                     "sort=timestamp%3Aasc"
-                     .format(from_time=from_time, stream=self.stream, level=self.level)
-        )
 
 log_levels = {
     0: {'name': 'emerg', 'color': ColorStreamHandler.CRITICAL },
@@ -116,11 +26,57 @@ log_levels = {
     7: {'name': 'debug', 'color': ColorStreamHandler.DEBUG },
   }
 
-def format(message):
-  return ( log_levels[message['level']]['color']
-         + ("{timestamp} {source} {facility}\n" \
-            "  {full_message}\n".format(**message))
-         + ColorStreamHandler.DEFAULT)
+def format_message(verbosity):
+  class SafeDict(dict):
+    '''A dictionary that returns empty strings for undefined keys.'''
+    @staticmethod
+    def __missing__(key):
+      '''Return an empty string.'''
+      return ''
+
+  def format_string(fstring, message):
+    return string.Formatter().vformat(fstring, (), SafeDict(message))
+
+  def format_default(message):
+    return log_levels[message['level']]['color'] + \
+           format_string( \
+             "{timestamp} {facility}\n"
+             "                         {full_message}\n",
+             message
+           ) + ColorStreamHandler.DEFAULT
+
+  def format_source(message):
+    return log_levels[message['level']]['color'] + \
+           format_string( \
+             "{timestamp} {facility} {source} - {workflows_service}\n"
+             "                         {full_message}\n",
+             message
+           ) + ColorStreamHandler.DEFAULT
+
+  def format_verbose(message):
+    return format_string( \
+             "{timestamp} {facility} {source} - {workflows_service}\n"
+             "{file}:{line} ({function})\n"
+             + log_levels[message['level']]['color'] +
+             "{full_message}\n\n",
+             message
+           ) + ColorStreamHandler.DEFAULT
+
+  def format_xverbose(message):
+    return format_string( \
+             "{timestamp} {facility} {source} - {workflows_service}\n"
+             "workflows {workflows_workflows}  {workflows_dlstbx}  Status:{workflows_statustext}  {process_name}:{thread_name}\n"
+             "{file}:{line} ({function})\n"
+             + log_levels[message['level']]['color'] +
+             "{full_message}\n\n",
+             message
+           ) + ColorStreamHandler.DEFAULT
+
+  formats = [ format_default, format_source, format_verbose, format_xverbose ]
+  try:
+    return formats[verbosity]
+  except IndexError:
+    return formats[-1]
 
 if __name__ == '__main__':
   parser = OptionParser(usage="dlstbx.graylog [options]")
@@ -131,6 +87,8 @@ if __name__ == '__main__':
                     help="Show messages with this loglevel and higher. Valid options: alert, critical, error, warning, notice, info, debug")
   parser.add_option("--time", dest="time", default=600, type="int",
                     help="Start showing messages from this many seconds back in time.")
+  parser.add_option("-v", "--verbose", dest="verbose", default=0, action="count",
+                    help="Show more detail (can be specified multiple times)")
   (options, args) = parser.parse_args(sys.argv[1:])
 
   try:
@@ -141,6 +99,7 @@ if __name__ == '__main__':
 
   g = GraylogAPI('/dls_sw/apps/zocalo/secrets/credentials-log.cfg')
   g.level = level
+  format = format_message(options.verbose)
   if options.follow:
     try:
       while True:
---
+++ util/graylog.py
@@ -0,0 +1,103 @@
+#
+# simple API to access Graylog messages
+#
+# Point to a configuration file to use it, eg:
+#  g = GraylogAPI('/dls_sw/apps/zocalo/secrets/credentials-log.cfg')
+
+from __future__ import absolute_import, division
+import ConfigParser
+import base64
+import json
+import urllib2
+
+class GraylogAPI():
+  last_seen_message = None
+  last_seen_timestamp = None
+
+  def __init__(self, configfile):
+    cfgparser = ConfigParser.ConfigParser(allow_no_value=True)
+    self.level = 6 # INFO
+    if not cfgparser.read(configfile):
+      raise RuntimeError('Could not read from configuration file %s' % filename)
+    self.url = cfgparser.get('graylog', 'url')
+    if not self.url.endswith('/'):
+      self.url += '/'
+    self.authstring = "Basic " \
+                    + base64.b64encode(cfgparser.get('graylog', 'username')
+                                                                 + ':' +
+                                       cfgparser.get('graylog', 'password'))
+    self.stream = cfgparser.get('graylog', 'stream')
+
+  def _get(self, url):
+    complete_url = self.url + url
+    req = urllib2.Request(complete_url, headers={"Accept": "application/json"})
+    req.add_header("Authorization", self.authstring)
+    handler = urllib2.urlopen(req)
+
+    returncode = handler.getcode()
+    success = returncode == 200
+    headers = { k: v for k, v in handler.headers.items() }
+    body = handler.read()
+    if success:
+      parsed = json.loads(body)
+    else:
+      parsed = None
+
+    return {
+      "success": success,
+      "returncode": returncode,
+      "headers": headers,
+      "body": body,
+      "parsed": parsed
+    }
+
+  def get_messages(self, time=600):
+    if self.last_seen_timestamp:
+      update = self.absolute_update()
+    else:
+      update = self.relative_update(time=time)
+      self.last_seen_timestamp = update['parsed']['to']
+    if not update['success']:
+      return
+    update = update['parsed']
+    messages = [ x.get('message', {}) for x in update.get('messages', []) ]
+    if self.last_seen_message:
+      message_ids = [ m['_id'] for m in messages ]
+      try:
+        seen_marker = message_ids.index(self.last_seen_message)
+        messages = messages[seen_marker + 1:] # skip previously seen message and all preceeding
+      except ValueError:
+        pass # last seen message not in selection
+    if messages:
+      self.last_seen_message = messages[-1]['_id']
+      self.last_seen_timestamp = messages[-1]['timestamp']
+    return messages
+
+  def get_all_messages(self, time=600):
+    messages = True
+    while messages:
+      messages = self.get_messages(time=time)
+      for message in messages:
+        yield message
+
+  def relative_update(self, time=600):
+    return self._get("search/universal/relative?"
+                     "query=level:%3C={level}&"
+                     "range={time}&"
+                     "filter=streams%3A{stream}&"
+                     "sort=timestamp%3Aasc"
+                     .format(time=time, stream=self.stream, level=self.level)
+        )
+
+  def absolute_update(self, from_time=None):
+    if not from_time:
+      from_time = self.last_seen_timestamp
+    from_time = from_time.replace(':', '%3A')
+    return self._get("search/universal/absolute?"
+                     "query=level:%3C={level}&"
+                     "from={from_time}&"
+                     "to=2031-01-01%2012%3A00%3A00&"
+                     "filter=streams%3A{stream}&"
+                     "sort=timestamp%3Aasc"
+                     .format(from_time=from_time, stream=self.stream, level=self.level)
+        )

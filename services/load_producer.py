78bef4fabe5a74874de12fc560abe8e7d85e3274
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2017-07-18 13:26:34 +0100

    Add services to benchmark ActiveMQ

---
+++ services/load_producer.py
@@ -0,0 +1,28 @@
+from __future__ import absolute_import, division
+from workflows.services.common_service import CommonService
+import time
+
+class LoadProducer(CommonService):
+  '''A service creating messages as quickly as possible.'''
+
+  # Human readable service name
+  _service_name = "Load Producer"
+
+  # Logger name
+  _logger_name = "dlstbx.services.load_producer"
+
+  def initializing(self):
+    '''Generate messages.'''
+    counter = 0
+    interval = time.time() + 5
+    content = 'X' * 1024
+    while True:
+      counter += 1
+      self._transport.send("transient.destination",
+#     self._transport.send("destination",
+                           content)
+      if interval < time.time():
+        self.log.info("Produced %5d 1K messages in 5 seconds = %7.1f/s",
+                      counter, counter/5)
+        counter = 0
+        interval = time.time() + 5
---
+++ services/load_receiver.py
@@ -0,0 +1,38 @@
+from __future__ import absolute_import, division
+from workflows.services.common_service import CommonService
+import time
+
+class LoadReceiver(CommonService):
+  '''service consuming messages as fast as possible.'''
+
+  # Human readable service name
+  _service_name = "Load receiver"
+
+  # Logger name
+  _logger_name = "dlstbx.services.load_receiver"
+
+  def initializing(self):
+    '''Subscribe to channels.'''
+    self.interval = time.time() + 5
+    self.count = 0
+    self._transport.subscribe('destination', self.consume_message)
+    self._transport.subscribe('transient.destination', self.consume_message)
+    self._register_idle(1, self.print_stats)
+
+  def consume_message(self, header, message):
+    '''Consume a message'''
+    if self.interval < time.time():
+      self.log.info("Received %5d messages in 5 seconds = %7.1f/s",
+                    self.count, self.count/5)
+      self.interval = time.time() + 5
+      self.count = 1
+    else:
+      self.count = self.count + 1
+
+  def print_stats(self):
+    '''Continue to print statistics when idle'''
+    if self.interval < time.time():
+      self.log.info("Received %5d messages in 5 seconds = %7.1f/s",
+                    self.count, self.count/5)
+      self.interval = time.time() + 5
+      self.count = 0

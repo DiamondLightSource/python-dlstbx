2449fcf9806bfeaae3252f4e947b00c337e9764a
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2017-08-23 10:14:16 +0100

    graypy still hasn't released my merged fix, so patch graypy on load until the new release comes along

--- __init__.py
+++ __init__.py
@@ -23,6 +23,34 @@ def enable_graylog():
       else:            return 1 # ALERT
   graypy.handler.SYSLOG_LEVELS = PythonLevelToSyslogConverter()
 
+  # Monkeypatch graypy 0.2.14 to include fix from
+  # https://github.com/severb/graypy/commit/bf00fd283876a00aaabb182b9edd58a483ac77ea
+  # until 0.2.15 is released.
+  try:
+    import pkg_resources
+    import traceback
+  except ImportError:
+    logging.getLogger('dlstbx').warn(
+        'Could not monkey-patch graypy: setuptools missing')
+  try:
+    if pkg_resources.get_distribution('graypy').version == '0.2.14':
+      original_func = graypy.handler.make_message_dict
+      def get_full_message(record):
+        if record.exc_info:
+          return '\n'.join(traceback.format_exception(*record.exc_info))
+        if record.exc_text:
+          return record.exc_text
+        return record.getMessage()
+      def wrapper(record, *args, **kwargs):
+        retval = original_func(record, *args, **kwargs)
+        if isinstance(retval, dict):
+          retval['full_message'] = get_full_message(record)
+        return retval
+      graypy.handler.make_message_dict = wrapper
+  except Exception:
+    logging.getLogger('dlstbx').warn(
+        'Could not monkey-patch graypy')
+
   # Create and enable graylog handler
   graylog = graypy.GELFHandler('graylog.diamond.ac.uk', 12203, \
                                level_names=True)

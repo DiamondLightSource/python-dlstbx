05d1dbfecd40d8872c3538c858e7165c7bb8763b
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2017-09-15 11:25:41 +0100

    Show version information for dependency packages

--- command_line/version.py
+++ command_line/version.py
@@ -1,3 +1,13 @@
 from __future__ import division
 from dlstbx.util.version import dlstbx_version
 print dlstbx_version()
+try:
+  import ispyb
+  print "python-ispyb", ispyb.__version__
+except ImportError:
+  pass
+try:
+  import workflows
+  print "python-workflows", workflows.__version__
+except ImportError:
+  pass

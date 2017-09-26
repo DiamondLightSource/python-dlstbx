62cdc22c20c8838d1cba6471467a339123c1ac36
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2017-09-26 16:33:35 +0100

    define dlstbx requirements on python environment

--- libtbx_refresh.py
+++ libtbx_refresh.py
@@ -1,4 +1,6 @@
-from __future__ import division
+from __future__ import division, absolute_import
+
+import os
 
 try:
   from dlstbx.util.version import dlstbx_version
@@ -9,8 +11,13 @@ except Exception:
 try:
   from dials.framework import env
   import libtbx.load_env
-  from os.path import join
   path = libtbx.env.dist_path("dlstbx")
-  env.cache.add(join(path, "extensions"))
+  env.cache.add(os.path.join(path, "extensions"))
 except Exception:
   pass
+
+try:
+  import libtbx.pip
+  libtbx.pip.require('workflows', '>=0.54')
+except ImportError:
+  print "\n" * 10 + "meh. update your cctbx maybe" + "\n" * 10

87ac0c30a68cc3613d93c201bc41a41c74b0eca1
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2017-10-28 18:32:02 +0100

    comma required for version number limit

--- libtbx_refresh.py
+++ libtbx_refresh.py
@@ -19,7 +19,7 @@ except Exception:
 import libtbx.pkg_utils
 libtbx.pkg_utils.require('mock', '>=2.0')
 libtbx.pkg_utils.require('pytest', '>=3.1')
-libtbx.pkg_utils.require('ispyb', '>=0.13<1.0')
+libtbx.pkg_utils.require('ispyb', '>=0.13,<1.0')
 libtbx.pkg_utils.require('workflows', '>=0.54')
 libtbx.pkg_utils.require('drmaa')
 libtbx.pkg_utils.require('junit_xml')

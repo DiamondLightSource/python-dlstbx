fa2f982f84246b4f08545c22fa8aca802bc54a09
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2017-11-22 10:18:08 +0000

    Python3 syntax

--- command_line/list_all_entry_points.py
+++ command_line/list_all_entry_points.py
@@ -1,6 +1,8 @@
+from __future__ import absolute_import, division, print_function
+
 import pkg_resources
 
-print "Entry points:"
+print("Entry points:")
 for entry_point in sorted(pkg_resources.iter_entry_points('workflows.services')):
-  print entry_point
-#  print entry_point.load()
+  print(entry_point)
+#  print(entry_point.load())

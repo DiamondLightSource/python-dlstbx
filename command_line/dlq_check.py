aa5eea0a0bb7786ecec5cf33fb020be9c78dd43f
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2018-03-29 15:36:45 +0100

    Use JMX instead of advisory messages to determine DLQ status.
    Spoiler: is >80% faster and more reliable.

--- command_line/dlq_check.py
+++ command_line/dlq_check.py
@@ -1,101 +1,41 @@
 from __future__ import absolute_import, division, print_function
 
 import sys
-import time
-import uuid
 from optparse import SUPPRESS_HELP, OptionParser
 
-import workflows.transport
+import dlstbx.util.jmxstats
 
 #
 # dlstbx.dlq_check
 #   Check number of messages in dead letter queues
 #
 
-
-class QueueStatus():
+def check_dlq(namespace=None):
   '''Monitor ActiveMQ queue activity.'''
 
-  # Dictionary of all known queues
-  gather_interval = 5
-  status = {}
+  configfile = '/dls_sw/apps/zocalo/secrets/credentials-jmx-access.cfg'
+  jmx = dlstbx.util.jmxstats.JMXAPI(configfile)
+  if namespace:
+    namespace = namespace + "."
+  else:
+    namespace = ''
 
-  # Unique ID for this queue status monitor
-  uuid = str(uuid.uuid4())
-  report_queue = 'transient.qmonitor.' + uuid
-  namespace = ''
+  result = jmx.org.apache.activemq(
+      type="Broker",
+      brokerName="localhost",
+      destinationType="Queue",
+      destinationName="DLQ.%s*" % namespace)
+  assert result['status'] == 200
 
-  def __init__(self, transport=None, namespace=""):
-    '''Set up monitor and connect to the network transport layer'''
-    if transport is None or isinstance(transport, basestring):
-      self._transport = workflows.transport.lookup(transport)()
-    else:
-      self._transport = transport()
-    assert self._transport.connect(), "Could not connect to transport layer"
-    self._transport.subscribe(self.report_queue, self.process_report, transformation=True)
-    self.check_namespace = namespace
-    if self.check_namespace and not self.check_namespace.endswith('.'):
-      self.check_namespace = self.check_namespace + "."
-    try:
-      # Get namespace for introspection
-      self.namespace = self._transport.get_namespace() + '.'
-    except AttributeError:
-      # use '' if get_namespace() is not offered by this transport method
-      self.namespace = ''
-
-  def run(self):
-    self.last_answer = 0
-    self.seen_answers = False
-    self.dlq_messages = 0
-    self.report = {}
-    gather = time.time()
-
-    self._transport.send('ActiveMQ.Statistics.Destination.DLQ.%s>' % self.check_namespace, '', headers = { 'JMSReplyTo': self.namespace + self.report_queue }, ignore_namespace=True )
-
-    try:
-      while time.time() < gather + 0.9 and not self.seen_answers:
-        time.sleep(0.1)
-      while self.seen_answers:
-        self.seen_answers = False
-        time.sleep(0.1)
-    except KeyboardInterrupt:
-      return
-
-    if self.dlq_messages > 0:
-      print("Total of %d DLQ messages found" % self.dlq_messages)
-      sys.exit(1)
-    else:
-      print("No DLQ messages found")
-      sys.exit(0)
-
-  def process_report(self, header, message):
-    self.seen_answers = True
-
-    report = {}
-    for entry in message['map']['entry']:
-      if 'string' in entry:
-        if isinstance(entry['string'], list):
-          name = entry['string'].pop(0)
-        else:
-          name = entry['string']
-          del(entry['string'])
-      if len(entry) == 1:
-        value_type = next(entry.iterkeys())
-        report[name] = next(entry.itervalues())
-        if value_type in ('long', 'int'):
-          report[name] = int(report[name])
-        if isinstance(report[name], list) and len(report[name]) == 1:
-          report[name] = report[name][0]
-      else:
-        report[name] = entry
-    destination = report.get('destinationName')
-    if not destination: return
-    if destination.endswith(self.report_queue): return
-
-    if destination.startswith('queue://DLQ.'):
-      if report.get('size'):
-        print("DLQ for %s contains %d entries" % (destination.replace('queue://DLQ.',''), report['size']))
-        self.dlq_messages += report['size']
+  def extract_queue_name(namestring):
+    namestringdict = {
+        component.split('=')[0]: component.split('=', 1)[1]
+        for component in namestring.split(',')
+        if '=' in component
+    }
+    return namestringdict.get('destinationName')
+  queuedata = { extract_queue_name(name): data['QueueSize'] for name, data in result['value'].items() }
+  return queuedata
 
 if __name__ == '__main__':
   parser = OptionParser(
@@ -104,18 +44,14 @@ if __name__ == '__main__':
   parser.add_option("-?", action="help", help=SUPPRESS_HELP)
   parser.add_option("-n", "--namespace", dest="namespace",
       default="", help="Restrict check to this namespace")
-  parser.add_option("-t", "--transport", dest="transport", metavar="TRN",
-      default="stomp", help="Transport mechanism, default '%default'")
-
-  # override default stomp host
-  parser.add_option("--test", action="store_true", dest="test", help="Run in ActiveMQ testing (zocdev) namespace")
-  default_configuration = '/dls_sw/apps/zocalo/secrets/credentials-live.cfg'
-  if '--test' in sys.argv:
-    default_configuration = '/dls_sw/apps/zocalo/secrets/credentials-testing.cfg'
-  from workflows.transport.stomp_transport import StompTransport
-  StompTransport.load_configuration_file(default_configuration)
-
-  workflows.transport.add_command_line_options(parser)
   (options, args) = parser.parse_args()
 
-  QueueStatus(transport=options.transport, namespace=options.namespace).run()
+  dlqs = check_dlq(namespace=options.namespace)
+  for queue, count in dlqs.items():
+    print("DLQ for %s contains %d entries" % (queue.replace('DLQ.',''), count))
+  total = sum(dlqs.values())
+  if total:
+    print("Total of %d DLQ messages found" % total)
+    sys.exit(1)
+  else:
+    print("No DLQ messages found")

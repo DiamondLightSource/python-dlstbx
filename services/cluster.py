21eeab5c3c1965cbffd65438ae66cb447db720de
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2018-05-09 09:43:55 +0100

    do some basic sanity checking on SGE project name

--- services/cluster.py
+++ services/cluster.py
@@ -52,11 +52,13 @@ class DLSCluster(CommonService):
     if cluster not in ('cluster', 'testcluster'):
       cluster = 'cluster'
     submission_params = parameters.get('cluster_submission_parameters', '')
-    if parameters.get('cluster_project'):
+    if parameters.get('cluster_project') \
+        and 1 < len(parameters['cluster_project'].strip()) < 8 \
+        and '{' not in parameters['cluster_project']:
       submission_params = "-P %s %s" % (parameters['cluster_project'], submission_params)
       self.log.debug("Using cluster project %s for submission", parameters['cluster_project'])
     else:
-      self.log.info("No cluster project set for job")
+      self.log.info("No cluster project set for job (%s)", repr(parameters.get('cluster_project')))
     commands = commands.replace('$RECIPEPOINTER', str(rw.recipe_pointer))
 
     if 'recipefile' in parameters:

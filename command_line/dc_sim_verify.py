0548c1286c4799de589d0b257a885737780dc045
Dimitrios Ladakis <dimitrios.ladakis@diamond.ac.uk>
2018-10-16 16:20:26 +0100

    added comments

--- command_line/dc_sim_verify.py
+++ command_line/dc_sim_verify.py
@@ -39,14 +39,14 @@ def check_test_outcome(test):
   for dcid in test['DCIDs']:
     data_collection = db.get_data_collection(dcid)
   
-    # Storing separate integrations
+    # Store separate integration objects
     fast_dp = data_collection.integrations[0].unit_cell
     xia2_3dii = data_collection.integrations[1].unit_cell
     xia2_dials = data_collection.integrations[2].unit_cell
     ap = data_collection.integrations[3].unit_cell
     ap_staraniso = data_collection.integrations[4].unit_cell
    
-    # Testing against definitions
+    # Test against definitions and accumulate test results as a list of booleans
     scenario = test['scenario']
   
     for integration in [fast_dp, xia2_3dii, xia2_dials, ap, ap_staraniso]:
@@ -58,7 +58,7 @@ def check_test_outcome(test):
       gamma = df.tests[scenario]['results']['gamma'] == integration.gamma  
       test_results.extend([a, b, c, alpha, beta, gamma])
            
-
+  # Process the results list after all tests have been completed, decide if value of 'success' is altered
   if all(test_results):
     test['success'] = True
   else:

99bb30a11bf3f72729c25cee86030e7bcda28894
rjgildea <richard.gildea@diamond.ac.uk>
2018-10-17 11:23:58 +0100

    use new ispyb-api to get pdbids

--- zocalo/wrapper/dimple.py
+++ zocalo/wrapper/dimple.py
@@ -10,30 +10,46 @@ logger = logging.getLogger('dlstbx.wrap.dimple')
 
 class DimpleWrapper(dlstbx.zocalo.wrapper.BaseWrapper):
 
-  def construct_commandline(self, params):
+  def construct_commandline(self):
     '''Construct dimple command line.
        Takes job parameter dictionary, returns array.'''
 
     command = ['dimple']
 
-    mtz = params['dimple']['data']
-    pdb = self.get_pdb(params)
-    if pdb is None:
+    mtz = os.path.abspath(self._params['dimple']['data'])
+    pdb = self.get_matching_pdb()
+    if not len(pdb):
       logger.info('Not running dimple as no PDB file available')
       return
     assert os.path.exists(mtz), mtz
     command.append(mtz)
-    command.append(pdb)
-    output_dir = params['working_directory']
+    for p in pdb:
+      command.append(p)
+    output_dir = os.path.abspath(self._params['working_directory'])
     command.append(output_dir)
     #command.append('--dls-naming')
     command.append('-fpng')
     return command
 
-  def get_pdb(self, params):
-    '''Get pdb code or file contents from ISPyB'''
-    pdbid = params['dimple'].get('pdbid')
-    return pdbid
+  def get_matching_pdb(self):
+    import ispyb.model.__future__
+    i = ispyb.open('/dls_sw/apps/zocalo/secrets/credentials-ispyb-sp.cfg')
+    ispyb.model.__future__.enable('/dls_sw/apps/zocalo/secrets/credentials-ispyb.cfg')
+    dcid = self._params['dcid']
+    working_directory = os.path.abspath(self._params['working_directory'])
+    pdb = i.get_data_collection(dcid).pdb
+    results = []
+    for pdb in i.get_data_collection(dcid).pdb:
+      #logger.info(pdb.name, pdb.code, pdb.rawfile)
+      if pdb.code is not None:
+        results.append(pdb.code)
+      elif pdb.rawfile is not None:
+        assert pdb.name is not None
+        pdb_filepath = os.path.join(working_directory, '%s.pdb' % pdb.name)
+        with open(pdb_filepath, 'wb') as f:
+          f.write(pdb.rawfile)
+        results.append(pdb_filepath)
+    return results
 
   def send_results_to_ispyb(self, working_directory, fast_dp_directory):
     logger.debug('Inserting dimple phasing results into ISPyB')
@@ -55,9 +71,9 @@ class DimpleWrapper(dlstbx.zocalo.wrapper.BaseWrapper):
     assert hasattr(self, 'recwrap'), \
       "No recipewrapper object found"
 
-    params = self.recwrap.recipe_step['job_parameters']
+    self._params = self.recwrap.recipe_step['job_parameters']
 
-    command = self.construct_commandline(params)
+    command = self.construct_commandline()
 
     if command is None:
       return
@@ -66,13 +82,13 @@ class DimpleWrapper(dlstbx.zocalo.wrapper.BaseWrapper):
 
     cwd = os.path.abspath(os.curdir)
 
-    working_directory = params['working_directory']
+    working_directory = os.path.abspath(self._params['working_directory'])
     if not os.path.exists(working_directory):
       os.makedirs(working_directory)
     os.chdir(working_directory)
 
     result = procrunner.run_process(
-      command, timeout=params.get('timeout'),
+      command, timeout=self._params.get('timeout'),
       print_stdout=True, print_stderr=True)
 
     logger.info('command: %s', ' '.join(result['command']))
@@ -86,7 +102,7 @@ class DimpleWrapper(dlstbx.zocalo.wrapper.BaseWrapper):
 
     logger.info('Sending dimple results to ISPyB')
     self.send_results_to_ispyb(
-      working_directory, os.path.dirname(params['dimple']['data']))
+      working_directory, os.path.dirname(self._params['dimple']['data']))
 
     os.chdir(cwd)
 

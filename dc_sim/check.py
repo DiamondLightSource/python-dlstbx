6de1f6376c6e104d24520ec8689e5ef7a15d481e
Dimitrios Ladakis <dimitrios.ladakis@diamond.ac.uk>
2018-10-19 13:42:10 +0100

    refactored method to produce a 'reason' string easier

--- dc_sim/check.py
+++ dc_sim/check.py
@@ -3,44 +3,56 @@ import ispyb
 import ispyb.model.__future__
 import dlstbx.dc_sim.definitions as df
 
+
 def check_test_outcome(test, db):
   
   test_results = []
-  values_in_db = [] 
+  values_in_db = []
+  failed_tests = []
  
   for dcid in test['DCIDs']:
     data_collection = db.get_data_collection(dcid)
   
     # Store separate integration objects
-    fast_dp = data_collection.integrations[0].unit_cell
-    xia2_3dii = data_collection.integrations[1].unit_cell
-    xia2_dials = data_collection.integrations[2].unit_cell
-    ap = data_collection.integrations[3].unit_cell
-    ap_staraniso = data_collection.integrations[4].unit_cell
+    fast_dp = data_collection.integrations[0]
+    xia2_3dii = data_collection.integrations[1]
+    xia2_dials = data_collection.integrations[2]
+    ap = data_collection.integrations[3]
+    ap_staraniso = data_collection.integrations[4]
    
     # Test against definitions and accumulate test results as a list of booleans
     scenario = test['scenario']
   
     for integration in [fast_dp, xia2_3dii, xia2_dials, ap, ap_staraniso]:
-      a = df.tests[scenario]['results']['a'] == integration.a
-      b = df.tests[scenario]['results']['b'] == integration.b
-      c = df.tests[scenario]['results']['c'] == integration.c
-      alpha = df.tests[scenario]['results']['alpha'] == integration.alpha
-      beta = df.tests[scenario]['results']['beta'] == integration.beta
-      gamma = df.tests[scenario]['results']['gamma'] == integration.gamma  
-      test_results.extend([a, b, c, alpha, beta, gamma])
-      values_in_db.extend([integration.a, integration.b ,integration.c ,integration.alpha ,integration.beta ,integration.gamma])
+      if not df.tests[scenario]['results']['a'] == integration.unit_cell.a:
+        failed_tests.append('a: {0} outside range {1} program {2} DCID:{3}'.format(integration.unit_cell.a, df.tests[scenario]['results']['a'], integration.program, dcid))
+      if not df.tests[scenario]['results']['b'] == integration.unit_cell.b:
+        failed_tests.append('b: {0} outside range {1} program {2} DCID:{3}'.format(integration.unit_cell.b, df.tests[scenario]['results']['b'], integration.program.name, dcid))
+      if not df.tests[scenario]['results']['c'] == integration.unit_cell.c:
+        failed_tests.append('c: {0} outside range {1} program {2} DCID:{3}'.format(integration.unit_cell.c, df.tests[scenario]['results']['c'], integration.program.name, dcid))
+      if not df.tests[scenario]['results']['alpha'] == integration.unit_cell.alpha:
+        failed_tests.append('alpha: {0} outside range {1} program {2} DCID:{3}'.format(integration.unit_cell.alpha, df.tests[scenario]['results']['alpha'], integration.program.name, dcid))
+      if not df.tests[scenario]['results']['beta'] == integration.unit_cell.beta:
+        failed_tests.append('beta: {0} outside range {1} program {2} DCID:{3}'.format(integration.unit_cell.beta, df.tests[scenario]['results']['beta'], integration.program.name, dcid))
+      if not df.tests[scenario]['results']['gamma'] == integration.unit_cell.gamma:
+        failed_tests.append('gamma: {0} outside range {1} program {2} DCID:{3}'.format(integration.unit_cell.gamma, df.tests[scenario]['results']['gamma'], integration.program.name, dcid))  
+      #test_results.extend([a, b, c, alpha, beta, gamma])
+      #values_in_db.extend([integration.a, integration.b ,integration.c ,integration.alpha ,integration.beta ,integration.gamma])
 
            
-  # Process the results list after all tests have been completed, decide if value of 'success' is altered
-  if all(test_results):
+  # check if there are any failed tests and update the message 
+  if not failed_tests:
     test['success'] = True
-  elif all(values_in_db) and not all(test_results):
+
+  elif failed_tests:
     test['success'] = False
+    test['reason'] = '-'.join(failed_tests)
+    
   else:
     pass
 
-  print(test_results, values_in_db)
+  #print(test_results, values_in_db)
+  print(test)
 
 if __name__ == '__main__':
 

91dc1620de03232ecf5b201fea708851f4608830
rjgildea <richard.gildea@diamond.ac.uk>
2018-10-24 15:43:56 +0100

    SNMCT wrapper:
    Optionally provide dcids or appids in json file. If none are provided
    run existing magic script to find related dcids.
    
    Use new ISPyB-API stored procedure to extract relevant DIALS
    autoprocessing results.

--- zocalo/wrapper/snmct.py
+++ zocalo/wrapper/snmct.py
@@ -16,16 +16,50 @@ class SNMCTWrapper(dlstbx.zocalo.wrapper.BaseWrapper):
 
     command = ['xia2.multi_crystal_scale']
 
-    for param, value in params['snmct'].iteritems():
-      if param == 'data':
-        for v in value:
-          logging.info('Input file: %s' % v)
-          command.append(v)
-      else:
-        logging.info('Parameter %s: %s' % (param, str(value)))
-        command.append('%s=%s' % (param, value))
+    appids = params['appids']
+    if not appids:
+      dcids = self.get_dcids(params)
+      appids = self.get_appids(dcids)
+    data_files = self.get_data_files_for_appids(appids)
+    for f in data_files:
+      command.append(f)
 
     return command
+
+  def get_data_files_for_appids(self, appids):
+    data_files = []
+    for appid in appids:
+      attachments = self.ispyb_conn.mx_processing.retrieve_program_attachments_for_program_id(appid)
+      for item in attachments:
+        if item['fileType'] == 'Result':
+          if (item['fileName'].endswith('experiments.json') or
+              item['fileName'].endswith('reflections.pickle')):
+            data_files.append(os.path.join(item['filePath'], item['fileName']))
+    return data_files
+
+  def get_appids(self, dcids):
+    appids = []
+    for dcid in dcids:
+      dc = self.ispyb_conn.get_data_collection(dcid)
+      intgr = dc.integrations
+      for intgr in dc.integrations:
+        appids.append(intgr.APPID)
+    return appids
+
+  def get_dcids(self, params):
+    this_dcid = params['dcid']
+    dcids = params['dcids']
+    if not dcids:
+      command = [
+        '/dls_sw/apps/mx-scripts/misc/GetAListOfAssociatedDCOnThisCrystalOrDir.sh',
+        this_dcid
+      ]
+      result = procrunner.run_process(
+        command, timeout=params.get('timeout'),
+        working_directory=params['working_directory'],
+        print_stdout=False, print_stderr=False)
+      dcids = result['stdout'].split()
+    return [this_dcid] + dcids
 
   def send_resuls_to_ispyb(self, json_file):
     from dlstbx.ispybtbx import ispybtbx
@@ -33,24 +67,28 @@ class SNMCTWrapper(dlstbx.zocalo.wrapper.BaseWrapper):
     return
 
   def run(self):
+    import ispyb
+    import ispyb.model.__future__
+    ispyb.model.__future__.enable('/dls_sw/apps/zocalo/secrets/credentials-ispyb.cfg')
+    self.ispyb_conn = ispyb.open('/dls_sw/apps/zocalo/secrets/credentials-ispyb-sp.cfg')
+
     assert hasattr(self, 'recwrap'), \
       "No recipewrapper object found"
 
     params = self.recwrap.recipe_step['job_parameters']
-    command = self.construct_commandline(params)
-
-    # run SNMCT in working directory
-
-    cwd = os.path.abspath(os.curdir)
 
     working_directory = params['working_directory']
     if not os.path.exists(working_directory):
       os.makedirs(working_directory)
-    os.chdir(working_directory)
+
+    command = self.construct_commandline(params)
+
+    # run SNMCT in working directory
 
     result = procrunner.run_process(
       command, timeout=params.get('timeout'),
-      print_stdout=True, print_stderr=True)
+      working_directory=params['working_directory'],
+      print_stdout=False, print_stderr=False)
 
     logger.info('command: %s', ' '.join(result['command']))
     logger.info('timeout: %s', result['timeout'])
@@ -60,7 +98,5 @@ class SNMCTWrapper(dlstbx.zocalo.wrapper.BaseWrapper):
     logger.info('exitcode: %s', result['exitcode'])
     logger.debug(result['stdout'])
     logger.debug(result['stderr'])
-
-    os.chdir(cwd)
 
     return result['exitcode'] == 0

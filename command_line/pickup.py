98ce2908d0fc38a5b1baf165bf1b52b4865c4582
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2018-10-25 08:23:17 +0100

    Improve pickup script
    prioritize messages according to recipes and submitting hosts

--- command_line/pickup.py
+++ command_line/pickup.py
@@ -5,7 +5,10 @@ import os
 import sys
 import time
 
-files = os.listdir('/dls_sw/apps/zocalo/dropfiles')
+from workflows.transport.stomp_transport import StompTransport
+
+dropdir = '/dls_sw/apps/zocalo/dropfiles'
+files = os.listdir(dropdir)
 
 print("Found %d files" % len(files))
 
@@ -13,24 +16,49 @@ if not files:
   sys.exit()
 
 print("Connecting to stomp...")
-from workflows.transport.stomp_transport import StompTransport
 default_configuration = '/dls_sw/apps/zocalo/secrets/credentials-live.cfg'
 StompTransport.load_configuration_file(default_configuration)
 stomp = StompTransport()
 stomp.connect()
 
+file_info = {
+    f: {
+        'filename': os.path.join(dropdir, f),
+    } for f in files }
+hosts = {}
+
 print("Waiting $NFS seconds")
 time.sleep(60)
 
-for f in files:
-  print("Sending " + f)
+recipe_priorities = {
+  'per-image-analysis-dozor': -30,
+  'xray-centering': -10,
+  'per-image-analysis-gridscan': -10,
+  'per-image-analysis-rotation': -5,
+  'archive-cbf': 3,
+  'most-recent-for-host': 30,
+}
 
-  with open(os.path.join('/dls_sw/apps/zocalo/dropfiles', f)) as fh:
+for f in file_info.values():
+  with open(f['filename']) as fh:
     data = json.load(fh)
-    message = data['message']
-    headers = data['headers']
-  stomp.send('processing_recipe', message, headers=headers)
-  os.remove(os.path.join('/dls_sw/apps/zocalo/dropfiles', f))
-  print("Done")
-  time.sleep(1)
+    f['message'] = data['message']
+    f['headers'] = data['headers']
+  f['originating-host'] = headers.get('dlstbx.go.host')
+  f['priority'] = sum(map(lambda r: recipe_priorities.get(r, 0), message.get('recipes', [])))
+  f['last-touch'] = os.path.getmtime(f['filename'])
+  if hosts.get(f['originating-host'], {}).get('last-touch', 0) < f['last-touch']:
+    hosts[f['originating-host']] = f
 
+for f in hosts.values():
+  f['priority'] += recipe_priorities['most-recent-for-host']
+
+count = 0
+file_count = len(file_info)
+for f in sorted(file_info, key=lambda f: file_info[f]['priority'], reverse=True):
+  print("Sending " + f)
+  stomp.send('processing_recipe', file_info[f]['message'], headers=file_info[f]['headers'])
+  os.remove(file_info[f]['filename'])
+  count = count + 1
+  print("Done ({count} of {total})".format(count=count, total=file_count))
+  time.sleep(2)

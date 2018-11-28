b2b89c861ae7d220eecdacd52cccf6d31626ef08
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2018-11-28 11:09:04 +0000

    same thing without typo

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import, division, print_function
 
 import sys
 
-def enable_graylog()
+def enable_graylog():
   # Set up graylog logging using the zocalo.
   # Direct logs to the Data Analysis stream in graylog.
   import zocalo

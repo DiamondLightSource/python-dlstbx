c8086b9c2c0180949bf0b132873bcaec66fc82c7
rjgildea <richard.gildea@diamond.ac.uk>
2018-11-29 14:36:11 +0000

    snmct: send results to ispyb

--- zocalo/wrapper/snmct.py
+++ zocalo/wrapper/snmct.py
@@ -11,6 +11,55 @@ logger = logging.getLogger('dlstbx.wrap.snmct')
 
 class SNMCTWrapper(dlstbx.zocalo.wrapper.BaseWrapper):
 
+  def send_results_to_ispyb(self, z):
+    ispyb_command_list = []
+
+    # Step 1: Add new record to AutoProc, keep the AutoProcID
+    register_autoproc = {
+        'ispyb_command': 'write_autoproc',
+        'autoproc_id': None,
+        'store_result': 'ispyb_autoproc_id',
+        'spacegroup': z['spacegroup'],
+        'refinedcell_a': z['unit_cell'][0],
+        'refinedcell_b': z['unit_cell'][1],
+        'refinedcell_c': z['unit_cell'][2],
+        'refinedcell_alpha': z['unit_cell'][3],
+        'refinedcell_beta': z['unit_cell'][4],
+        'refinedcell_gamma': z['unit_cell'][5],
+    }
+    ispyb_command_list.append(register_autoproc)
+
+    # Step 2: Store scaling results, linked to the AutoProcID
+    #         Keep the AutoProcScalingID
+    insert_scaling = z['scaling_statistics']
+    insert_scaling.update({
+        'ispyb_command': 'insert_scaling',
+        'autoproc_id': '$ispyb_autoproc_id',
+        'store_result': 'ispyb_autoprocscaling_id',
+    })
+    ispyb_command_list.append(insert_scaling)
+
+    # Step 3: Store integration result, linked to the ScalingID
+    integration = {
+        'ispyb_command': 'upsert_integration',
+        'scaling_id': '$ispyb_autoprocscaling_id',
+        'cell_a': z['unit_cell'][0],
+        'cell_b': z['unit_cell'][1],
+        'cell_c': z['unit_cell'][2],
+        'cell_alpha': z['unit_cell'][3],
+        'cell_beta': z['unit_cell'][4],
+        'cell_gamma': z['unit_cell'][5],
+        #'refined_xbeam': z['refined_beam'][0],
+        #'refined_ybeam': z['refined_beam'][1],
+    }
+    ispyb_command_list.append(integration)
+
+    logger.info("Sending %s", str(ispyb_command_list))
+    self.recwrap.send_to('ispyb', {
+        'ispyb_command_list': ispyb_command_list,
+    })
+    logger.info("Sent %d commands to ISPyB", len(ispyb_command_list))
+
   def construct_commandline(self, params):
     '''Construct snmct command line.
        Takes job parameter dictionary, returns array.'''
@@ -128,6 +177,65 @@ class SNMCTWrapper(dlstbx.zocalo.wrapper.BaseWrapper):
     logger.debug(result['stdout'])
     logger.debug(result['stderr'])
 
+    json_file = working_directory.join('iotbx-merging-stats.json')
+    scaled_unmerged_mtz = working_directory.join('scaled_unmerged.mtz')
+    if scaled_unmerged_mtz.check():
+
+      import iotbx.merging_statistics
+      i_obs = iotbx.merging_statistics.select_data(
+        str(scaled_unmerged_mtz.strpath), data_labels=None)
+
+      def get_merging_statistics(i_obs):
+        result = iotbx.merging_statistics.dataset_statistics(
+          i_obs=i_obs,
+          n_bins=20,
+          anomalous=False,
+          use_internal_variance=False,
+          eliminate_sys_absent=False,
+          assert_is_not_unique_set_under_symmetry=False)
+        return result
+
+      merging_stats = get_merging_statistics(i_obs.customized_copy(anomalous_flag=False))
+      anom_merging_stats = get_merging_statistics(i_obs.customized_copy(anomalous_flag=True))
+
+      with json_file.open('wb') as fh:
+        fh.write(merging_stats.as_json())
+
+      ispyb_d = {
+        'commandline': ' '.join(result['command']),
+        'spacegroup': i_obs.space_group().type().lookup_symbol(),
+        'unit_cell': list(i_obs.unit_cell().parameters()),
+        'scaling_statistics': {},
+      }
+      for stats, bin in (('overall', merging_stats.overall),
+                         ('innerShell', merging_stats.bins[0]),
+                         ('outerShell', merging_stats.bins[-1])):
+        ispyb_d['scaling_statistics'][stats] = {
+          'anom_completeness': bin.anom_completeness,
+          'anom_multiplicity': bin.mean_redundancy,
+          'cc_anom': bin.cc_anom,
+          'cc_half': bin.cc_one_half,
+          'completeness': bin.completeness,
+          'mean_i_sig_i': bin.i_over_sigma_mean,
+          'multiplicity': bin.mean_redundancy,
+          'n_tot_obs': bin.n_obs,
+          'n_tot_unique_obs': bin.n_uniq,
+          'r_meas_all_iplusi_minus': bin.r_meas,
+          'r_merge': bin.r_merge,
+          'res_lim_high': bin.d_min,
+          'res_lim_low': bin.d_max,
+        }
+      for stats, bin in (('overall', anom_merging_stats.overall),
+                         ('innerShell', anom_merging_stats.bins[0]),
+                         ('outerShell', anom_merging_stats.bins[-1])):
+        ispyb_d['scaling_statistics'][stats].update({
+          'anom_completeness': bin.anom_completeness,
+          'anom_multiplicity': bin.mean_redundancy,
+          'cc_anom': bin.cc_anom,
+          'r_meas_all_iplusi_minus': bin.r_meas,
+        })
+      self.send_results_to_ispyb(ispyb_d)
+
     # copy output files to result directory
     results_directory.ensure(dir=True)
     if params.get('create_symlink'):
@@ -164,7 +272,7 @@ class SNMCTWrapper(dlstbx.zocalo.wrapper.BaseWrapper):
           'file_name': destination.basename,
           'file_type': filetype,
         })
-    #if allfiles:
-      #self.record_result_all_files({ 'filelist': allfiles })
+    if allfiles:
+      self.record_result_all_files({ 'filelist': allfiles })
 
     return result['exitcode'] == 0

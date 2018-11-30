74c7425732fb7d7658c0491998381fc886d587a9
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2018-11-30 09:46:17 +0000

    New command: dlstbx.show_recipeID
    Reads information about a recipe ID from the dispatcher logbook.
    This is available for 30 days after a recipe was dispatched.

---
+++ command_line/show_recipeID.py
@@ -0,0 +1,50 @@
+#
+# dlstbx.show_recipeID
+#   prints out information about a given recipe ID
+#
+
+from __future__ import absolute_import, division, print_function
+
+import py
+import sys
+from optparse import SUPPRESS_HELP, OptionParser
+
+import dlstbx.util.colorstreamhandler
+
+if __name__ == "__main__":
+    parser = OptionParser(usage="dlstbx.show_recipeID [options] recipe-ID (..)")
+    parser.add_option("-?", action="help", help=SUPPRESS_HELP)
+    (options, args) = parser.parse_args(sys.argv[1:])
+
+    if not args:
+        parser.print_help()
+        sys.exit(0)
+
+    c = dlstbx.util.colorstreamhandler.ColorStreamHandler
+    base_path = py.path.local("/dls/tmp/zocalo/dispatcher")
+    candidates = base_path.listdir()
+    directories = list(filter(lambda x: x.check(dir=True), candidates))
+
+    for recipe in args:
+        for d in directories:
+            recipe_file = d.join(recipe[0:2]).join(recipe[2:])
+            if recipe_file.check():
+                print(
+                    "{c.BOLD}{c.GREEN}Recipe {recipe}{c.DEFAULT}".format(
+                        recipe=recipe, c=c
+                    )
+                )
+                for line in recipe_file.readlines(cr=False):
+                    if not line.startswith(" ") and line.endswith(":"):
+                        print(
+                            "{c.BOLD}{c.YELLOW}{line}{c.DEFAULT}".format(line=line, c=c)
+                        )
+                    else:
+                        print(line)
+                break
+        else:
+            print(
+                "{c.BOLD}{c.RED}Recipe {recipe} not found{c.DEFAULT}".format(
+                    recipe=recipe, c=c
+                )
+            )

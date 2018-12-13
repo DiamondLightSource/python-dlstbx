0dee7e736efb240b9d253ac0fc6b8a98e25cf0ca
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2018-12-13 16:14:29 +0000

    Diffraction JPEG generating service
    Fixes SCI-7460

--- services/images.py
+++ services/images.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import, division, print_function
 
 import os
+import re
 
 import PIL.Image
 import procrunner
@@ -31,17 +32,93 @@ class DLSImages(CommonService):
     def image_call(self, rw, header, message):
         """Call dispatcher."""
         command = rw.recipe_step.get("parameters", {}).get("image_command")
-        if command == "thumbnail":
-            return self.thumbnail(rw, header, message)
+        func = "do_" + str(command)
+        if hasattr(self, func):
+            return getattr(self, func)(rw, header, message)
         self.log.error("Unknown command: %r", command)
         rw.transport.nack(header)
 
-    def thumbnail(self, rw, header, message):
+    def do_diffraction(self, rw, header, message):
+        """Take a diffraction data file and transform it into JPEGs."""
+        filename = rw.recipe_step.get("parameters", {}).get("file")
+        if isinstance(message, dict) and message.get("file"):
+            filename = message["file"]
+
+        if not filename:
+            # 'file' is a filename
+            # 'input' is a xia2-type string, may need to remove :x:x suffix
+            filename = rw.recipe_step.get("parameters", {}).get("input")
+            if isinstance(message, dict) and message.get("input"):
+                filename = message["input"]
+            if ":" in filename:
+                filename = filename.split(":")[0]
+
+        if not filename or filename == "None":
+            self.log.info("Skipping diffraction JPG generation: filename not specified")
+            rw.transport.ack(header)
+            return
+        if not os.path.exists(filename):
+            self.log.error("File %s not found", filename)
+            rw.transport.nack(header)
+            return
+        sizex = rw.recipe_step.get("parameters", {}).get("size-x", 400)
+        if isinstance(message, dict) and message.get("size-x"):
+            sizex = message["size-x"]
+        sizey = rw.recipe_step.get("parameters", {}).get("size-y", 192)
+        if isinstance(message, dict) and message.get("size-y"):
+            sizey = message["size-y"]
+        output = rw.recipe_step.get("parameters", {}).get("output")
+        if isinstance(message, dict) and message.get("output"):
+            output = message["output"]
+        if not output:
+            # split off extension
+            output = filename[: filename.rindex(".")]
+            # find comissioning directory
+            output = re.sub(
+                r"(/[a-z]{2}[0-9]{4,}-[0-9]+/)", r"\g<0>jpegs/", output, count=1
+            )
+            output = output + ".jpeg"
+        output_small = output[: output.rindex(".")] + ".thumb.jpeg"
+
+        result = procrunner.run(
+            [
+                "dials.export_bitmaps",
+                filename,
+                "imageset_index=1",
+                "format=jpeg",
+                "quality=95",
+                "binning=4",
+                "brightness=60",
+                'output_file="%s"' % output,
+            ]
+        )
+        if result["exitcode"]:
+            self.log.error(
+                "Export bitmaps failed with exitcode %r:\n%r",
+                result["exitcode"],
+                result["stderr"],
+            )
+            rw.transport.nack(header)
+            return
+        if not os.path.exists(output):
+            self.log.error("Output file %s not found", output)
+            rw.transport.nack(header)
+            return
+        with PIL.Image.open(output) as fh:
+            fh.thumbnail((sizex, sizey))
+            fh.save(output_small)
+
+        self.log.info(
+            "Created thumbnail %s -> %s -> %s", filename, output, output_small
+        )
+        rw.transport.ack(header)
+
+    def do_thumbnail(self, rw, header, message):
         """Take a single file and create a smaller version of the same file."""
         filename = rw.recipe_step.get("parameters", {}).get("file")
         if isinstance(message, dict) and message.get("file"):
             filename = message["file"]
-        if not filename or filename == 'None':
+        if not filename or filename == "None":
             self.log.info("Skipping thumbnail generation: filename not specified")
             rw.transport.ack(header)
             return

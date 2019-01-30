ca472903285c25499d12be0afbccad05b6057a3f
graeme-winter <graeme.winter@gmail.com>
2019-01-30 08:46:52 +0000

    Jiffy tool to find funny Eiger frames i.e. those with an unusual number of masked pixels

---
+++ command_line/find_funny_eiger_frames.py
@@ -0,0 +1,16 @@
+from __future__ import division, print_function, absolute_import
+from dxtbx import load
+from scitbx.array_family import flex
+
+def get_masked_pixel_count(filename):
+    images = load(filename)
+    n = images.get_num_images()
+    MAX = 0xffff
+    return [(images.get_raw_data(j) == MAX).count(True) for j in range(n)]
+
+if __name__ == '__main__':
+    import sys
+    result = get_masked_pixel_count(sys.argv[1])
+    minimum = min(result)
+    for j, r in enumerate(result):
+        print('%5d %8d' % (j, r - minimum))

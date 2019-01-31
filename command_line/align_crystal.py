d1600399f2180b4e302ad455bc80cc16bf4ec132
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2019-01-31 12:00:27 +0000

    align_crystal changes
    I think this should not be a class, so refactor towards that functions.
    If indexing fails then stop.

--- command_line/align_crystal.py
+++ command_line/align_crystal.py
@@ -11,17 +11,34 @@ import procrunner
 
 logger = logging.getLogger('dlstbx.align_crystal')
 
+def _run_command(args):
+  logger.info('command: %s', ' '.join(args))
+  result = procrunner.run_process(args,
+    print_stdout=False, print_stderr=False)
+
+  if result['exitcode']:
+    logger.info('exitcode: %s', result['exitcode'])
+    logger.info(result['stdout'])
+    logger.info(result['stderr'])
+  else:
+    logger.debug('time_start: %s', result['time_start'])
+    logger.debug('time_end: %s', result['time_end'])
+    logger.info('runtime: %s', result['runtime'])
+  return result
+
 class align_crystal(object):
   def __init__(self, image_files, nproc=None):
     self._image_files = image_files
     self._nproc = nproc
     datablock = self._import(self._image_files)
     reflections = self._find_spots(datablock)
-    experiments, reflections = self._index(datablock, reflections)
-    experiments, reflections = self._lattice(experiments, reflections)
-    self._align_crystal(experiments)
+    success, experiments, reflections = self._index(datablock, reflections)
+    if success:
+      experiments, reflections = self._lattice(experiments, reflections)
+      self._align_crystal(experiments)
 
-  def _import(self, image_files):
+  @staticmethod
+  def _import(image_files):
     '''Run dials.import'''
     datablock_json = 'datablock.json'
     args = [
@@ -29,7 +46,7 @@ class align_crystal(object):
       'allow_multiple_sweeps=True',
       'output.datablock=%s' % datablock_json
     ] + image_files
-    result = self._run_command(args)
+    result = _run_command(args)
     return datablock_json
 
   def _find_spots(self, datablock_json):
@@ -41,10 +58,11 @@ class align_crystal(object):
     ]
     if self._nproc is not None:
       args.append('nproc=%s' % self._nproc)
-    result = self._run_command(args)
+    result = _run_command(args)
     return strong_pickle
 
-  def _index(self, datablock_json, strong_pickle):
+  @staticmethod
+  def _index(datablock_json, strong_pickle):
     '''Run dials.index'''
     # could iteratively try different indexing options until one succeeds
     experiments_json = 'indexed_experiments.json'
@@ -54,13 +72,14 @@ class align_crystal(object):
       'output.experiments=%s' % experiments_json,
       'output.reflections=%s' % indexed_pickle
     ]
-    result = self._run_command(args)
-    return experiments_json, indexed_pickle
+    result = _run_command(args)
+    success = not result['exitcode'] and not result['stderr']
+    return success, experiments_json, indexed_pickle
 
   def _lattice(self, experiments_json, indexed_pickle):
     '''Run dials.refine_bravais_settings'''
     args = ['dials.refine_bravais_settings', experiments_json, indexed_pickle]
-    result = self._run_command(args)
+    result = _run_command(args)
     assert os.path.exists('bravais_summary.json')
     with open('bravais_summary.json', 'rb') as f:
       d = json.load(f)
@@ -74,35 +93,23 @@ class align_crystal(object):
     self._reindex('indexed.pickle', soln['cb_op'])
     return experiments_json, reflections_pickle
 
-  def _reindex(self, indexed_pickle, cb_op):
+  @staticmethod
+  def _reindex(indexed_pickle, cb_op):
     '''Reindex the indexed reflections using the given cb_op'''
     args = ['dials.reindex', indexed_pickle, 'change_of_basis_op="%s"' % cb_op]
-    result = self._run_command(args)
+    result = _run_command(args)
     return result
 
-  def _integrate(self, experiments_json, indexed_pickle):
+  @staticmethod
+  def _integrate(experiments_json, indexed_pickle):
     pass
 
-  def _align_crystal(self, experiments_json):
+  @staticmethod
+  def _align_crystal(experiments_json):
     args = ['dials.align_crystal', experiments_json]
-    result = self._run_command(args)
+    result = _run_command(args)
     return result
 
-  def _run_command(self, args):
-    logger.info(' '.join(args))
-    result = procrunner.run_process(args,
-      print_stdout=False, print_stderr=False)
-
-    logger.info('command: %s', ' '.join(result['command']))
-    logger.debug('timeout: %s', result['timeout'])
-    logger.debug('time_start: %s', result['time_start'])
-    logger.debug('time_end: %s', result['time_end'])
-    logger.info('runtime: %s', result['runtime'])
-    logger.info('exitcode: %s', result['exitcode'])
-    logger.info(result['stdout'])
-    logger.info(result['stderr'])
-
-    return result
 
 def run(args):
   # setup logging
@@ -115,7 +122,7 @@ def run(args):
   logger.addHandler(fh)
 
   image_files = [f for f in args if os.path.isfile(f)]
-  assert len(image_files)
+  assert image_files
   align_crystal(image_files)
 
 if __name__ == '__main__':

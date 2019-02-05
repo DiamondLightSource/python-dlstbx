78907b2d24f7ad0d859ef3ad18eeed106406941e
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2019-02-05 15:58:04 +0000

    odin2kafka: mangle multipart messages
    deserialize first part of message and generate a unique stream ID
    
    separate metadata and data into two streams

--- command_line/odin2kafka.py
+++ command_line/odin2kafka.py
@@ -1,17 +1,23 @@
 from __future__ import absolute_import, division, print_function
 
+import json
 import logging
+import random
+import string
 import sys
 from optparse import SUPPRESS_HELP, OptionParser
 
-from confluent_kafka import Producer
 import dlstbx
-from dlstbx.util.colorstreamhandler import ColorStreamHandler
 import msgpack
 import zmq
+from confluent_kafka import Producer
+from dlstbx.util.colorstreamhandler import ColorStreamHandler
 
 log = logging.getLogger("dlstbx.odin2kafka")
 
+o2k_instance = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(10))
+o2k_index = 0
+o2k_map = {}
 
 def delivery_report(err, msg):
     """
@@ -70,15 +76,27 @@ if __name__ == "__main__":
         # Trigger any available delivery report callbacks from previous produce() calls
         p.poll(0)
 
-        log.info("Received %d part multipart message (%d bytes)", len(data), sum(len(x) for x in data))
-        serial_data = msgpack.packb(data)
+        header = data[0] = json.loads(data[0])
+        if header.get('htype') in ('dheader-1.0', 'dseries_end-1.0'):
+          target_topic = "hoggery.header"
+        else:
+          target_topic = "hoggery.data"
+        series = header.get('series')
+        if series:
+          if series not in o2k_map:
+            o2k_map[series] = str(o2k_index)
+            o2k_index = o2k_index + 1
+          series_map = o2k_map[series]
+          header['hoggery-id'] = o2k_instance + ":" + series_map + ":" + options.zeromq + ":" + str(series)
 
-        print(data[0])
+        log.info("Received %d part multipart message for %s (%d bytes)", len(data), target_topic, sum(len(x) for x in data))
+        print(header)
+        serial_data = msgpack.packb(data, use_bin_type=True)
         # Asynchronously produce a message, the delivery report callback
         # will be triggered from poll() above, or flush() below, when the message has
         # been successfully delivered or failed permanently.
         log.debug("Serialised to %d bytes", len(serial_data))
-        p.produce("test", serial_data, callback=delivery_report)
+        p.produce(target_topic, serial_data, callback=delivery_report)
         p.poll(0)
 
     # Wait for any outstanding messages to be delivered and delivery report

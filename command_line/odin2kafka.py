84576469e84dcd8b7e2ce5da582fde17464c58c1
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2019-02-07 09:53:00 +0000

    Update odin2kafka interface
    * use acquisition ID
    * new topic layout

--- command_line/odin2kafka.py
+++ command_line/odin2kafka.py
@@ -2,9 +2,7 @@ from __future__ import absolute_import, division, print_function
 
 import json
 import logging
-import random
-import string
-import sys
+import time
 from optparse import SUPPRESS_HELP, OptionParser
 
 import dlstbx
@@ -15,9 +13,6 @@ from dlstbx.util.colorstreamhandler import ColorStreamHandler
 
 log = logging.getLogger("dlstbx.odin2kafka")
 
-o2k_instance = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(10))
-o2k_index = 0
-o2k_map = {}
 
 def delivery_report(err, msg):
     """
@@ -27,7 +22,12 @@ def delivery_report(err, msg):
     if err:
         log.warning("Message delivery failed: {}".format(err))
     else:
-        log.info("Message delivered to {} [{}] @{}".format(msg.topic(), msg.partition(), msg.offset()))
+        log.info(
+            "Message delivered to {} [{}] @{}".format(
+                msg.topic(), msg.partition(), msg.offset()
+            )
+        )
+
 
 if __name__ == "__main__":
     parser = OptionParser(usage="dlstbx.odin2kafka [options]")
@@ -46,6 +46,13 @@ if __name__ == "__main__":
         dest="zeromq",
         default="tcp://127.0.0.1:9999",
         help="ZeroMQ stream to connect to",
+    )
+    parser.add_option(
+        "--acqid",
+        dest="acqid",
+        default=None,
+        help="Set acquisition ID if not defined",
+        type=int,
     )
 
     (options, args) = parser.parse_args()
@@ -70,35 +77,51 @@ if __name__ == "__main__":
     consumer_receiver.connect(options.zeromq)
     log.debug("ZeroMQ connection set up")
 
-    while True:
-        p.poll(0)
-        data = consumer_receiver.recv_multipart(copy=True)
-        # Trigger any available delivery report callbacks from previous produce() calls
-        p.poll(0)
+    try:
+        while True:
+            p.poll(0)
+            data = consumer_receiver.recv_multipart(copy=True)
+            # Trigger any available delivery report callbacks from previous produce() calls
+            p.poll(0)
 
-        header = data[0] = json.loads(data[0])
-        if header.get('htype') in ('dheader-1.0', 'dseries_end-1.0'):
-          target_topic = "hoggery.header"
-        else:
-          target_topic = "hoggery.data"
-        series = header.get('series')
-        if series:
-          if series not in o2k_map:
-            o2k_map[series] = str(o2k_index)
-            o2k_index = o2k_index + 1
-          series_map = o2k_map[series]
-          header['hoggery-id'] = o2k_instance + ":" + series_map + ":" + options.zeromq + ":" + str(series)
+            header = data[0] = json.loads(data[0])
+            if not header.get("acqID"):
+                header["acqID"] = options.acqid or header.get("series", 1)
+            series = str(header["acqID"])
+            if header.get("htype") in ("dheader-1.0", "dseries_end-1.0"):
+                target_topic = "hoggery.%s.meta" % series
+            else:
+                target_topic = "hoggery.%s.data" % series
 
-        log.info("Received %d part multipart message for %s (%d bytes)", len(data), target_topic, sum(len(x) for x in data))
-        print(header)
-        serial_data = msgpack.packb(data, use_bin_type=True)
-        # Asynchronously produce a message, the delivery report callback
-        # will be triggered from poll() above, or flush() below, when the message has
-        # been successfully delivered or failed permanently.
-        log.debug("Serialised to %d bytes", len(serial_data))
-        p.produce(target_topic, serial_data, callback=delivery_report)
-        p.poll(0)
+            log.info(
+                "Received %d part multipart message for %s (%d bytes)",
+                len(data),
+                target_topic,
+                sum(len(x) for x in data),
+            )
+            print(header)
+            serial_data = msgpack.packb(data, use_bin_type=True)
+            # Asynchronously produce a message, the delivery report callback
+            # will be triggered from poll() above, or flush() below, when the message has
+            # been successfully delivered or failed permanently.
+            log.debug("Serialised to %d bytes", len(serial_data))
+            p.produce(target_topic, serial_data, callback=delivery_report)
 
+            if header.get("htype") in ("dheader-1.0", "dseries_end-1.0"):
+                log.debug("Logging stream activity to log")
+                update = {
+                    "DCID": series,
+                    "activity": "acquisition",
+                    "timestamp": time.time(),
+                }
+                update = msgpack.packb(update, use_bin_type=True)
+                p.produce(
+                    "hoggery.activity", update, key=series, callback=delivery_report
+                )
+
+            p.poll(0)
+    except KeyboardInterrupt:
+        pass
     # Wait for any outstanding messages to be delivered and delivery report
     # callbacks to be triggered.
     p.flush()

7d7b4d9f79e0f9b0e17999194fd2120ed4576d07
rjgildea <richard.gildea@diamond.ac.uk>
2019-04-04 11:15:28 +0100

    backwards compatibility for DIALS 1.x

--- command_line/eiger2xds.py
+++ command_line/eiger2xds.py
@@ -19,32 +19,62 @@ def run():
     import libtbx.load_env
 
     from dials.util.options import OptionParser
-    from dials.util.options import flatten_experiments
+    from dials.util.version import dials_version
 
     usage = "%s [options] datablock.json" % libtbx.env.dispatcher_name
 
-    parser = OptionParser(
-        usage=usage,
-        phil=phil_scope,
-        read_experiments=True,
-        read_experiments_from_images=True,
-        epilog=help_message,
-    )
-    params, options, args = parser.parse_args(
-        show_diff_phil=True, return_unhandled=True
-    )
-    experiments = flatten_experiments(params.input.experiments)
+    if dials_version().startswith("DIALS 2"):
+        from dials.util.options import flatten_experiments
 
-    if len(experiments) == 0:
-        parser.print_help()
-        exit(0)
+        parser = OptionParser(
+            usage=usage,
+            phil=phil_scope,
+            read_experiments=True,
+            read_experiments_from_images=True,
+            epilog=help_message,
+        )
+        params, options, args = parser.parse_args(
+            show_diff_phil=True, return_unhandled=True
+        )
+        experiments = flatten_experiments(params.input.experiments)
 
-    if len(experiments) > 1:
-        raise Sorry("Only one experiment can be processed at a time")
+        if len(experiments) == 0:
+            parser.print_help()
+            exit(0)
+
+        if len(experiments) > 1:
+            raise Sorry("Only one experiment can be processed at a time")
+        else:
+            imagesets = experiments.imagesets()
+            assert len(imagesets) == 1, len(imagesets)
+            imageset = imagesets[0]
+
     else:
-        imagesets = experiments.imagesets()
-        assert len(imagesets) == 1, len(imagesets)
-        imageset = imagesets[0]
+        # 20190404 backwards compatibility for DIALS 1.x
+        from dials.util.options import flatten_datablocks
+
+        parser = OptionParser(
+            usage=usage,
+            phil=phil_scope,
+            read_datablocks=True,
+            read_datablocks_from_images=True,
+            epilog=help_message,
+        )
+        params, options, args = parser.parse_args(
+            show_diff_phil=True, return_unhandled=True
+        )
+        datablocks = flatten_datablocks(params.input.datablock)
+
+        if len(datablocks) == 0:
+            parser.print_help()
+            return
+
+        if len(datablocks) > 1:
+            raise Sorry("Only one datablock can be processed at a time")
+        else:
+            imagesets = datablocks[0].extract_imagesets()
+            assert len(imagesets) == 1
+            imageset = imagesets[0]
 
     to_xds = xds.to_xds(imageset)
     xds_inp = to_xds.XDS_INP()

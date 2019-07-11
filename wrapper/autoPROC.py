4214d33b1e3193409f09f51a5ad08687efc78c6e
Irakli Sikharulidze <irakli.sikharulidze@diamond.ac.uk>
2019-07-11 17:46:03 +0100

    SCI-8817: fix flake8 warning without breaking the code

--- wrapper/autoPROC.py
+++ wrapper/autoPROC.py
@@ -477,7 +477,7 @@ class autoPROCWrapper(zocalo.wrapper.BaseWrapper):
             destination = results_directory.join(filename.basename)
             logger.debug("Copying %s to %s", filename.strpath, destination.strpath)
             filename.copy(destination)
-            if keep_as:
+            if filename.basename not in keep:
                 continue  # only copy file, do not register in ISPyB
             if "staraniso" in filename.basename:
                 anisofiles.append((destination.basename, destination.dirname, keep_as))

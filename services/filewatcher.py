faf5c76fd069947a00c041228076b4d52b5bc0df
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2020-01-30 09:26:24 +0000

    Add a profiling helper class

--- services/filewatcher.py
+++ services/filewatcher.py
@@ -1,5 +1,6 @@
 from __future__ import absolute_import, division, print_function
 
+import contextlib
 import os.path
 import time
 
@@ -25,6 +26,52 @@ def is_file_selected(file_number, selection, total_files):
         * total_files
         // (selection - 1),
     )
+
+
+class _Profiler(object):
+    """
+    A helper class that can record summary statistics on time spent in
+    code blocks. Example usage:
+
+    profiler = _Profiler()
+    with profiler.record():
+        ...
+    print(profiler.mean)
+    print(profiler.max)
+    """
+
+    def __init__(self):
+        self._timing_max = None
+        self._timing_sum = None
+        self._timing_count = None
+
+    @contextlib.contextmanager
+    def record(self):
+        start = time.time()
+        try:
+            yield
+        finally:
+            runtime = time.time() - start
+            if self._timing_count:
+                self._timing_count += 1
+                self._timing_sum += runtime
+                if runtime > self._timing_max:
+                    self._timing_max = runtime
+            else:
+                self._timing_count = 1
+                self._timing_sum = runtime
+                self._timing_max = runtime
+
+    @property
+    def max(self):
+        return self._timing_max
+
+    @property
+    def mean(self):
+        if self._timing_count:
+            return self._timing_sum / self._timing_count
+        else:
+            return None
 
 
 class DLSFileWatcher(CommonService):

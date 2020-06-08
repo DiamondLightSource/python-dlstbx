3d620bbb5b0d8c98ff0435298726d97d9df142fa
Richard Gildea <richard.gildea@diamond.ac.uk>
2020-06-08 09:34:12 +0100

    Fix eiger2xds; add test

--- command_line/eiger2xds.py
+++ command_line/eiger2xds.py
@@ -1,9 +1,14 @@
 # LIBTBX_SET_DISPATCHER_NAME eiger2xds
 
 import os
+import sys
 
 import iotbx.phil
 from dxtbx.serialize import xds
+
+from dials.util.options import flatten_experiments
+from dials.util.options import OptionParser
+
 
 help_message = """
 """
@@ -16,64 +21,31 @@ phil_scope = iotbx.phil.parse(
 )
 
 
-def run():
-    from dials.util.options import OptionParser
-    from dials.util.version import dials_version
-
+def run(args):
     usage = "eiger2xds [options] datablock.json"
 
-    if dials_version().startswith("DIALS 2"):
-        from dials.util.options import flatten_experiments
+    parser = OptionParser(
+        usage=usage,
+        phil=phil_scope,
+        read_experiments=True,
+        read_experiments_from_images=True,
+        epilog=help_message,
+    )
+    params, options, args = parser.parse_args(
+        args=args, show_diff_phil=True, return_unhandled=True
+    )
+    experiments = flatten_experiments(params.input.experiments)
 
-        parser = OptionParser(
-            usage=usage,
-            phil=phil_scope,
-            read_experiments=True,
-            read_experiments_from_images=True,
-            epilog=help_message,
-        )
-        params, options, args = parser.parse_args(
-            show_diff_phil=True, return_unhandled=True
-        )
-        experiments = flatten_experiments(params.input.experiments)
+    if len(experiments) == 0:
+        parser.print_help()
+        exit(0)
 
-        if len(experiments) == 0:
-            parser.print_help()
-            exit(0)
-
-        if len(experiments) > 1:
-            exit("Only one experiment can be processed at a time")
-        else:
-            imagesets = experiments.imagesets()
-            assert len(imagesets) == 1, len(imagesets)
-            imageset = imagesets[0]
-
+    if len(experiments) > 1:
+        exit("Only one experiment can be processed at a time")
     else:
-        # 20190404 backwards compatibility for DIALS 1.x
-        from dials.util.options import flatten_datablocks
-
-        parser = OptionParser(
-            usage=usage,
-            phil=phil_scope,
-            read_datablocks=True,
-            read_datablocks_from_images=True,
-            epilog=help_message,
-        )
-        params, options, args = parser.parse_args(
-            show_diff_phil=True, return_unhandled=True
-        )
-        datablocks = flatten_datablocks(params.input.datablock)
-
-        if len(datablocks) == 0:
-            parser.print_help()
-            return
-
-        if len(datablocks) > 1:
-            exit("Only one datablock can be processed at a time")
-        else:
-            imagesets = datablocks[0].extract_imagesets()
-            assert len(imagesets) == 1
-            imageset = imagesets[0]
+        imagesets = experiments.imagesets()
+        assert len(imagesets) == 1, len(imagesets)
+        imageset = imagesets[0]
 
     to_xds = xds.to_xds(imageset)
     xds_inp = to_xds.XDS_INP()
@@ -86,11 +58,11 @@ def run():
     if not durin_lib:
         print("Couldn't find plugin %s in PATH" % plugin_name)
 
-    with open("XDS.INP", "wb") as f:
+    with open("XDS.INP", "w") as f:
         print(xds_inp, file=f)
         print("LIB=%s" % durin_lib, file=f)
     print("Written XDS input file to XDS.INP")
 
 
 if __name__ == "__main__":
-    run()
+    run(sys.argv[1:])

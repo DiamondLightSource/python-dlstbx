e17f2565edbe8315ef796514604aef748d12b700
Richard Gildea <richard.gildea@diamond.ac.uk>
2020-06-22 12:08:23 +0100

    Mimas backlog: monitor number of waiting jobs
    Look at backlog of waiting jobs in high.q and medium.q rather than
    the number of free slots. Aim to keep a backlog of up to 10 jobs
    waiting on the cluster.

--- services/mimas_backlog.py
+++ services/mimas_backlog.py
@@ -16,7 +16,8 @@ class DLSMimasBacklog(CommonService):
         """Subscribe to mimas.held and transient.statistics.cluster"""
         self.log.info("MimasBacklog service starting up")
 
-        self._cluster_free_slots = 0
+        self._max_jobs_waiting = 10
+        self._jobs_waiting = self._max_jobs_waiting
         self._held_data = None
         self._lock = threading.Lock()
 
@@ -32,47 +33,41 @@ class DLSMimasBacklog(CommonService):
         )
 
         # Subscribe to the transient.statistics.cluster topic, which we will
-        # examine to determine the number of free slots
+        # examine to determine the number of waiting jobs
         self._transport.subscribe_broadcast(
             "transient.statistics.cluster", self.on_statistics_cluster,
         )
 
     def on_statistics_cluster(self, header, message):
         """
-        Examine the statistics cluster message to determine number of free slots.
+        Examine the message to determine number of waiting jobs.
 
-        Limit the number of free slots to 10, so that we don't dump too many
-        held messages at once, since we only get updated cluster statistics
-        intermittently.
+        If there are fewer than 10 waiting jobs, and we have a stored held
+        message, then forward this held message to the trigger step.
 
-        If we have free slots, and we have a stored held message, then forward
-        this held message to the trigger step.
-
-        We are only interested in the "live" cluster for now. Since we are
-        currently only holding messages that would have been sent to medium.q,
-        we only care about number of free slots in this queue
+        We are only interested in the "live" cluster for now. We are only
+        concerned about the number of waiting jobs in high.q or medium.q.
         """
         if (
             message["statistic-cluster"] == "live"
-            and message["statistic"] == "utilization"
+            and message["statistic"] == "waiting-jobs-per-queue"
         ):
-            free_slots = message["cpu"]["free_for_medium"]
-            self.log.debug(f"free_for_medium: {free_slots}")
             with self._lock:
-                self._cluster_free_slots = min(10, free_slots)
-                if self._cluster_free_slots > 0 and self._held_data:
+                self._jobs_waiting = message["high.q"] + message["medium.q"]
+                self.log.info(f"Jobs waiting: {self._jobs_waiting}")
+                if self._jobs_waiting < self._max_jobs_waiting and self._held_data:
                     self.forward_message(*self._held_data)
                     self._held_data = None
 
     def on_mimas_held(self, rw, header, message):
         """
-        Forward message to trigger if we have free cluster slots available.
+        Forward message to trigger if there are fewer than 10 waiting jobs.
 
-        Otherwise, store this message until we do have free slots.
+        Otherwise, store this message until there are fewer waiting jobs.
         """
-        self.log.debug(f"Free slots: {self._cluster_free_slots}")
+        self.log.debug(f"Jobs waiting: {self._jobs_waiting}")
         with self._lock:
-            if self._cluster_free_slots > 0:
+            if self._jobs_waiting < self._max_jobs_waiting:
                 self.forward_message(rw, header, message)
             else:
                 assert not self._held_data, "unexpectedly received multiple messages"
@@ -82,7 +77,8 @@ class DLSMimasBacklog(CommonService):
         """
         Forward the held message to the trigger step.
 
-        Acknowledge receipt of the message, and decrement the free-slot counter.
+        Acknowledge receipt of the message, and increment the jobs_waiting
+        counter.
         """
         # Conditionally acknowledge receipt of the message
         txn = rw.transport.transaction_begin()
@@ -91,5 +87,5 @@ class DLSMimasBacklog(CommonService):
         rw.send(message, transaction=txn)
         # Commit transaction
         rw.transport.transaction_commit(txn)
-        self._cluster_free_slots -= 1
+        self._jobs_waiting += 1
         self.log.info(f"Sent message to trigger: {message}")

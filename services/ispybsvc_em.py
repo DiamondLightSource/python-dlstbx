58cd8f79af28b60c5e33825a3658b29d25d98ac1
Anna Horstmann <anna.horstmann@diamond.ac.uk>
2020-09-01 10:36:25 +0100

    Add a service for ISPyB EM

---
+++ services/ispybsvc_em.py
@@ -0,0 +1,41 @@
+import ispyb
+
+
+class EM_Mixin:
+    def do_insert_ctf(self, parameters, **kwargs):
+
+        try:
+            result = self.ispyb.em_acquisition.insert_ctf(
+                ctf_id=parameters("ctf_id"),
+                motion_correction_id=self.get_motioncorrection_id(
+                    parameters("datacollection_id"), parameters("micrograph_name")
+                )["motioncorrection_id"],
+                auto_proc_program_id=parameters("auto_proc_program_id"),
+                box_size_x=parameters("box_size_x"),
+                box_size_y=parameters("box_size_y"),
+                min_resolution=parameters("min_resolution"),
+                max_resolution=parameters("max_resolution"),
+                min_defocus=parameters("min_defocus"),
+                max_defocus=parameters("max_defocus"),
+                astigmatism=parameters("astigmatism"),
+                defocus_step_size=parameters("defocus_step_size"),
+                astigmatism_angle=parameters("astigmatism_angle"),
+                estimated_resolution=parameters("estimated_resolution"),
+                estimated_defocus=parameters("estimated_defocus"),
+                amplitude_contrast=parameters("amplitude_contrast"),
+                cc_value=parameters("cc_value"),
+                fft_theoretical_full_path=parameters("fft_theoretical_full_path"),
+                comments=parameters("comments"),
+            )
+            return {"success": True, "return_value": result}
+        except ispyb.ISPyBException as e:
+            self.log.error(
+                "Updating DIMPLE failure for %s caused exception '%s'.",
+                e,
+                exc_info=True,
+            )
+            return False
+
+    def get_motioncorrection_id(self, datacollectionid, micrographname):
+        """Not implemented yet"""
+        return {"motioncorrection_id": 1234}
---
+++ test/services/test_ispybsvc_em.py
@@ -0,0 +1,48 @@
+import mock
+import ispyb
+import services.ispybsvc_em as em_svc
+
+
+def parameters(arg):
+    return "parameters:" + arg
+
+
+def test_do_insert_ctf_handles_ispyb_error():
+    dls = em_svc.EM_Mixin()
+    dls.ispyb = mock.Mock()
+    dls.ispyb.em_acquisition.insert_ctf.side_effect = ispyb.ISPyBException()
+    dls.log = mock.Mock()
+
+    assert dls.do_insert_ctf(parameters) is False
+    assert dls.log.error.called
+
+
+def test_insert_ctf_is_called_with_parameters():
+    dls = em_svc.EM_Mixin()
+    dls.ispyb = mock.Mock()
+    dls.ispyb.em_acquisition.insert_ctf = mock.Mock()
+    return_test = dls.do_insert_ctf(parameters)
+    assert return_test == {
+        "success": True,
+        "return_value": dls.ispyb.em_acquisition.insert_ctf.return_value,
+    }
+    dls.ispyb.em_acquisition.insert_ctf.assert_called_once_with(
+        ctf_id=parameters("ctf_id"),
+        motion_correction_id=1234,
+        auto_proc_program_id=parameters("auto_proc_program_id"),
+        box_size_x=parameters("box_size_x"),
+        box_size_y=parameters("box_size_y"),
+        min_resolution=parameters("min_resolution"),
+        max_resolution=parameters("max_resolution"),
+        min_defocus=parameters("min_defocus"),
+        max_defocus=parameters("max_defocus"),
+        astigmatism=parameters("astigmatism"),
+        defocus_step_size=parameters("defocus_step_size"),
+        astigmatism_angle=parameters("astigmatism_angle"),
+        estimated_resolution=parameters("estimated_resolution"),
+        estimated_defocus=parameters("estimated_defocus"),
+        amplitude_contrast=parameters("amplitude_contrast"),
+        cc_value=parameters("cc_value"),
+        fft_theoretical_full_path=parameters("fft_theoretical_full_path"),
+        comments=parameters("comments"),
+    )

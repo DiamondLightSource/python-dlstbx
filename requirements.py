8bdf0cd73e190b1f2d5f99bdc49b1f0ddd650839
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2020-10-02 14:03:02 +0100

    Jenkins installation does not have the conda command

--- requirements.py
+++ requirements.py
@@ -142,4 +142,4 @@ def check():
 if __name__ == "__main__":
     actions = check()
     if actions:
-        subprocess.run(["conda", "install", *actions, *sys.argv[1:]], check=True)
+        subprocess.run(["libtbx.conda", "install", *actions, *sys.argv[1:]], check=True)

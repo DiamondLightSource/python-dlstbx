a909011207576ea32ae7984dd4983edf68ebb98f
Richard Gildea <richard.gildea@diamond.ac.uk>
2020-11-25 17:46:36 +0000

    Test _notify_for_found_file

--- test/services/test_filewatcher.py
+++ test/services/test_filewatcher.py
@@ -1,4 +1,5 @@
 import time
+import pytest
 from unittest import mock
 
 import workflows.transport.common_transport
@@ -49,10 +50,7 @@ def test_filewatcher_watch_pattern(mocker, tmpdir):
             "expected-per-image-delay": "0.1",
             "timeout": 1,
         },
-        output={
-            "any": 1,
-            "select-2": 2,
-        },
+        output={"any": 1, "select-2": 2},
     )
     rw = RecipeWrapper(message=m, transport=t)
     send_to = mocker.spy(rw, "send_to")
@@ -162,21 +160,15 @@ def test_filewatcher_watch_list(mocker, tmpdir):
         print(send_to.mock_calls)
         if i == 0:
             send_to.assert_any_call(
-                "first",
-                {"file": f, "file-list-index": i + 1},
-                transaction=mock.ANY,
+                "first", {"file": f, "file-list-index": i + 1}, transaction=mock.ANY
             )
         send_to.assert_has_calls(
             [
                 mock.call(
-                    "every",
-                    {"file": f, "file-list-index": i + 1},
-                    transaction=mock.ANY,
+                    "every", {"file": f, "file-list-index": i + 1}, transaction=mock.ANY
                 ),
                 mock.call(
-                    i + 1,
-                    {"file": f, "file-list-index": i + 1},
-                    transaction=mock.ANY,
+                    i + 1, {"file": f, "file-list-index": i + 1}, transaction=mock.ANY
                 ),
                 mock.call(
                     f"{i + 1}",
@@ -318,24 +310,16 @@ def test_filewatcher_watch_list_timeout(mocker, tmpdir):
     send_to.assert_has_calls(
         [
             mock.call(
-                "first",
-                {"file": files[0], "file-list-index": 1},
-                transaction=mock.ANY,
+                "first", {"file": files[0], "file-list-index": 1}, transaction=mock.ANY
             ),
             mock.call(
-                "every",
-                {"file": files[0], "file-list-index": 1},
-                transaction=mock.ANY,
+                "every", {"file": files[0], "file-list-index": 1}, transaction=mock.ANY
             ),
             mock.call(
-                1,
-                {"file": files[0], "file-list-index": 1},
-                transaction=mock.ANY,
+                1, {"file": files[0], "file-list-index": 1}, transaction=mock.ANY
             ),
             mock.call(
-                "1",
-                {"file": files[0], "file-list-index": 1},
-                transaction=mock.ANY,
+                "1", {"file": files[0], "file-list-index": 1}, transaction=mock.ANY
             ),
         ]
     )
@@ -373,3 +357,30 @@ def test_parse_selections():
     assert DLSFileWatcher._parse_selections({"select": 2}) == {}
     assert DLSFileWatcher._parse_selections({"select-1": 3}) == {1: "select-1"}
     assert DLSFileWatcher._parse_selections({"select-2": 4}) == {2: "select-2"}
+
+
+@pytest.mark.parametrize(
+    "nth_file,expected",
+    [
+        (1, ["first", "every", 1, "1", "select-2", "every-3"]),
+        (2, ["every", 2, "2"]),
+        (4, ["every", 4, "4", "every-3"]),
+        (9, ["every", 9, "9", "select-2", "last"]),
+    ],
+)
+def test_notify_for_found_file(nth_file, expected):
+    notify_function = mock.Mock()
+    DLSFileWatcher._notify_for_found_file(
+        nth_file=nth_file,
+        filecount=9,
+        selections={2: "select-2"},
+        everys={3: "every-3"},
+        notify_function=notify_function,
+    )
+    for notify in expected:
+        notify_function.assert_any_call(notify)
+    for notify in {"first", "every", nth_file, "select-2", "every-3", "last"} - set(
+        expected
+    ):
+        with pytest.raises(AssertionError):
+            notify_function.assert_any_call(notify)

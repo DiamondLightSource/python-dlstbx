eb7f6bec9556d635a30d033c64172e107d464681
Richard Gildea <richard.gildea@diamond.ac.uk>
2021-01-18 10:36:08 +0000

    SWMR only for i03

--- mimas/core.py
+++ mimas/core.py
@@ -160,11 +160,12 @@ def run(
                     DCID=scenario.DCID, recipe="per-image-analysis-eiger-streamdump"
                 )
             )
-            tasks.append(
-                dlstbx.mimas.MimasRecipeInvocation(
-                    DCID=scenario.DCID, recipe="per-image-analysis-gridscan-swmr"
+            if scenario.beamline == "i03":
+                tasks.append(
+                    dlstbx.mimas.MimasRecipeInvocation(
+                        DCID=scenario.DCID, recipe="per-image-analysis-gridscan-swmr"
+                    )
                 )
-            )
 
     if scenario.event == dlstbx.mimas.MimasEvent.END:
 

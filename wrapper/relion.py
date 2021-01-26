e3ea2011356424e088829f36b58fd4e123574237
Colin Palmer <colin.palmer@stfc.ac.uk>
2021-01-26 18:49:14 +0000

    Update relion_it module version for testing

--- wrapper/relion.py
+++ wrapper/relion.py
@@ -89,7 +89,7 @@ class RelionWrapper(zocalo.wrapper.BaseWrapper):
             "#!/bin/bash",
             "source /etc/profile.d/modules.sh",
             "module load hamilton",
-            "module load EM/yolo_relion_it/relion_3.1_cryolo_1.6.1",
+            "module load EM/yolo_relion_it/relion_3.1.1_cryolo_1.7.6",
             " ".join(["exec"] + [str(item) for item in relion_command]),
         ]
         script_file = working_directory / "run_script.sh"

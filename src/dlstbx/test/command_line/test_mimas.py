8f27a3d150c4392ad19722314e95225d98f47200
Richard Gildea <richard.gildea@diamond.ac.uk>
2021-03-18 20:37:39 +0000

    Extract get_scenarios() function in mimas cli
    Use this to test mimas actions without relying on the cli tool.

--- src/dlstbx/test/command_line/test_mimas.py
+++ src/dlstbx/test/command_line/test_mimas.py
@@ -1,7 +1,22 @@
 # flake8: noqa W291
 import pytest
 
+import dlstbx.mimas
 from dlstbx.cli import mimas
+
+
+def get_zocalo_commands(dcid):
+    scenarios = mimas.get_scenarios(dcid)
+    commands = {}
+    for scenario in scenarios:
+        commands[scenario.event.name] = set()
+        actions = dlstbx.mimas.core.run(scenario)
+        for a in actions:
+            dlstbx.mimas.validate(a)
+            commands[scenario.event.name].add(
+                dlstbx.mimas.zocalo_command_line(a).strip()
+            )
+    return commands
 
 
 @pytest.mark.parametrize(
@@ -12,22 +27,21 @@ from dlstbx.cli import mimas
         5918093,  # i24
     ],
 )
-def test_eiger_rotation(dcid, capsys):
-    mimas.run([f"{dcid}", "-c"])
-    captured = capsys.readouterr()
-    expected = f"""\
-At the start of data collection {dcid}:
- - zocalo.go -r per-image-analysis-rotation-swmr {dcid}
-
-At the end of data collection {dcid}:
- - ispyb.job --new --dcid={dcid} --source=automatic --recipe=autoprocessing-autoPROC-eiger   
- - ispyb.job --new --dcid={dcid} --source=automatic --recipe=autoprocessing-fast-dp-eiger   --trigger 
- - ispyb.job --new --dcid={dcid} --source=automatic --recipe=autoprocessing-xia2-3dii-eiger  --add-param=resolution.cc_half_significance_level:0.1 
- - ispyb.job --new --dcid={dcid} --source=automatic --recipe=autoprocessing-xia2-dials-eiger  --add-param=resolution.cc_half_significance_level:0.1 --trigger 
- - zocalo.go -r archive-nexus {dcid}
- - zocalo.go -r generate-crystal-thumbnails {dcid}
- - zocalo.go -r generate-diffraction-preview {dcid}"""
-    assert expected in captured.out
+def test_eiger_rotation(dcid):
+    commands = get_zocalo_commands(dcid)
+    print(commands)
+    assert commands == {
+        "START": {f"zocalo.go -r per-image-analysis-rotation-swmr {dcid}"},
+        "END": {
+            f"ispyb.job --new --dcid={dcid} --source=automatic --recipe=autoprocessing-xia2-3dii-eiger  --add-param=resolution.cc_half_significance_level:0.1",
+            f"ispyb.job --new --dcid={dcid} --source=automatic --recipe=autoprocessing-fast-dp-eiger   --trigger",
+            f"zocalo.go -r generate-diffraction-preview {dcid}",
+            f"ispyb.job --new --dcid={dcid} --source=automatic --recipe=autoprocessing-xia2-dials-eiger  --add-param=resolution.cc_half_significance_level:0.1 --trigger",
+            f"ispyb.job --new --dcid={dcid} --source=automatic --recipe=autoprocessing-autoPROC-eiger",
+            f"zocalo.go -r generate-crystal-thumbnails {dcid}",
+            f"zocalo.go -r archive-nexus {dcid}",
+        },
+    }
 
 
 def test_i03_eiger_screening(capsys):

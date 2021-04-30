329a88c0268cb0ed8bc75dcc0e5b21936df55da6
Daniel Hatton <daniel.hatton@diamond.ac.uk>
2021-04-30 11:03:30 +0100

    Add dummy do_insert_particle_picker method to ISPyB service to avoid future crashes from Relion wrapper messages

--- src/dlstbx/services/ispybsvc_em.py
+++ src/dlstbx/services/ispybsvc_em.py
@@ -100,6 +100,14 @@ class EM_Mixin:
             )
             return False
 
+    def do_insert_particle_picker(self, parameters, **kwargs):
+        # We don't yet have a way of inserting information from this message
+
+        appid = parameters("program_id")
+        dcid = parameters("dcid")
+        self.log.info(f"Would insert particle picker parameters. AutoProcProgramID: {appid}, DCID: {dcid}")
+        return {"success": True, "return_value": None}
+
     def do_insert_class2d(self, parameters, **kwargs):
         # This gives some output we can read from; ISPyB doesn't have fields for Class 2D yet
 

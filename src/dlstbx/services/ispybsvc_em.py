3c188825264119c2d76a899a0e9fadb1179e2a1f
Anna Horstmann <anna.horstmann@diamond.ac.uk>
2021-05-18 09:03:22 +0100

    Fix ordering of insertion calls and correct naming.

--- src/dlstbx/services/ispybsvc_em.py
+++ src/dlstbx/services/ispybsvc_em.py
@@ -144,7 +144,7 @@ class EM_Mixin:
         try:
             result = self.ispyb.em_acquisition.insert_particle_picker(
                 particle_picker_id=parameters("particle_picker_id"),
-                first_motion_correction_id=parameters("first_motion_correction_id"),
+                first_motion_correction_id=mcid,
                 auto_proc_program_id=parameters("program_id"),
                 particle_picking_template=parameters("particle_picking_template"),
                 particle_diameter=parameters("particle_diameter"),
@@ -165,22 +165,26 @@ class EM_Mixin:
         dcid = parameters("dcid")
         self.log.info(f"Inserting 2D Classification parameters. DCID: {dcid}")
         try:
-            result = self.ispyb.em_acquisition.insert_particle_classification_group(
-                particle_classification_group_id=parameters(
-                    "particle_classification_group_id"
-                ),
-                particle_picker_id=parameters("particle_picker_id"),
-                auto_proc_program_id=parameters("program_id"),
-                type="2D",
-                batch_number=parameters("batch_number"),
-                number_of_particles_per_batch=parameters(
-                    "number_of_particles_per_batch"
-                ),
-                number_of_classes_per_batch=parameters("number_of_classes_per_batch"),
-                symmetry=parameters("symmetry"),
+            class2d_result = (
+                self.ispyb.em_acquisition.insert_particle_classification_group(
+                    particle_classification_group_id=parameters(
+                        "particle_classification_group_id"
+                    ),
+                    particle_picker_id=parameters("particle_picker_id"),
+                    auto_proc_program_id=parameters("program_id"),
+                    type="2D",
+                    batch_number=parameters("batch_number"),
+                    number_of_particles_per_batch=parameters(
+                        "number_of_particles_per_batch"
+                    ),
+                    number_of_classes_per_batch=parameters(
+                        "number_of_classes_per_batch"
+                    ),
+                    symmetry=parameters("symmetry"),
+                )
             )
             self.log.info(
-                f"Created 2D Classification Group record {result} for DCID {dcid}"
+                f"Created 2D Classification Group record {class2d_result} for DCID {dcid}"
             )
         except ispyb.ISPyBException as e:
             self.log.error(
@@ -189,21 +193,27 @@ class EM_Mixin:
                 exc_info=True,
             )
         try:
-            result = self.ispyb.em_acquisition.insert_particle_classification(
-                particle_classification_id=parameters("particle_classification_id"),
-                particle_classification_group_id=parameters(
-                    "particle_classification_group_id"
-                ),
-                class_number=parameters("class_number"),
-                class_image_full_path=parameters("class_image_full_path"),
-                particles_per_class=parameters("partices_per_class"),
-                rotation_accuracy=parameters("rotation_accuracy"),
-                translation_accuracy=parameters("translation_accuracy"),
-                estimated_resolution=parameters("estimated_resolution"),
-                overall_fourier_completeness=parameters("overall_fourier_completeness"),
+            class2d_group_result = (
+                self.ispyb.em_acquisition.insert_particle_classification(
+                    particle_classification_id=parameters("particle_classification_id"),
+                    particle_classification_group_id=parameters(
+                        "particle_classification_group_id"
+                    ),
+                    class_number=parameters("class_number"),
+                    class_image_full_path=parameters("class_image_full_path"),
+                    particles_per_class=parameters("partices_per_class"),
+                    rotation_accuracy=parameters("rotation_accuracy"),
+                    translation_accuracy=parameters("translation_accuracy"),
+                    estimated_resolution=parameters("estimated_resolution"),
+                    overall_fourier_completeness=parameters(
+                        "overall_fourier_completeness"
+                    ),
+                )
             )
-            self.log.info(f"Created 2D Classification record {result} for DCID {dcid}")
-            return {"success": True, "return_value": result}
+            self.log.info(
+                f"Created 2D Classification record {class2d_group_result} for DCID {dcid}"
+            )
+            return {"success": True, "return_value": class2d_group_result}
         except ispyb.ISPyBException as e:
             self.log.error(
                 "Inserting 2D Classification entry caused exception '%s'.",
@@ -215,42 +225,30 @@ class EM_Mixin:
     def do_insert_class3d(self, parameters, **kwargs):
 
         dcid = parameters("dcid")
-        self.log.info(f"Inserting CryoEM Initial Model parameters. DCID: {dcid}")
-        try:
-            result = self.ispyb.em_acquisition.insert_cryoem_initial_model(
-                cryoem_initial_model_id=parameters("cryoem_inital_model_id"),
-                particle_classification_id=parameters("particle_classification_id"),
-                resolution=parameters("resolution"),
-                number_of_particles=parameters("number_of_particles"),
-            )
-            self.log.info(
-                f"Created CryoEM Initial Model record {result} for DCID {dcid}"
-            )
-        except ispyb.ISPyBException as e:
-            self.log.error(
-                "Inserting CryoEM Initial Model entry caused exception '%s'.",
-                e,
-                exc_info=True,
-            )
+        class3d_result = None
 
         self.log.info(f"Inserting 3D Classification parameters. DCID: {dcid}")
         try:
-            result = self.ispyb.em_acquisition.insert_particle_classification_group(
-                particle_classification_group_id=parameters(
-                    "particle_classification_group_id"
-                ),
-                particle_picker_id=parameters("particle_picker_id"),
-                auto_proc_program_id=parameters("program_id"),
-                type="3D",
-                batch_number=parameters("batch_number"),
-                number_of_particles_per_batch=parameters(
-                    "number_of_particles_per_batch"
-                ),
-                number_of_classes_per_batch=parameters("number_of_classes_per_batch"),
-                symmetry=parameters("symmetry"),
+            class3d_result = (
+                self.ispyb.em_acquisition.insert_particle_classification_group(
+                    particle_classification_group_id=parameters(
+                        "particle_classification_group_id"
+                    ),
+                    particle_picker_id=parameters("particle_picker_id"),
+                    auto_proc_program_id=parameters("program_id"),
+                    type="3D",
+                    batch_number=parameters("batch_number"),
+                    number_of_particles_per_batch=parameters(
+                        "number_of_particles_per_batch"
+                    ),
+                    number_of_classes_per_batch=parameters(
+                        "number_of_classes_per_batch"
+                    ),
+                    symmetry=parameters("symmetry"),
+                )
             )
             self.log.info(
-                f"Created 3D Classification Group record {result} for DCID {dcid}"
+                f"Created 3D Classification Group record {class3d_result} for DCID {dcid}"
             )
         except ispyb.ISPyBException as e:
             self.log.error(
@@ -259,24 +257,49 @@ class EM_Mixin:
                 exc_info=True,
             )
         try:
-            result = self.ispyb.em_acquisition.insert_particle_classification(
-                particle_classification_id=parameters("particle_classification_id"),
-                particle_classification_group_id=parameters(
-                    "particle_classification_group_id"
-                ),
-                class_number=parameters("class_number"),
-                class_image_full_path=parameters("class_image_full_path"),
-                particles_per_class=parameters("partices_per_class"),
-                rotation_accuracy=parameters("rotation_accuracy"),
-                translation_accuracy=parameters("translation_accuracy"),
-                estimated_resolution=parameters("estimated_resolution"),
-                overall_fourier_completeness=parameters("overall_fourier_completeness"),
+            class3d_group_result = (
+                self.ispyb.em_acquisition.insert_particle_classification(
+                    particle_classification_id=parameters("particle_classification_id"),
+                    particle_classification_group_id=parameters(
+                        "particle_classification_group_id"
+                    ),
+                    class_number=parameters("class_number"),
+                    class_image_full_path=parameters("class_image_full_path"),
+                    particles_per_class=parameters("partices_per_class"),
+                    rotation_accuracy=parameters("rotation_accuracy"),
+                    translation_accuracy=parameters("translation_accuracy"),
+                    estimated_resolution=parameters("estimated_resolution"),
+                    overall_fourier_completeness=parameters(
+                        "overall_fourier_completeness"
+                    ),
+                )
             )
-            self.log.info(f"Created 3D Classification record {result} for DCID {dcid}")
-            return {"success": True, "return_value": result}
+            self.log.info(
+                f"Created 3D Classification record {class3d_group_result} for DCID {dcid}"
+            )
+            return {"success": True, "return_value": class3d_group_result}
         except ispyb.ISPyBException as e:
             self.log.error(
                 "Inserting 3D Classification entry caused exception '%s'.",
+                e,
+                exc_info=True,
+            )
+        self.log.info(f"Inserting CryoEM Initial Model parameters. DCID: {dcid}")
+        try:
+            initial_model_result = (
+                self.ispyb.em_acquisition.insert_cryoem_initial_model(
+                    cryoem_initial_model_id=parameters("cryoem_inital_model_id"),
+                    particle_classification_id=class3d_result,
+                    resolution=parameters("init_model_resolution"),
+                    number_of_particles=parameters("init_model_number_of_particles"),
+                )
+            )
+            self.log.info(
+                f"Created CryoEM Initial Model record {initial_model_result} for DCID {dcid}"
+            )
+        except ispyb.ISPyBException as e:
+            self.log.error(
+                "Inserting CryoEM Initial Model entry caused exception '%s'.",
                 e,
                 exc_info=True,
             )

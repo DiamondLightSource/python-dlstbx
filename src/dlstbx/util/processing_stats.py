76f9e98ed483f0eaf58416dfb0d6f6108ad1ef53
Irakli Sikharulidze <irakli.sikharulidze@diamond.ac.uk>
2021-06-22 12:45:57 +0100

    Refactor map calculation code

--- src/dlstbx/util/processing_stats.py
+++ src/dlstbx/util/processing_stats.py
@@ -1,13 +1,13 @@
-import os
-from os.path import basename, splitext
 import itertools
 import operator
+import os
+from os.path import basename, splitext
 
+import cctbx.maptbx.resolution_from_map_and_model
 import iotbx.ccp4_map
 import iotbx.pdb
-from iotbx.gui_tools.reflections import map_coeffs_from_mtz_file
-import cctbx.maptbx.resolution_from_map_and_model
 import mmtbx.maps.correlation
+from iotbx.gui_tools.reflections import map_coeffs_from_mtz_file
 
 
 def get_pdb_chain_stats(pdb_file, logger):
@@ -73,7 +73,7 @@ def get_pdb_chain_stats(pdb_file, logger):
     }
 
 
-def get_mapfile_stats(_wd, mdl_dict, logger):
+def get_mapfile(_wd, mdl_dict, logger):
     map_filename = "".join(
         [
             splitext(basename(mdl_dict["mtz"]))[0],
@@ -84,28 +84,19 @@ def get_mapfile_stats(_wd, mdl_dict, logger):
     )
     map_filepath = os.path.join(_wd, map_filename)
 
-    try:
-        map_coeffs = map_coeffs_from_mtz_file(
-            mdl_dict["mtz"],
-            f_label=",".join([mdl_dict["fwt"], mdl_dict["phwt"]]),
-            phi_label=None,
-            fom_label=None,
-        )
+    map_coeffs = map_coeffs_from_mtz_file(
+        mdl_dict["mtz"],
+        f_label=",".join([mdl_dict["fwt"], mdl_dict["phwt"]]),
+        phi_label=None,
+        fom_label=None,
+    )
 
-        map_coeffs = map_coeffs.map_to_asu().average_bijvoet_mates()
-        map_data = map_coeffs.fft_map(resolution_factor=1 / 3.0)
-        map_data.apply_sigma_scaling()
-        map_data.as_ccp4_map(file_name=map_filepath)
-    except Exception:
-        logger.info(f"Cannot generate {map_filepath} map file")
-        return None
-    try:
-        mapcc, mapcc_dmin = calculate_mapcc(mdl_dict["pdb"], map_filepath, logger)
-    except Exception:
-        logger.info(f"Cannot generate {map_filepath} map file")
-        return None
+    map_coeffs = map_coeffs.map_to_asu().average_bijvoet_mates()
+    map_data = map_coeffs.fft_map(resolution_factor=1 / 3.0)
+    map_data.apply_sigma_scaling()
+    map_data.as_ccp4_map(file_name=map_filepath)
 
-    return (map_filepath, mapcc, mapcc_dmin)
+    return map_filepath
 
 
 def calculate_mapcc(pdb_filepath, map_filepath, logger):
@@ -146,24 +137,31 @@ def get_model_data(working_directory, mdl_dict, logger):
     try:
         model_dict.update(get_pdb_chain_stats(mdl_dict["pdb"], logger))
     except Exception:
-        logger.info("Cannot get chain stats from results files")
+        logger.info(f"Cannot get chain stats from {mdl_dict['pdb']}")
         return
 
     try:
-        (map_filename, mapcc, mapcc_dmin) = get_mapfile_stats(
-            working_directory, mdl_dict, logger
-        )
-        if map_filename:
-            model_dict.update(
-                {
-                    "map": map_filename,
-                    "mapcc": mapcc,
-                    "mapcc_dmin": mapcc_dmin,
-                }
-            )
-
+        map_filename = get_mapfile(working_directory, mdl_dict, logger)
+        model_dict["map"] = map_filename
     except Exception:
-        logger.info("Cannot generate map file from results")
+        logger.info(f"Cannot generate map file from {mdl_dict['mtz']}")
         return
+
+    if model_dict["total"] == 0:
+        logger.info("Protein model not available")
+        return
+
+    try:
+        map_filepath = os.path.join(working_directory, map_filename)
+        mapcc, mapcc_dmin = calculate_mapcc(mdl_dict["pdb"], map_filepath, logger)
+        model_dict.update(
+            {
+                "mapcc": mapcc,
+                "mapcc_dmin": mapcc_dmin,
+            }
+        )
+    except Exception:
+        logger.info(f"Cannot generate mapCC values from {map_filepath} map file")
+        return None
 
     return model_dict

9597b0a532d110a8f2a38b2e9bf6455075f2b58e
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2021-07-09 13:58:47 +0100

    Run health checks against all sorts of webservices

--- setup.py
+++ setup.py
@@ -130,10 +130,16 @@ service_list = [
 health_checks = [
     "activemq = dlstbx.health_checks.activemq:check_activemq_dlq",
     "gpfs = dlstbx.health_checks.graylog:check_gfps_expulsion",
-    "services.epics.available = dlstbx.health_checks.epics:get_diamond_ring_status",
-    "services.mx.agamemnon = dlstbx.health_checks.network:check_agamemnon",
     "graylog-alive = dlstbx.health_checks.graylog:check_graylog_is_alive",
     "graylog-history = dlstbx.health_checks.graylog:check_graylog_has_history",
+    "services.dls.cas = dlstbx.health_checks.network:check_cas",
+    "services.dls.epics = dlstbx.health_checks.epics:get_diamond_ring_status",
+    "services.dls.gitlab = dlstbx.health_checks.network:check_gitlab",
+    "services.dls.jira = dlstbx.health_checks.network:check_jira",
+    "services.dls.synchweb = dlstbx.health_checks.network:check_synchweb",
+    "services.dls.uas = dlstbx.health_checks.network:check_uas",
+    "services.mx.agamemnon = dlstbx.health_checks.network:check_agamemnon",
+    "services.mx.dbserver = dlstbx.health_checks.network:check_dbserver",
     "slowfs = dlstbx.health_checks.graylog:check_filesystem_is_responsive",
 ]
 
--- src/dlstbx/health_checks/network.py
+++ src/dlstbx/health_checks/network.py
@@ -5,8 +5,46 @@ import urllib.request
 from dlstbx.health_checks import REPORT, CheckFunctionInterface, Status
 
 
+def _check_service(URL, checkname, servicename):
+    try:
+        with urllib.request.urlopen(URL, timeout=3) as response:
+            data = response.read(250)
+            status = response.status
+    except socket.timeout:
+        return Status(
+            Source=checkname,
+            Level=REPORT.ERROR,
+            Message=f"Connection to {servicename} timed out",
+            URL=URL,
+        )
+    except urllib.error.URLError as e:
+        return Status(
+            Source=checkname,
+            Level=REPORT.ERROR,
+            Message=f"Connection to {servicename} failed with {e}",
+            MessageBody=repr(e),
+            URL=URL,
+        )
+
+    if status == 200:
+        return Status(
+            Source=checkname,
+            Level=REPORT.PASS,
+            Message=f"{servicename} online",
+            URL=URL,
+        )
+    return Status(
+        Source=checkname,
+        Level=REPORT.ERROR,
+        Message=f"{servicename} returned status {status}",
+        MessageBody="Response: " + data.decode("latin-1"),
+        URL=URL,
+    )
+
+
 def check_agamemnon(cfc: CheckFunctionInterface):
     URL = "http://agamemnon.diamond.ac.uk:8080/status"
+    servicename = "Agamemnon"
     try:
         with urllib.request.urlopen(URL, timeout=3) as response:
             data = response.read(250)
@@ -15,14 +53,14 @@ def check_agamemnon(cfc: CheckFunctionInterface):
         return Status(
             Source=cfc.name,
             Level=REPORT.ERROR,
-            Message=f"Connection to Agamemnon timed out",
+            Message=f"Connection to {servicename} timed out",
             URL=URL,
         )
     except urllib.error.URLError as e:
         return Status(
             Source=cfc.name,
             Level=REPORT.ERROR,
-            Message=f"Connecting to Agamemnon failed with {e}",
+            Message=f"Connecting to {servicename} failed with {e}",
             MessageBody=repr(e),
             URL=URL,
         )
@@ -36,13 +74,49 @@ def check_agamemnon(cfc: CheckFunctionInterface):
         return Status(
             Source=cfc.name,
             Level=REPORT.PASS,
-            Message=f"Agamemnon{version} is alive and responsive",
+            Message=f"{servicename}{version} is alive and responsive",
             URL=URL,
         )
     return Status(
         Source=cfc.name,
         Level=REPORT.ERROR,
-        Message=f"Agamemnon returned status {status}",
+        Message=f"{servicename} returned status {status}",
         MessageBody="Response: " + data.decode("latin-1"),
         URL=URL,
     )
+
+
+def check_cas(cfc: CheckFunctionInterface):
+    URL = "https://auth.diamond.ac.uk/cas"
+    servicename = "Authentication services"
+    return _check_service(URL, cfc.name, servicename)
+
+
+def check_dbserver(cfc: CheckFunctionInterface):
+    URL = "http://sci-serv3:2611/server-status"
+    servicename = "dbserver"
+    return _check_service(URL, cfc.name, servicename)
+
+
+def check_gitlab(cfc: CheckFunctionInterface):
+    URL = "https://gitlab.diamond.ac.uk"
+    servicename = "Gitlab"
+    return _check_service(URL, cfc.name, servicename)
+
+
+def check_jira(cfc: CheckFunctionInterface):
+    URL = "https://jira.diamond.ac.uk"
+    servicename = "JIRA"
+    return _check_service(URL, cfc.name, servicename)
+
+
+def check_synchweb(cfc: CheckFunctionInterface):
+    URL = "http://ispyb.diamond.ac.uk"
+    servicename = "Synchweb"
+    return _check_service(URL, cfc.name, servicename)
+
+
+def check_uas(cfc: CheckFunctionInterface):
+    URL = "https://uas.diamond.ac.uk/"
+    servicename = "UAS"
+    return _check_service(URL, cfc.name, servicename)

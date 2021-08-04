b429c594db0e60fa367dbe47ec250792dd44fbce
Daniel Hatton <daniel.hatton@diamond.ac.uk>
2021-08-04 22:25:37 +0100

    For now disable the EM initial model ISPyB insert as the messages aren't correct

--- src/dlstbx/services/ispybsvc_em.py
+++ src/dlstbx/services/ispybsvc_em.py
@@ -367,6 +367,9 @@ class EM_Mixin:
             return False
 
     def do_insert_cryoem_initial_model(self, parameters, message, **kwargs):
+        return {"success": True, "return_value": None}
+
+    def do_insert_cryoem_initial_model_full(self, parameters, message, **kwargs):
         if message is None:
             message = {}
         dcid = parameters("dcid")

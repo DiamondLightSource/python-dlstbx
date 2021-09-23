40de531c95205c6c6a93903e48c654e1d4165c7d
Richard Gildea <richard.gildea@diamond.ac.uk>
2021-09-23 10:39:37 +0100

    Incorporate activemq into new queue_monitor

--- src/dlstbx/cli/queue_monitor_rmq.py
+++ src/dlstbx/cli/queue_monitor_rmq.py
@@ -4,13 +4,14 @@
 #
 
 import argparse
+import getpass
 import json
 import logging
+import re
 import time
 import urllib.parse
 import urllib.request
 
-import numpy as np
 import pandas as pd
 
 logger = logging.getLogger("dlstbx.queue_monitor")
@@ -19,7 +20,7 @@ logger = logging.getLogger("dlstbx.queue_monitor")
 RABBITMQ_HOST = "rabbitmq1.diamond.ac.uk"
 
 
-def get_queue_stats() -> pd.DataFrame:
+def get_rabbitmq_stats() -> pd.DataFrame:
     password_mgr = urllib.request.HTTPPasswordMgrWithDefaultRealm()
     password_mgr.add_password(
         realm=None,
@@ -44,8 +45,108 @@ def get_queue_stats() -> pd.DataFrame:
         "message_stats.publish_details.rate",
         "message_stats.deliver_get",
         "message_stats.deliver_get_details.rate",
+        "name.prefix",  # custom field added below
+        "dtype",  # custom field added below
     ]
+
+    # If there have been no recently published or delivered messages then these fields
+    # might not be present
+    if "message_stats.publish" not in stats:
+        stats["message_stats.publish"] = 0
+        stats["message_stats.publish_details.rate"] = 0
+    if "message_stats.deliver_get" not in stats:
+        stats["message_stats.deliver_get"] = 0
+        stats["message_stats.deliver_get_details.rate"] = 0
+
+    stats["name.prefix"] = stats["name"].str.split(".", 1).str[0]
+    stats["dtype"] = "queue"  # RabbitMQ doesn't have the same queue/topic distinction
     return stats[fields]
+
+
+def get_activemq_queue_and_topic_info() -> pd.DataFrame:
+    attributes = [
+        "ConsumerCount",
+        "QueueSize",
+        "EnqueueCount",
+        "DequeueCount",
+        "InFlightCount",
+    ]
+    queues = jmx.org.apache.activemq(
+        type="Broker",
+        brokerName="localhost",
+        destinationType="Queue",
+        destinationName="*",
+    )
+    queue_info, topic_info = {}, {}
+    if "value" in queues:
+        for destination in queues["value"]:
+            dest = destination[destination.index("destinationName=") :]
+            dest = (dest.split(",")[0])[16:]
+            if dest.startswith("ActiveMQ.Advisory."):
+                continue
+            queue_info[dest] = queues["value"][destination]
+    else:
+        logger.warning("Could not obtain queue status via JMX.\n%r", queues)
+    topics = jmx.org.apache.activemq(
+        type="Broker",
+        brokerName="localhost",
+        destinationType="Topic",
+        destinationName="*",
+        attribute=",".join(attributes),
+    )
+    if "value" in topics:
+        for destination in topics["value"]:
+            dest = destination[destination.index("destinationName=") :]
+            dest = (dest.split(",")[0])[16:]
+            if dest.startswith("ActiveMQ.Advisory."):
+                continue
+            topic_info[dest] = topics["value"][destination]
+    else:
+        logger.warning("Could not obtain topic status via JMX.\n%r", topics)
+    return queue_info, topic_info
+
+
+AMQ2RMQ = {
+    "EnqueueCount": "message_stats.publish",
+    "ConsumerCount": "consumers",
+    "QueueSize": "messages_ready",
+    "InFlightCount": "messages_unacknowledged",
+    "DequeueCount": "message_stats.deliver_get",
+}
+
+
+def get_activemq_stats() -> pd.DataFrame:
+    stats = pd.DataFrame(
+        columns=[
+            "name",
+            "consumers",
+            "messages",
+            "messages_ready",
+            "messages_unacknowledged",
+            "message_stats.publish",
+            "message_stats.publish_details.rate",
+            "message_stats.deliver_get",
+            "message_stats.deliver_get_details.rate",
+        ]
+    )
+    for dtype, destinations in zip(
+        ("queue", "topic"), get_activemq_queue_and_topic_info()
+    ):
+        for dname, dinfo in destinations.items():
+            row = {AMQ2RMQ.get(k, k): dinfo[k] for k in AMQ2RMQ}
+            name = dname.replace("uk.ac.diamond.", "u.a.d.").replace("transient.", "t.")
+            name = re.sub(
+                "([0-9a-f]{8})-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}",
+                "\\1-(..)",
+                name,
+            )
+            row["name"] = name
+            row["name.prefix"] = name.split(".", 1)[0]
+            row["dtype"] = dtype
+            stats = stats.append(row, ignore_index=True)
+
+    stats["messages"] = stats["messages_ready"] + stats["messages_unacknowledged"]
+    return stats
 
 
 def print_stats(stats: pd.DataFrame) -> None:
@@ -64,19 +165,9 @@ def print_stats(stats: pd.DataFrame) -> None:
     # messages_unacknowledged - Number of messages delivered to clients but not yet acknowledged.messages
     # messages - Sum of ready and unacknowledged messages (queue depth).
 
-    # Convert rates to equivalent number of messages in last 5 seconds
-    stats["change_publish_rate"] = (
-        stats["message_stats.publish_details.rate"] * 5
-    ).apply(np.ceil)
-    stats["change_deliver_rate"] = (
-        stats["message_stats.deliver_get_details.rate"] * 5
-    ).apply(np.ceil)
-
-    stats = stats.fillna(0.0).astype(int, errors="ignore")
-    stats = stats[stats["messages"] > 0].set_index("name")
-    stats = stats.sort_index().sort_values(by="messages")
-    status = stats.to_dict(orient="index")
-    longest = stats.reset_index().astype(str).applymap(len).max()
+    all_stats = stats.fillna(0.0).astype(int, errors="ignore").set_index("name")
+    all_stats = all_stats.sort_index().sort_values(by="messages", ascending=False)
+    longest = all_stats.reset_index().astype(str).applymap(len).max()
 
     c_gray = "\x1b[30m"
     c_green = "\x1b[32m"
@@ -92,51 +183,59 @@ def print_stats(stats: pd.DataFrame) -> None:
         ">{colour[hold]}[ {filter_zero[messages_ready]:{longest[messages_ready]}} | {colour[listeners]}{filter_zero[consumers]:<{longest[consumers]}}{colour[hold]} | {colour[flight]}{filter_zero[messages_unacknowledged]:<{longest[messages_unacknowledged]}}{colour[hold]} ]"
         "{colour[output]}> {filter_zero[change_deliver_rate]:<{longest[change_deliver_rate]}}{colour[reset]}"
     )
-    #   line +=  " -- {0[relevance]}{colour[reset]}"
+    # line +=  " -- {0[relevance]}{colour[reset]}"
 
     print("\033[H\033[J", end="")
-    queue_sep = "{header}RabbitMQ status: {highlight}{queues}{header} queues containing {highlight}{messages}{header} messages{reset}".format(
-        messages=stats["messages_ready"].sum(),
-        queues=len(stats),
-        highlight=c_bold + c_yellow,
-        reset=c_reset,
-        header=c_reset + c_yellow,
-    )
-    print(queue_sep)
 
-    for qname in status.keys():
-        colour = {
-            "input": c_green if status[qname]["change_publish_rate"] else c_gray,
-            "hold": c_blue if status[qname]["messages_ready"] else c_gray,
-            "flight": c_blue
-            if status[qname]["messages_ready"]
-            or status[qname]["messages_unacknowledged"]
-            else c_gray,
-            "output": c_green if status[qname]["change_deliver_rate"] else c_gray,
-            "reset": c_reset,
-            "listeners": c_yellow if status[qname]["consumers"] else c_gray,
-            "namespace": c_magenta
-            # if status[qname]["shortdest.prefix"] == "zocdev"
-            # else "",
-        }
-        filter_zero = {
-            key: status[qname][key] if status[qname][key] > 0 else ""
-            for key in (
-                "change_deliver_rate",
-                "messages_unacknowledged",
-                "messages_ready",
-                "consumers",
-            )
-        }
-        print(
-            line.format(
-                status[qname],
-                qname=qname,
-                longest=longest,
-                colour=colour,
-                filter_zero=filter_zero,
-            )
+    for dtype in ("queue", "topic"):
+
+        stats = all_stats[all_stats["dtype"] == dtype]
+        queue_sep = "{header}RabbitMQ status: {highlight}{queues}{header} {dtype}s containing {highlight}{messages}{header} messages{reset}".format(
+            messages=stats["messages_ready"].sum(),
+            queues=len(stats),
+            highlight=c_bold + c_yellow,
+            reset=c_reset,
+            header=c_reset + c_yellow,
+            dtype=dtype,
         )
+        print(queue_sep)
+
+        stats = stats[stats["relevance"] > 0]
+        status = stats.to_dict(orient="index")
+
+        for qname in status.keys():
+            colour = {
+                "input": c_green if status[qname]["change_publish_rate"] else c_gray,
+                "hold": c_blue if status[qname]["messages_ready"] else c_gray,
+                "flight": c_blue
+                if status[qname]["messages_ready"]
+                or status[qname]["messages_unacknowledged"]
+                else c_gray,
+                "output": c_green if status[qname]["change_deliver_rate"] else c_gray,
+                "reset": c_reset,
+                "listeners": c_yellow if status[qname]["consumers"] else c_gray,
+                "namespace": c_magenta
+                if status[qname]["name.prefix"] == "zocdev"
+                else "",
+            }
+            filter_zero = {
+                key: status[qname][key] if status[qname][key] > 0 else ""
+                for key in (
+                    "change_deliver_rate",
+                    "messages_unacknowledged",
+                    "messages_ready",
+                    "consumers",
+                )
+            }
+            print(
+                line.format(
+                    status[qname],
+                    qname=qname,
+                    longest=longest,
+                    colour=colour,
+                    filter_zero=filter_zero,
+                )
+            )
 
     print(
         "\n{header}What do the numbers mean:{reset}".format(
@@ -153,6 +252,12 @@ def run():
     parser = argparse.ArgumentParser(usage="dlstbx.queue_monitor")
     parser.add_argument("-?", action="help", help=argparse.SUPPRESS)
     parser.add_argument(
+        "--rabbitmq",
+        action="store_true",
+        dest="rabbitmq",
+        help="Show stats for the RabbitMQ server",
+    )
+    parser.add_argument(
         "--test",
         action="store_true",
         dest="test",
@@ -167,10 +272,56 @@ def run():
 
     args = parser.parse_args()
 
+    previous_stats = None
+
+    if not args.rabbitmq:
+        import dlstbx.util.jmxstats
+
+        global jmx
+        if args.test:
+            jmx = dlstbx.util.jmxstats.JMXAPI(
+                "/dls/tmp/%s/zocdev-activemq/latest-credentials" % getpass.getuser()
+            )
+        else:
+            jmx = dlstbx.util.jmxstats.JMXAPI()
+
     try:
         while True:
-            stats = get_queue_stats()
+            if args.rabbitmq:
+                stats = get_rabbitmq_stats()
+            else:
+                stats = get_activemq_stats()
+
+            stats["relevance"] = (
+                stats["messages_ready"] + stats["messages_unacknowledged"]
+            )
+            if previous_stats is not None:
+                stats["change_publish_rate"] = (
+                    stats["message_stats.publish"]
+                    - previous_stats["message_stats.publish"]
+                )
+                stats["change_deliver_rate"] = (
+                    stats["message_stats.deliver_get"]
+                    - previous_stats["message_stats.deliver_get"]
+                )
+                stats["changehist_publish_rate"] = stats[
+                    "change_publish_rate"
+                ] - previous_stats.get("change_publish_rate", 0)
+                change_hist_publish_rate = stats[
+                    "change_publish_rate"
+                ] - previous_stats.get("change_publish_rate", 0)
+                stats["relevance"] = stats["relevance"].add(
+                    previous_stats["relevance"] // 2
+                    + previous_stats["change_publish_rate"].clip(lower=0)
+                    + previous_stats["change_deliver_rate"].clip(lower=0)
+                    + change_hist_publish_rate.clip(lower=0)
+                )
+            else:
+                stats["change_publish_rate"] = 0
+                stats["change_deliver_rate"] = 0
+                stats["changehist_publish_rate"] = 0
             print_stats(stats)
+            previous_stats = stats
             time.sleep(args.gather_interval)
     except KeyboardInterrupt:
         print("\x1b[0m")

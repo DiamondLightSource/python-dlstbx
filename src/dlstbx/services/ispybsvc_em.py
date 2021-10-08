8863c071251099582bafbc37648dfbf5735ea43e
Daniel Hatton <daniel.hatton@diamond.ac.uk>
2021-10-08 13:38:01 +0100

    Only use time stamp if you have it

--- src/dlstbx/services/ispybsvc_em.py
+++ src/dlstbx/services/ispybsvc_em.py
@@ -118,9 +118,10 @@ class EM_Mixin:
                 movie_params["dataCollectionId"] = full_parameters("dcid")
                 movie_params["movieNumber"] = full_parameters("image_number")
                 movie_params["movieFullPath"] = full_parameters("micrograph_full_path")
-                movie_params["createdTimeStamp"] = datetime.fromtimestamp(
-                    full_parameters("created_time_stamp")
-                ).strftime("%Y-%m-%d %H:%M:%S")
+                if full_parameters("created_time_stamp"):
+                    movie_params["createdTimeStamp"] = datetime.fromtimestamp(
+                        full_parameters("created_time_stamp")
+                    ).strftime("%Y-%m-%d %H:%M:%S")
                 movieid = self.ispyb.em_acquisition.insert_movie(
                     list(movie_params.values())
                 )

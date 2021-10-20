8ebd2d0707b7c95814c12910dbd59e77bf3c772b
Daniel Hatton <daniel.hatton@diamond.ac.uk>
2021-10-20 09:34:07 +0100

    Insert into zocalo database tables from em_usage

--- src/dlstbx/cli/em_usage.py
+++ src/dlstbx/cli/em_usage.py
@@ -1,5 +1,7 @@
 import datetime
+import json
 import pathlib
+from typing import Optional
 
 import ispyb.sqlalchemy
 import pandas as pd
@@ -7,6 +9,49 @@ import sqlalchemy
 from ispyb.sqlalchemy import VRun
 from relion import Project
 from relion.cli import project_timeline
+from relion.zocalo.alchemy import ClusterJobInfo, RelionJobInfo
+from sqlalchemy.dialects.mysql import insert
+
+
+def _get_sessionmaker():
+    try:
+        configuration = pathlib.Path(
+            "/dls_sw/apps/zocalo/secrets/sql-zocalo-profiling.json"
+        ).read_text()
+    except PermissionError:
+        configuration = pathlib.Path(
+            "/dls_sw/apps/zocalo/secrets/sql-zocalo-readonly.json"
+        ).read_text()
+    secret_ingredients = json.loads(configuration)
+    sqlalchemy_url = "mysql+mysqlconnector://{user}:{passwd}@{host}:{port}/{db}".format(
+        **secret_ingredients
+    )
+    _sessionmaker = sqlalchemy.orm.sessionmaker(
+        bind=sqlalchemy.create_engine(sqlalchemy_url, connect_args={"use_pure": True})
+    )
+    return _sessionmaker
+
+
+def _df_to_db(df: pd.DataFrame, appid: Optional[int] = None):
+    session_maker = _get_sessionmaker()
+    with session_maker() as session:
+        for index, row in df.iterrows():
+            insert_cmd = insert(ClusterJobInfo).values(
+                cluster="hamilton",
+                cluster_id=row["cluster_id"],
+                auto_proc_program_id=appid,
+                start_time=row["cluster_start_time"],
+                end_time=row["end_time"],
+            )
+            session.execute(insert_cmd)
+            insert_cmd = insert(RelionJobInfo).values(
+                cluster_id=row["cluster_id"],
+                relion_start_time=row["start_time"],
+                num_micrographs=row["num_mics"],
+                job_name=row["job"],
+            )
+            session.execute(insert_cmd)
+            session.commit()
 
 
 def run() -> None:
@@ -72,24 +117,24 @@ def run() -> None:
     }
     df_all = pd.DataFrame(job_info)
 
-    with db_session_maker() as db_session:
-        for m, sess_list in sessions.items():
-            for sess in sess_list:
-                processed_dir = (
-                    pathlib.Path("/dls")
-                    / m
-                    / "data"
-                    / str(last_year)
-                    / sess["session"]
-                    / "processed"
-                )
-                for autoproc_dir in processed_dir.glob("*/*"):
-                    if not autoproc_dir.is_symlink():
-                        df = project_timeline._get_dataframe(
-                            Project(autoproc_dir / "relion", cluster=True)
-                        )
-                        if not df.empty:
-                            df_all = pd.concat([df_all, df])
+    for m, sess_list in sessions.items():
+        for sess in sess_list:
+            processed_dir = (
+                pathlib.Path("/dls")
+                / m
+                / "data"
+                / str(last_year)
+                / sess["session"]
+                / "processed"
+            )
+            for autoproc_dir in processed_dir.glob("*/*"):
+                if not autoproc_dir.is_symlink():
+                    df = project_timeline._get_dataframe(
+                        Project(autoproc_dir / "relion", cluster=True)
+                    )
+                    if not df.empty:
+                        _df_to_db(df)
+                        df_all = pd.concat([df_all, df])
 
     df_all["total_time"] = df_all["total_time"].dt.total_seconds()
     print(

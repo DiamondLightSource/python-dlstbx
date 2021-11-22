a8822236a016ff295462656a8fec4310a336a85c
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2021-11-22 16:17:28 +0000

    Work in progress
    for RabbitMQ health checks

--- src/dlstbx/health_checks/rabbitmq.py
+++ src/dlstbx/health_checks/rabbitmq.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from datetime import datetime
 
 import zocalo.configuration
@@ -63,15 +65,99 @@ def check_rabbitmq_dlq(cfc: CheckFunctionInterface):
     return list(report_updates.values())
 
 
-def check_rabbitmq_health(cfc: CheckFunctionInterface):
+def check_rabbitmq_health(cfc: CheckFunctionInterface) -> list[Status]:
     zc = zocalo.configuration.from_file()
     zc.activate_environment("live")
-    rmq = RabbitMQAPI.from_zocalo_configuration(zc)
+    hosts = ["rabbitmq1", "rabbitmq2", "rabbitmq3"]
+    api = {
+        host: RabbitMQAPI(
+            url=f"https://{host}.diamond.ac.uk/api",
+            user=zc.rabbitmqapi["username"],
+            password=zc.rabbitmqapi["password"],
+        )
+        for host in hosts
+    }
 
     db_status = cfc.current_status
     check_prefix = cfc.name
     report_updates = {}
     now = f"{datetime.now():%Y-%m-%d %H:%M:%S}"
+
+    # 1. is every host up?
+    # 2. does every host think the entire cluster is up?
+    # 3. now check more stuff
+
+    nodes_seen_by = {}
+    for host in hosts:
+        try:
+            nodes_seen_by[host] = api[host].nodes()
+            from pprint import pprint
+
+            pprint(api[host].get("overview"))
+            exit(1)
+            pprint(nodes_seen_by[host])
+            report_updates[f"{check_prefix}.{host}.status"] = Status(
+                Source=f"{check_prefix}.{host}.status",
+                Level=REPORT.PASS,
+                Message=f"Node {host} is running",
+                URL=f"https://{host}.diamond.ac.uk/",
+            )
+        except Exception as e:
+            report_updates[f"{check_prefix}.{host}.status"] = Status(
+                Source=f"{check_prefix}.{host}.status",
+                Level=REPORT.ERROR,
+                Message=f"Node status API call on {host} resulted in {type(e).__name__}",
+                MessageBody=str(e),
+                URL=f"https://{host}.diamond.ac.uk/",
+            )
+    if not nodes_seen_by:
+        report_updates[check_prefix] = Status(
+            Source=check_prefix,
+            Level=REPORT.ERROR,
+            Message="RabbitMQ cluster is down",
+        )
+        return list(report_updates.values())
+
+    if (
+        len(
+            {
+                frozenset(nodeinfo.name for nodeinfo in seen_nodes)
+                for seen_nodes in nodes_seen_by.values()
+            }
+        )
+        != 1
+    ):
+        report_updates[check_prefix] = Status(
+            Source=check_prefix,
+            Level=REPORT.ERROR,
+            Message="RabbitMQ cluster is running with a net split",
+            MessageBody="\n".join(
+                f"{host} sees nodes: {', '.join(nodeinfo.name for nodeinfo in nodes_seen_by[host])}"
+                for host in sorted(nodes_seen_by)
+            ),
+        )
+    elif any(len(seen) < 2 for seen in nodes_seen_by.values()):
+        report_updates[check_prefix] = Status(
+            Source=check_prefix,
+            Level=REPORT.ERROR,
+            Message="RabbitMQ cluster is degraded",
+        )
+    elif len(nodes_seen_by) != len(hosts) or any(
+        len(seen) != len(hosts) for seen in nodes_seen_by.values()
+    ):
+        report_updates[check_prefix] = Status(
+            Source=check_prefix,
+            Level=REPORT.WARNING,
+            Message="RabbitMQ cluster is degraded",
+        )
+    else:
+        report_updates[check_prefix] = Status(
+            Source=check_prefix,
+            Level=REPORT.PASS,
+            Message="RabbitMQ cluster is healthy",
+        )
+
+    return list(report_updates.values())
 
     success, failures = rmq.health_checks
     for check, msg in failures.items():
@@ -82,6 +168,11 @@ def check_rabbitmq_health(cfc: CheckFunctionInterface):
             MessageBody=msg,
             URL=zc.rabbitmqapi["base_url"],
         )
+    from pprint import pprint
+
+    pprint(rmq.nodes())
+    pprint(success)
+    pprint(failures)
 
     for node in rmq.nodes():
         for alarm in {"disk_free_alarm", "mem_alarm"}:
@@ -121,4 +212,5 @@ def check_rabbitmq_health(cfc: CheckFunctionInterface):
                     URL=zc.rabbitmqapi["base_url"],
                 )
 
+    pprint(report_updates)
     return list(report_updates.values())

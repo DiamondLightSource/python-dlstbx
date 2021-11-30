3d0dd4d9e25f2e4dc342aaee5d5c4394b5767c1e
Abi Emery <abigail.emery@diamond.ac.uk>
2021-11-30 14:23:00 +0000

    Add logging levels to xrc latency

--- src/dlstbx/services/xray_centering.py
+++ src/dlstbx/services/xray_centering.py
@@ -1,5 +1,6 @@
 import dataclasses
 import json
+import logging
 import pathlib
 import threading
 import time
@@ -43,6 +44,8 @@ class Parameters(pydantic.BaseModel):
     output: pathlib.Path = None
     log: pathlib.Path = None
     results_symlink: str = None
+    latency_log_warning: float = 45
+    latency_log_error: float = 300
 
 
 class RecipeStep(pydantic.BaseModel):
@@ -282,8 +285,19 @@ class DLSXRayCentering(CommonService):
 
                 # Write latency log message
                 latency = time.time() - cd.last_image_seen_at
-                self.log.info(
+                if latency >= parameters.latency_log_error:
+                    message_level = logging.ERROR
+                elif (
+                    parameters.latency_log_warning
+                    <= latency
+                    < parameters.latency_log_error
+                ):
+                    message_level = logging.WARNING
+                else:
+                    message_level = logging.INFO
+                self.logger.log(
                     f"X-ray centering completed for DCID {parameters.dcid} with latency of {latency:.2f} seconds",
+                    level=message_level,
                     extra={"xray-centering-latency": latency},
                 )
 

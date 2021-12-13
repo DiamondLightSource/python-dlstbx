47d961ea1ce00c3fd05916a8ee568af19454b43a
Richard Gildea <richard.gildea@diamond.ac.uk>
2021-12-13 09:17:00 +0000

    Add missing specification.py

---
+++ src/dlstbx/mimas/specification.py
@@ -0,0 +1,106 @@
+from __future__ import annotations
+
+from abc import abstractmethod
+from dataclasses import dataclass
+from typing import Any
+
+import dlstbx.mimas
+
+
+class BaseSpecification:
+    @abstractmethod
+    def is_satisfied_by(self, candidate: Any) -> bool:
+        raise NotImplementedError()
+
+    def __and__(self, other: BaseSpecification) -> AndSpecification:
+        return AndSpecification(self, other)
+
+    def __or__(self, other: BaseSpecification) -> OrSpecification:
+        return OrSpecification(self, other)
+
+    def __invert__(self) -> InvertSpecification:
+        return InvertSpecification(self)
+
+
+@dataclass(frozen=True)
+class AndSpecification(BaseSpecification):
+    first: BaseSpecification
+    second: BaseSpecification
+
+    def is_satisfied_by(self, candidate: Any) -> bool:
+        return self.first.is_satisfied_by(candidate) and self.second.is_satisfied_by(
+            candidate
+        )
+
+
+@dataclass(frozen=True)
+class OrSpecification(BaseSpecification):
+    first: BaseSpecification
+    second: BaseSpecification
+
+    def is_satisfied_by(self, candidate: Any) -> bool:
+        return self.first.is_satisfied_by(candidate) or self.second.is_satisfied_by(
+            candidate
+        )
+
+
+@dataclass(frozen=True)
+class InvertSpecification(BaseSpecification):
+    subject: BaseSpecification
+
+    def is_satisfied_by(self, candidate: Any) -> bool:
+        return not self.subject.is_satisfied_by(candidate)
+
+
+class ScenarioSpecification(BaseSpecification):
+    @abstractmethod
+    def is_satisfied_by(self, candidate: dlstbx.mimas.MimasScenario) -> bool:
+        raise NotImplementedError()
+
+
+@dataclass(frozen=True)
+class BeamlineSpecification(ScenarioSpecification):
+    beamline: str
+
+    def is_satisfied_by(self, candidate: dlstbx.mimas.MimasScenario) -> bool:
+        return candidate.beamline == self.beamline
+
+
+@dataclass(frozen=True)
+class EventSpecification(ScenarioSpecification):
+    event: dlstbx.mimas.MimasEvent
+
+    def is_satisfied_by(self, candidate: dlstbx.mimas.MimasScenario) -> bool:
+        return candidate.event == self.event
+
+
+@dataclass(frozen=True)
+class DCClassSpecification(ScenarioSpecification):
+    dcclass: dlstbx.mimas.MimasDCClass
+
+    def is_satisfied_by(self, candidate: dlstbx.mimas.MimasScenario) -> bool:
+        return candidate.dcclass == self.dcclass
+
+
+@dataclass(frozen=True)
+class DetectorClassSpecification(ScenarioSpecification):
+    detectorclass: dlstbx.mimas.MimasDetectorClass
+
+    def is_satisfied_by(self, candidate: dlstbx.mimas.MimasScenario) -> bool:
+        return candidate.detectorclass == self.detectorclass
+
+
+from typing import Set
+
+
+@dataclass(frozen=True)
+class VisitSpecification(ScenarioSpecification):
+    visits: Set[str]
+
+    def is_satisfied_by(self, candidate: dlstbx.mimas.MimasScenario) -> bool:
+        return candidate.visit and candidate.visit.startswith(tuple(self.visits))
+
+
+# class HasSpaceGroupSpecification(ScenarioSpecification):
+#     def is_satisfied_by(self, candidate: dlstbx.mimas.MimasScenario) -> bool:
+#         return candidate.spacegroup is not None

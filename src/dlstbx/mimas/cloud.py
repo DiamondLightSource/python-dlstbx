bb6553aa8a8145c83a0388d0be4f4a3d59ceeeb4
Richard Gildea <richard.gildea@diamond.ac.uk>
2022-01-18 09:57:25 +0000

    Move mimas cloud logic to cloud sub-module

--- src/dlstbx/mimas/cloud.py
+++ src/dlstbx/mimas/cloud.py
@@ -3,13 +3,13 @@ from __future__ import annotations
 from typing import List
 
 from dlstbx import mimas
-from dlstbx.mimas.core import is_end, is_rotation, xia2_dials_absorption_params
+from dlstbx.mimas.core import is_end, is_rotation
 from dlstbx.mimas.specification import BeamlineSpecification, VisitSpecification
 
 CLOUD_VISITS = {
     "cm",
-    "nt28218",
-    "mx",
+    "lb" "mx",
+    "nt31175",
 }
 
 
@@ -17,7 +17,7 @@ is_cloud = (
     VisitSpecification(CLOUD_VISITS)
     & is_end
     & is_rotation
-    & BeamlineSpecification("i03")
+    & BeamlineSpecification("i04-1")
 )
 
 
@@ -26,19 +26,19 @@ def handle_cloud(
     scenario: mimas.MimasScenario,
 ) -> List[mimas.Invocation]:
     return [
-        # xia2-dials
-        mimas.MimasISPyBJobInvocation(
-            DCID=scenario.DCID,
-            autostart=True,
-            recipe="autoprocessing-xia2-dials-eiger-cloud",
-            source="automatic",
-            parameters=(
-                mimas.MimasISPyBParameter(
-                    key="resolution.cc_half_significance_level", value="0.1"
-                ),
-                *xia2_dials_absorption_params(scenario),
-            ),
-        ),
+        # # xia2-dials
+        # mimas.MimasISPyBJobInvocation(
+        #     DCID=scenario.DCID,
+        #     autostart=True,
+        #     recipe="autoprocessing-xia2-dials-eiger-cloud",
+        #     source="automatic",
+        #     parameters=(
+        #         mimas.MimasISPyBParameter(
+        #             key="resolution.cc_half_significance_level", value="0.1"
+        #         ),
+        #         *xia2_dials_absorption_params(scenario),
+        #     ),
+        # ),
         # xia2-3dii
         mimas.MimasISPyBJobInvocation(
             DCID=scenario.DCID,

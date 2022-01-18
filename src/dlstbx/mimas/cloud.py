f2a56025a00c499fdd293840f42909ae158e2f31
Richard Gildea <richard.gildea@diamond.ac.uk>
2022-01-18 12:04:42 +0000

    Support for dynamically controlling cloud recipes
    Read cloud configuration from zocalo.configuration storage plugin:
    
    - Visit and beamline specifications can be controlled by
      zocalo.mimas.cloud.visit_pattern and zocalo.mimas.cloud.beamlines
      respectively.
    - Individual recipes can be switched on/off via
      zocalo.mimas.cloud.recipes

--- src/dlstbx/mimas/cloud.py
+++ src/dlstbx/mimas/cloud.py
@@ -2,61 +2,80 @@ from __future__ import annotations
 
 from typing import List
 
+import zocalo.configuration
+
 from dlstbx import mimas
-from dlstbx.mimas.core import is_end, is_rotation
+from dlstbx.mimas.core import (
+    is_end,
+    is_mx_beamline,
+    is_rotation,
+    is_vmxi,
+    xia2_dials_absorption_params,
+)
 from dlstbx.mimas.specification import BeamlineSpecification, VisitSpecification
 
-CLOUD_VISITS = {
-    "cm",
-    "lb",
-    "mx",
-    "nt31175",
-}
 
-
-is_cloud = (
-    VisitSpecification(CLOUD_VISITS)
-    & is_end
-    & is_rotation
-    & BeamlineSpecification("i04-1")
-)
-
-
-@mimas.match_specification(is_cloud)
+@mimas.match_specification(is_rotation & is_end & is_mx_beamline & ~is_vmxi)
 def handle_cloud(
     scenario: mimas.MimasScenario,
 ) -> List[mimas.Invocation]:
-    return [
-        # # xia2-dials
-        # mimas.MimasISPyBJobInvocation(
-        #     DCID=scenario.DCID,
-        #     autostart=True,
-        #     recipe="autoprocessing-xia2-dials-eiger-cloud",
-        #     source="automatic",
-        #     parameters=(
-        #         mimas.MimasISPyBParameter(
-        #             key="resolution.cc_half_significance_level", value="0.1"
-        #         ),
-        #         *xia2_dials_absorption_params(scenario),
-        #     ),
-        # ),
-        # xia2-3dii
-        mimas.MimasISPyBJobInvocation(
-            DCID=scenario.DCID,
-            autostart=True,
-            recipe="autoprocessing-xia2-3dii-eiger-cloud",
-            source="automatic",
-            parameters=(
-                mimas.MimasISPyBParameter(
-                    key="resolution.cc_half_significance_level", value="0.1"
-                ),
-            ),
-        ),
-        # autoPROC
-        mimas.MimasISPyBJobInvocation(
-            DCID=scenario.DCID,
-            autostart=True,
-            recipe="autoprocessing-autoPROC-eiger-cloud",
-            source="automatic",
-        ),
-    ]
+
+    tasks: List[mimas.Invocation] = []
+
+    zc = zocalo.configuration.from_file()
+    zc.activate()
+
+    if not zc.storage:
+        return tasks
+
+    visit_pattern = set(zc.storage.get("zocalo.mimas.cloud.visit_pattern", []))
+    cloud_beamlines = set(zc.storage.get("zocalo.mimas.cloud.beamlines", []))
+    cloud_recipes = set(zc.storage.get("zocalo.mimas.cloud.recipes", []))
+
+    cloud_spec = VisitSpecification(visit_pattern) & BeamlineSpecification(
+        beamlines=cloud_beamlines
+    )
+
+    if cloud_spec.is_satisfied_by(scenario):
+        if "autoprocessing-xia2-dials-eiger-cloud" in cloud_recipes:
+            tasks.append(
+                mimas.MimasISPyBJobInvocation(
+                    DCID=scenario.DCID,
+                    autostart=True,
+                    recipe="autoprocessing-xia2-dials-eiger-cloud",
+                    source="automatic",
+                    parameters=(
+                        mimas.MimasISPyBParameter(
+                            key="resolution.cc_half_significance_level", value="0.1"
+                        ),
+                        *xia2_dials_absorption_params(scenario),
+                    ),
+                )
+            )
+
+        if "autoprocessing-xia2-3dii-eiger-cloud" in cloud_recipes:
+            tasks.append(
+                mimas.MimasISPyBJobInvocation(
+                    DCID=scenario.DCID,
+                    autostart=True,
+                    recipe="autoprocessing-xia2-3dii-eiger-cloud",
+                    source="automatic",
+                    parameters=(
+                        mimas.MimasISPyBParameter(
+                            key="resolution.cc_half_significance_level", value="0.1"
+                        ),
+                    ),
+                )
+            )
+
+        if "autoprocessing-autoPROC-eiger-cloud" in cloud_recipes:
+            tasks.append(
+                mimas.MimasISPyBJobInvocation(
+                    DCID=scenario.DCID,
+                    autostart=True,
+                    recipe="autoprocessing-autoPROC-eiger-cloud",
+                    source="automatic",
+                )
+            )
+
+    return tasks

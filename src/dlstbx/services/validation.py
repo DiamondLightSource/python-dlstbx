584b1399e500668c101f84de843c6e45bcb02f2c
Richard Gildea <richard.gildea@diamond.ac.uk>
2022-01-19 10:57:34 +0000

    Skip i04-1 wavelength check for dc_sim

--- src/dlstbx/services/validation.py
+++ src/dlstbx/services/validation.py
@@ -130,7 +130,11 @@ class DLSValidation(CommonService):
             return fail("wavelength not set in image header")
 
         if output.get("beamline") == "i04-1":
-            if wavelength < 0.9100 or wavelength > 0.9300:
+            if output.get("dc_comments", "").startswith("Simulated datacollection"):
+                self.log.debug(
+                    "Skipping i04-1 wavelength validation for simulated data collection"
+                )
+            elif wavelength < 0.9100 or wavelength > 0.9300:
                 return fail(
                     f"Image wavelength {wavelength} outside of allowed range for I04-1 (0.9100-0.9300)"
                 )

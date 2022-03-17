6f55b2d4a24289ead7ba24fd79f786006e738a41
Richard Gildea <rjgildea@users.noreply.github.com>
2022-03-17 17:34:55 +0000

    Remove deprecated cli (#153)
    These have been mostly been moved to zocalo or ispyb
    
    Co-authored-by: Markus Gerstel <markus.gerstel@diamond.ac.uk>

--- src/dlstbx/cli/queue_drain.py
+++
@@ -1,8 +0,0 @@
-from __future__ import annotations
-
-import zocalo.cli.queue_drain
-
-
-def run():
-    print("\ndlstbx.queue_drain is deprecated. Use 'zocalo.queue_drain' instead\n")
-    zocalo.cli.queue_drain.run()
--- src/dlstbx/cli/shutdown.py
+++
@@ -1,8 +0,0 @@
-from __future__ import annotations
-
-import zocalo.cli.shutdown
-
-
-def run():
-    print("\ndlstbx.shutdown is deprecated. Use 'zocalo.shutdown' instead\n")
-    zocalo.cli.shutdown.run()

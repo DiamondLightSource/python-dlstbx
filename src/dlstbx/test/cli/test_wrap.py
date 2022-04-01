67814216386fa63f7b1e2c26e2b4d6f9ab3cb221
Richard Gildea <richard.gildea@diamond.ac.uk>
2022-04-01 13:40:49 +0100

    Use OfflineTransport

--- src/dlstbx/test/cli/test_wrap.py
+++ src/dlstbx/test/cli/test_wrap.py
@@ -4,5 +4,7 @@ import procrunner
 
 
 def test_dummy_wrapper(caplog):
-    result = procrunner.run(["dlstbx.wrap", "--wrap", "dummy"], timeout=65)
+    result = procrunner.run(
+        ["dlstbx.wrap", "--wrap", "dummy", "-t", "OfflineTransport"], timeout=65
+    )
     assert b"successfully finished processing" in result.stderr

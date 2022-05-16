40e55dc86f8583b31abdb8d42140263307fd6910
Richard Gildea <richard.gildea@diamond.ac.uk>
2022-05-16 10:15:53 +0100

    Switch mimas test to gridscan dcid
    So that it doesn't need updating every time we change the cloud settings

--- src/dlstbx/system_test/mimas.py
+++ src/dlstbx/system_test/mimas.py
@@ -10,7 +10,7 @@ imagepath = "/dls/mx/data/nt24686/nt24686-7/VMXi-AB5081/well_113/images/"
 class MimasService(CommonSystemTest):
     """Tests for the per-image-analysis service."""
 
-    def test_i03_rotation_end(self):
+    def test_i03_gridscan_end(self):
         """Find all files referenced in an example nexus dataset."""
 
         recipe = {
@@ -19,19 +19,17 @@ class MimasService(CommonSystemTest):
                 "service": "MIMAS Business Logic",
                 "queue": "mimas",
                 "parameters": {
-                    "dcid": "8114255",
+                    "dcid": "8257178",
                     "event": "end",
                     "beamline": "i03",
                     "visit": "cm31105-2",
                     "detectorclass": "eiger",
-                    "dc_class": {"grid": False, "screen": False, "rotation": True},
-                    "ispyb_images": "",
-                    "preferred_processing": "xia2/DIALS",
+                    "dc_class": {
+                        "grid": True,
+                        "screen": False,
+                        "rotation": False,
+                    },
                     "run_status": "DataCollection Successful",
-                    "space_group": "",
-                    "sweep_list": [[8114255, 1, 450]],
-                    "unit_cell": False,
-                    "diffraction_plan_info": None,
                 },
                 "output": {"dispatcher": 2, "ispyb": 3},
             },
@@ -59,94 +57,30 @@ class MimasService(CommonSystemTest):
             headers={"workflows-recipe": True},
         )
 
-        ispyb_job_template = {
-            "DCID": 8114255,
-            "source": "automatic",
-            "comment": "",
-            "displayname": "",
-            "parameters": [],
-            "sweeps": [],
-            "triggervariables": [],
-        }
-
         expected_recipe_pointers_and_payloads = (
-            (2, {"recipes": ["archive-nexus"], "parameters": {"ispyb_dcid": 8114255}}),
             (
                 2,
                 {
                     "recipes": ["generate-crystal-thumbnails"],
-                    "parameters": {"ispyb_dcid": 8114255},
+                    "parameters": {"ispyb_dcid": 8257178},
                 },
+            ),
+            (
+                2,
+                {"recipes": ["archive-nexus"], "parameters": {"ispyb_dcid": 8257178}},
             ),
             (
                 2,
                 {
                     "recipes": ["generate-diffraction-preview"],
-                    "parameters": {"ispyb_dcid": 8114255},
+                    "parameters": {"ispyb_dcid": 8257178},
                 },
             ),
             (
                 2,
                 {
-                    "recipes": ["per-image-analysis-rotation-swmr"],
-                    "parameters": {"ispyb_dcid": 8114255},
-                },
-            ),
-            (
-                2,
-                {
-                    "recipes": ["processing-rlv-eiger"],
-                    "parameters": {"ispyb_dcid": 8114255},
-                },
-            ),
-            (
-                3,
-                ispyb_job_template
-                | {"autostart": True, "recipe": "autoprocessing-fast-dp-eiger"},
-            ),
-            (
-                3,
-                ispyb_job_template
-                | {
-                    "autostart": True,
-                    "recipe": "autoprocessing-xia2-dials-eiger-cluster",
-                    "parameters": [
-                        {
-                            "key": "resolution.cc_half_significance_level",
-                            "value": "0.1",
-                        },
-                        {"key": "absorption_level", "value": "medium"},
-                    ],
-                },
-            ),
-            (
-                3,
-                ispyb_job_template
-                | {
-                    "autostart": False,
-                    "recipe": "autoprocessing-xia2-3dii-eiger-cluster",
-                    "parameters": [
-                        {"key": "resolution.cc_half_significance_level", "value": "0.1"}
-                    ],
-                },
-            ),
-            (
-                3,
-                ispyb_job_template
-                | {
-                    "autostart": False,
-                    "recipe": "autoprocessing-autoPROC-eiger-cluster",
-                },
-            ),
-            (
-                3,
-                ispyb_job_template
-                | {
-                    "autostart": True,
-                    "recipe": "autoprocessing-xia2-3dii-eiger-cloud",
-                    "parameters": [
-                        {"key": "resolution.cc_half_significance_level", "value": "0.1"}
-                    ],
+                    "recipes": ["per-image-analysis-gridscan-i03"],
+                    "parameters": {"ispyb_dcid": 8257178},
                 },
             ),
         )

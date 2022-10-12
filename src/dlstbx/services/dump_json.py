95ef2bc24a0902b82ef79d427418413102662cf8
Richard Gildea <richard.gildea@diamond.ac.uk>
2022-10-12 15:25:43 +0100

    Simplify and generalise
    Now the service will take arbitrary message dicts and write them
    to a defined output_filename. Messages will be optionally filtered
    according to the keys defined in by the include and exclude recipe
    parameters.

--- src/dlstbx/services/dump_json.py
+++ src/dlstbx/services/dump_json.py
@@ -4,34 +4,8 @@ import json
 import threading
 from pathlib import Path
 
-import pydantic
 import workflows.recipe
 from workflows.services.common_service import CommonService
-
-from dlstbx.services.indexer import IndexedLatticeResult
-from dlstbx.util import ChainMapWithReplacement
-
-
-def lookup_command(command, refclass):
-    return getattr(refclass, "do_" + command, None)
-
-
-class PerImageAnalysisPayload(pydantic.BaseModel):
-    output_filename: Path
-    file_number: pydantic.PositiveInt = pydantic.Field(alias="file-number")
-    n_spots_total: pydantic.NonNegativeInt
-    n_spots_4A: pydantic.NonNegativeInt
-    total_intensity: float
-
-    class Config:
-        allow_population_by_field_name = True
-
-
-class IndexingPayload(pydantic.BaseModel):
-    output_filename: Path
-    file_number: pydantic.NonNegativeInt = pydantic.Field(alias="file-number")
-    lattices: list[IndexedLatticeResult]
-    n_unindexed: pydantic.NonNegativeInt
 
 
 class JSON(CommonService):
@@ -56,92 +30,57 @@ class JSON(CommonService):
             prefetch_count=100,
         )
         self._lock = threading.Lock()
-        self._data = {}
+        self._data: dict[Path, list[tuple(dict, dict)]] = {}
 
     def receive_msg(
         self, rw: workflows.recipe.RecipeWrapper, header: dict, message: dict
     ):
-        command = rw.recipe_step["parameters"].get("command")
-        if not command:
-            self.log.error("Received message is not a valid json command")
+        output_filename = rw.recipe_step["parameters"]["output_filename"]
+        if not output_filename:
+            self.log.error("Received message contains no output_filename")
             rw.transport.nack(header)
             return
-        command_function = lookup_command(command, self)
-        if not command_function:
-            self.log.error("Received unknown json command (%s)", command)
-            rw.transport.nack(header)
-            return
+        output_filename = Path(output_filename)
+        exclude = set(rw.recipe_step["parameters"].get("exclude", []))
+        include = set(rw.recipe_step["parameters"].get("include", []))
+        filtered_message = message
+        if include:
+            filtered_message = {
+                k: v for k, v in filtered_message.items() if k in include
+            }
+        if exclude:
+            filtered_message = {
+                k: v for k, v in filtered_message.items() if k not in exclude
+            }
 
         with self._lock:
-            self._data.setdefault(command, {})
-            parameters = ChainMapWithReplacement(
-                message if isinstance(message, dict) else {},
-                rw.recipe_step["parameters"],
-                substitutions=rw.environment,
-            )
-            output_filename = parameters["output_filename"]
-            self._data[command].setdefault(output_filename, [])
-            self._data[command][output_filename].append(
-                (
-                    header,
-                    parameters,
-                )
-            )
+            self._data.setdefault(output_filename, [])
+            self._data[output_filename].append((header, filtered_message))
 
     def process_messages(self):
         with self._lock:
-            for command, command_data in self._data.items():
-                if not command_data:
-                    continue
-                self.log.debug("Running json call %s", command)
-                command_function = lookup_command(command, self)
-                for output_filename, grouped_data in command_data.items():
-                    try:
-                        command_function(
-                            output_filename=output_filename,
-                            results=[params for _, params in grouped_data],
-                        )
-                    except Exception as e:
-                        self.log.error(
-                            f"Uncaught exception {e!r} in json function {command!r}",
-                            exc_info=True,
-                        )
-                        for header, _ in grouped_data:
-                            self.transport.nack(header)
-                        return
-                    else:
-                        for header, _ in grouped_data:
-                            self.transport.ack(header)
+            for output_filename in list(self._data):
+                grouped_data = self._data[output_filename]
+                self.log.info(
+                    f"Writing {len(grouped_data)} messages to {output_filename}"
+                )
+                try:
+                    output_filename = Path(output_filename)
+                    output_filename.parent.mkdir(exist_ok=True, parents=True)
+                    with output_filename.open(mode="a") as fh:
+                        for _, message in grouped_data:
+                            fh.write(json.dumps(message) + "\n")
+                except Exception as e:
+                    self.log.error(
+                        f"Uncaught exception {e!r} writing messages to {output_filename}",
+                        exc_info=True,
+                    )
+                    for header, _ in grouped_data:
+                        self.transport.nack(header)
+                    return
+                else:
+                    for header, _ in grouped_data:
+                        self.transport.ack(header)
 
-                    # delete this data now we've processed it
-                    self._data[command][output_filename] = []
-
-                del self._data[command][output_filename]
-
-    @pydantic.validate_arguments(config=dict(arbitrary_types_allowed=True))
-    def do_store_per_image_analysis_result(
-        self,
-        *,
-        output_filename: Path,
-        results: list[PerImageAnalysisPayload],
-        **kwargs,
-    ):
-        self.log.debug(f"{results=}")
-        output_filename.parent.mkdir(exist_ok=True, parents=True)
-        with output_filename.open(mode="a") as fh:
-            for result in results:
-                fh.write(json.dumps(result.dict(exclude={"output_filename"})) + "\n")
-
-    @pydantic.validate_arguments(config=dict(arbitrary_types_allowed=True))
-    def do_store_indexing_result(
-        self,
-        *,
-        output_filename: Path,
-        results: list[IndexingPayload],
-        **kwargs,
-    ):
-        self.log.debug(f"{results=}")
-        output_filename.parent.mkdir(exist_ok=True, parents=True)
-        with output_filename.open(mode="a") as fh:
-            for result in results:
-                fh.write(json.dumps(result.dict(exclude={"output_filename"})) + "\n")
+                # delete this data now we've processed it
+                del self._data[output_filename]

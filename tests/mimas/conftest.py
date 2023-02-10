1fd048fd0e38d6d4076e26398815c00ac4c17541
Richard Gildea <richard.gildea@diamond.ac.uk>
2023-02-10 15:00:12 +0000

    Missed conftest.py

---
+++ tests/mimas/conftest.py
@@ -0,0 +1,23 @@
+from __future__ import annotations
+
+from unittest import mock
+
+import pytest
+import zocalo.configuration
+
+from dlstbx import mimas
+
+
+@pytest.fixture
+def get_zocalo_commands():
+    def get_zocalo_commands_(scenario):
+        mock_zc = mock.MagicMock(zocalo.configuration.Configuration, autospec=True)
+
+        commands = set()
+        actions = mimas.handle_scenario(scenario, zc=mock_zc)
+        for a in actions:
+            mimas.validate(a)
+            commands.add(mimas.zocalo_command_line(a).strip())
+        return commands
+
+    return get_zocalo_commands_

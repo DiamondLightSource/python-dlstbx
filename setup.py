cf65d7ec4ee4498c1e740771e9040231381c6efd
Phil Blowey <philip.blowey@diamond.ac.uk>
2024-03-20 15:28:24 +0000

    Create initial wrapper with basic function

--- setup.py
+++ setup.py
@@ -71,6 +71,7 @@ known_wrappers = [  # please keep alphabetically sorted
     "fast_dp = dlstbx.wrapper.fast_dp:FastDPWrapper",
     "fast_ep = dlstbx.wrapper.fast_ep:FastEPWrapper",
     "fast_rdp = dlstbx.wrapper.fast_rdp:FastRDPWrapper",
+    "metal_id = dlstbx.wrapper.metal_id:MetalIdWrapper",
     "mosflm_strategy = dlstbx.wrapper.mosflm_strategy:MosflmStrategyWrapper",
     "mr_predict = dlstbx.wrapper.mr_predict:MRPredictWrapper",
     "mrbump = dlstbx.wrapper.mrbump:MrBUMPWrapper",

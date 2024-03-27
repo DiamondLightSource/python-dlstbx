4414d40eae4aeea015ad4a7fcc675a983212c28a
Irakli Sikharulidze <irakli.sikharulidze@diamond.ac.uk>
2024-03-27 15:32:45 +0000

    Add summary statistics for slurm and iris jobs

--- src/dlstbx/services/cluster_monitor.py
+++ src/dlstbx/services/cluster_monitor.py
@@ -1,10 +1,14 @@
 from __future__ import annotations
 
 import collections
+import itertools
 import logging
 import time
+from pprint import pformat
 
+import requests
 from workflows.services.common_service import CommonService
+from zocalo.util import slurm
 
 import dlstbx.util.cluster
 
@@ -56,6 +60,14 @@ class DLSClusterMonitor(CommonService):
         # Statistics go with debug level to a separate logger so they can be
         # filtered by log monitors.
         self.cluster_statistics = dlstbx.util.cluster.ClusterStatistics()
+
+        self.slurm_api: slurm.SlurmRestApi = (
+            slurm.SlurmRestApi.from_zocalo_configuration(self.config)
+        )
+        self.iris_api: slurm.SlurmRestApi = (
+            slurm.SlurmRestApi.from_zocalo_configuration(self.config, cluster="iris")
+        )
+
         self.stats_log = logging.getLogger(self._logger_name + ".stats")
         self.stats_log.setLevel(logging.DEBUG)
         self._register_idle(30, self.update_cluster_statistics)
@@ -87,6 +99,18 @@ class DLSClusterMonitor(CommonService):
                 self.calculate_cluster_statistics(
                     joblist, queuelist, downstream_name, timestamp
                 )
+        for scheduler, cluster_api in [
+            ("slurm", self.slurm_api),
+            ("iris", self.iris_api),
+        ]:
+            try:
+                self.log.debug(f"Gathering {scheduler} job statistics")
+                timestamp = time.time()
+                job_info_resp: slurm.models.OpenapiJobInfoResp = cluster_api.get_jobs()
+            except requests.HTTPError as e:
+                self.log.error(f"Failed Slurm API call: {e}\n" f"{e.response.text}")
+            else:
+                self.calculate_slurm_statistics(scheduler, job_info_resp, timestamp)
 
     def calculate_cluster_statistics(self, joblist, queuelist, cluster, timestamp):
         self.log.debug("Processing %s cluster statistics", cluster)
@@ -223,7 +247,21 @@ class DLSClusterMonitor(CommonService):
             corestats, description="utilization", cluster=cluster, timestamp=timestamp
         )
 
-    def report_statistic(self, data, **kwargs):
+    def calculate_slurm_statistics(
+        self, scheduler, response: slurm.models.OpenapiJobInfoResp, timestamp
+    ):
+        self.log.debug(f"Processing {scheduler} job states")
+        jobs_states = itertools.chain(
+            *[job.job_state for job in dict(response.jobs).get("__root__", [])]
+        )
+        data: dict[str, int] = dict(
+            collections.Counter([js.name for js in jobs_states])
+        )
+        self.report_statistic(
+            data, cluster=scheduler, description="job-states", timestamp=timestamp
+        )
+
+    def report_statistic(self, data: dict[str, int], **kwargs):
         data_pack = {
             "statistic-group": "cluster",
             "statistic": kwargs["description"],
@@ -231,5 +269,6 @@ class DLSClusterMonitor(CommonService):
             "statistic-timestamp": kwargs["timestamp"],
         }
         data_pack.update(data)
+        self.log.debug(pformat(data_pack))
         self._transport.broadcast("transient.statistics.cluster", data_pack)
         self._transport.send("statistics.cluster", data_pack, persistent=False)

8de73edf84a95cfc4c8ad53d5541c1d1fdffe691
Phil Blowey <philip.blowey@diamond.ac.uk>
2024-04-03 17:04:29 +0100

    Add pointless step to scaleit method

--- src/dlstbx/wrapper/scaleit.py
+++ src/dlstbx/wrapper/scaleit.py
@@ -1,10 +1,14 @@
 # Wrapper for method of using ccp4 scaleit to adjust 2 .mtz file to the same overall scale.
 from __future__ import annotations
 
+import os
 import subprocess
 from pathlib import Path
 
+# from iotbx import mtz
 from dlstbx.wrapper import Wrapper
+
+# from argparse import Namespace
 
 
 class ScaleitWrapper(Wrapper):
@@ -19,31 +23,65 @@ class ScaleitWrapper(Wrapper):
 
         # Get parameters from the recipe file
         self.params = self.recwrap.recipe_step["job_parameters"]
-        # Timestamp for creating unique file output for prototyping - remove when recipe file is sorted.
-        # timestamp = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
-        # self.working_directory = (
-        #     Path(self.params["working_directory"]) / timestamp
-        # )  # Timestamp added for prototyping to create unique directory
-        # self.results_directory = (
-        #     Path(self.params["results_directory"]) / timestamp
-        # )  # Timestamp added for prototyping to create unique directory
+
         self.working_directory = Path(
             self.recwrap.recipe_step["parameters"]["workingdir"]
         )
         # Make the directories if they don't already exist
         self.working_directory.mkdir(parents=True, exist_ok=True)
         self.log.info(f"Here are the params {self.params}")
-        mtz = self.params["scaleit"].get("data", [])
-        if not mtz:
+        mtz_files = self.params["scaleit"].get("data", [])
+        if not mtz_files:
             self.log.error("Could not identify on what data to run")
             return False
-        if len(mtz) != 2:
+        if len(mtz_files) != 2:
             self.log.error(
-                f"Exactly two data files need to be provided, {len(mtz)} files were given"
+                f"Exactly two data files need to be provided, {len(mtz_files)} files were given"
             )
 
-        mtz_nat = mtz[0]
-        mtz_der = mtz[1]
+        if pdb := self.params["scaleit"].get("pdb"):
+            mtz_reindexed = []
+            print("HERE")
+            # Reindex the mtz files to ensure that they have the same symmetry as the pdb file
+            for mtz_file in mtz_files:
+                print(mtz_file)
+                _filename = os.path.basename(mtz_file)
+                _filename = os.path.splitext(_filename)[0]
+                hklout = self.working_directory / f"{_filename}_reindexed.mtz"
+                pointless_command = [
+                    f"pointless hklin {mtz_file} hklout {hklout} xyzin {pdb}"
+                ]
+                print(pointless_command)
+                result = subprocess.run(
+                    pointless_command,
+                    shell=True,
+                    cwd=self.working_directory,
+                    capture_output=True,
+                    text=True,
+                )
+                with open(
+                    self.working_directory / f"reindex_{_filename}.log", "w"
+                ) as log_file:
+                    log_file.write(result.stdout)
+                with open(
+                    self.working_directory / f"reindex_{_filename}_error.log", "w"
+                ) as log_file:
+                    log_file.write(result.stderr)
+
+                if "Incompatible symmetries" in result.stderr:
+                    self.log.error(
+                        f"{mtz_file} has incompatible symmetry to ref file {pdb}"
+                    )
+                    raise Exception(
+                        f"{mtz_file} has incompatible symmetry to ref file {pdb}"
+                    )
+                mtz_reindexed += hklout
+            mtz_files = mtz_reindexed
+
+        mtz_nat = mtz_files[0]
+        mtz_der = mtz_files[1]
+
+        # Get the F and SIGF data column headings
 
         # Add the F and SIGF data from one file to the other with cad
         mtz_combi = self.working_directory / "combined.mtz"
@@ -51,10 +89,7 @@ class ScaleitWrapper(Wrapper):
             f"cad hklin1 {mtz_nat} hklin2 {mtz_der} hklout {mtz_combi} <<END-CAD\n"
             + "TITLE Add data for scaling\n"
             + "LABIN FILE 1 E1=F E2=SIGF\n"
-            + "LABIN FILE 2 -\n"
-            + "E1=FreeR_flag E2=IMEAN E3=SIGIMEAN E4=N E5=I(+) E6=SIGI(+) E7=I(-) -\n"
-            + "E8=SIGI(-) E9=N(+) E10=N(-) E11=F E12=SIGF E13=F(+) E14=SIGF(+) -\n"
-            + "E15=F(-) E16=SIGF(-) E17=DANO E18=SIGDANO\n"
+            + "LABIN FILE 2 ALL\n"
             + "LABOUT FILE 1 E1=Fscale E2 = SIGFscale\n"
             + "SYSAB_KEEP\n"
             + "END\n"

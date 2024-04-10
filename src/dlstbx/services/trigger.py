a858ba23d741350ada4fea2a8c086bb56b2e8601
Irakli Sikharulidze <irakli.sikharulidze@diamond.ac.uk>
2024-04-10 10:54:41 +0100

    Run postprocessing jobs at DLS

--- src/dlstbx/services/trigger.py
+++ src/dlstbx/services/trigger.py
@@ -702,7 +702,7 @@ class DLSTrigger(CommonService):
         jp["comments"] = parameters.comment
         jp["datacollectionid"] = dcid
         jp["display_name"] = "fast_ep"
-        jp["recipe"] = "postprocessing-fast-ep"
+        jp["recipe"] = "postprocessing-fast-ep-cluster"
         jobid = self.ispyb.mx_processing.upsert_job(list(jp.values()))
         self.log.debug(f"fast_ep trigger: generated JobID {jobid}")
 
@@ -768,7 +768,7 @@ class DLSTrigger(CommonService):
             jp["comments"] = parameters.comment
             jp["datacollectionid"] = dcid
             jp["display_name"] = "MrBUMP"
-            jp["recipe"] = "postprocessing-mrbump"
+            jp["recipe"] = "postprocessing-mrbump-cluster"
             jobid = self.ispyb.mx_processing.upsert_job(list(jp.values()))
             jobids.append(jobid)
             self.log.debug(f"mrbump trigger: generated JobID {jobid}")
@@ -996,7 +996,7 @@ class DLSTrigger(CommonService):
         jp["comments"] = parameters.comment
         jp["datacollectionid"] = dcid
         jp["display_name"] = "big_ep"
-        jp["recipe"] = "postprocessing-big-ep"
+        jp["recipe"] = "postprocessing-big-ep-dls"
         jobid = self.ispyb.mx_processing.upsert_job(list(jp.values()))
         self.log.debug(f"big_ep trigger: generated JobID {jobid}")
 

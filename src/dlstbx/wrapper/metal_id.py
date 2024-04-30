f429a95ca324e6428e00b42e5bd39aff29da9da0
Phil Blowey <philip.blowey@diamond.ac.uk>
2024-04-30 16:09:00 +0100

    Check pdb files for similarity

--- src/dlstbx/wrapper/metal_id.py
+++ src/dlstbx/wrapper/metal_id.py
@@ -6,6 +6,8 @@ import pathlib
 import re
 import subprocess
 
+from iotbx import pdb
+
 from dlstbx.wrapper import Wrapper
 
 
@@ -44,38 +46,134 @@ class MetalIdWrapper(Wrapper):
 
     _logger_name = "dlstbx.wrap.metal_id"
 
+    def are_pdbs_similar(self, file_1, file_2, tolerances):
+        """
+        Determine if two pdb files have the same crystal symmetry, the same number
+        and type of atoms and sufficiently similar unit cell and atomic coordinates
+        within the defined tolerances
+        """
+
+        def read_pdb(file):
+            """
+            Read a pdb file to get crystal symmetry, atom names and atom coordinates
+            """
+            pdb_obj = pdb.input(file)
+            sym = pdb_obj.crystal_symmetry()
+            atoms = pdb_obj.atoms()
+            atom_names = atoms.extract_name()
+            list_atoms = [atom for atom in atom_names]
+            atom_coords = atoms.extract_xyz()
+            list_coords = [coords for coords in atom_coords]
+            return sym, list_atoms, list_coords
+
+        # Read pdb files
+        sym_1, atoms_1, coords_1 = read_pdb(file_1)
+        sym_2, atoms_2, coords_2 = read_pdb(file_2)
+
+        # Get tolerances
+
+        # Compare symmetry
+        is_similar_sym = sym_1.is_similar_symmetry(
+            sym_2,
+            relative_length_tolerance=tolerances["rel_cell_length"],
+            absolute_angle_tolerance=tolerances["abs_cell_angle"],
+        )
+        if not is_similar_sym:
+            self.log.error("PDB file symmetries are too different")
+            return False
+
+        # Compare atom type/number
+        if atoms_1 != atoms_2:
+            self.log.error("Different number or type of atoms in pdb files")
+
+        # Compare atom coordinates
+        combined_coords = zip(coords_1, coords_2)
+        for xyz_1, xyz_2 in combined_coords:
+            # Calculate the distance between xyz_1 and xyz_2
+            diff = abs(
+                (
+                    (xyz_1[0] - xyz_2[0]) ** 2
+                    + (xyz_1[1] - xyz_2[1]) ** 2
+                    + (xyz_1[2] - xyz_2[2]) ** 2
+                )
+                ** 0.5
+            )
+            if diff > tolerances["abs_coord_diff"]:
+                self.log.error(
+                    f"PDB atom coordinates have difference > tolerance ({tolerances['abs_coord_diff']} Å"
+                )
+                return False
+        return True
+
     def run(self):
         assert hasattr(self, "recwrap"), "No recipewrapper object found"
         self.log.debug(
             f"Running recipewrap file {self.recwrap.recipe_step['parameters']['recipewrapper']}"
         )
         # Get parameters from the recipe file
-        pha_above = pathlib.Path(
-            self.recwrap.recipe_step["job_parameters"]["anode_map_above"]
-        )
-        pha_below = pathlib.Path(
-            self.recwrap.recipe_step["job_parameters"]["anode_map_below"]
-        )
-        pdb = pathlib.Path(self.recwrap.recipe_step["job_parameters"]["pdb"])
+        params = self.recwrap.recipe_step["job_parameters"]
 
-        working_directory = pathlib.Path(
-            self.recwrap.recipe_step["job_parameters"]["working_directory"]
-        )
+        pha_above = pathlib.Path(params["anode_map_above"])
+        pha_below = pathlib.Path(params["anode_map_below"])
+        pdb_files = params["pdb"]
+
+        working_directory = pathlib.Path(params["working_directory"])
         working_directory.mkdir(parents=True, exist_ok=True)
 
-        results_directory = pathlib.Path(
-            self.recwrap.recipe_step["job_parameters"]["results_directory"]
-        )
+        results_directory = pathlib.Path(params["results_directory"])
         results_directory.mkdir(parents=True, exist_ok=True)
 
+        # Check file inputs
+        for file_type, file_path in [
+            ("AnoDe map above", pha_above),
+            ("AnoDe map below", pha_below),
+        ]:
+            if not os.path.isfile(file_path):
+                self.log.error(f"Could not find {file_type}, expected at: {file_path}")
+                return {"success": True}
+
+        # Check and handle pdb file(s)
+        if isinstance(pdb_files, str):
+            # Handles case where a single pdb file is given, not as a list
+            pdb_file = pathlib.Path(pdb_files)
+        elif len(pdb_files) > 2:
+            self.log.error(
+                f"Expected up to two pdb files, instead {len(pdb_files)} were given"
+            )
+        elif len(pdb_files) == 2:
+            self.log.info(
+                f"Checking pdb files for similarity. Files: {pdb_files[0]}, {pdb_files[1]}"
+            )
+            tolerances = params.get("pdb_comparison_tolerances")
+            if not tolerances:
+                self.log.warning(
+                    "PDB similarity tolerances not specified, using default values"
+                )
+                tolerances = {
+                    "rel_cell_length": 0.01,
+                    "abs_cell_angle": 1.0,
+                    "abs_coord_diff": 3.0,
+                }
+            pdbs_are_similar = self.are_pdbs_similar(
+                pdb_files[0], pdb_files[1], tolerances
+            )
+            if not pdbs_are_similar:
+                self.log.error("PDB files are not similar enough, not running metal_id")
+                return False
+            self.log.info("PDB files are similar enough, continuing with metal_id")
+            pdb_file = pathlib.Path(pdb_files[0])
+        else:
+            pdb_file = pathlib.Path(pdb_files[0])
+
         self.log.info("Making double difference map")
+        self.log.info(f"Using {pdb_file} as reference coordinates for map")
         map_out = working_directory / "diff.map"
         map_sig_thresh = 8  # Threshold in rmsd for difference map peaks/contours
         coot_script = [
             "#!/usr/bin/env coot",
             "# python script for coot - generated by metal_ID",
             "set_nomenclature_errors_on_read('ignore')",
-            f"read_pdb('{pdb}')",
+            f"read_pdb('{pdb_file}')",
             f"map_above = read_phs_and_make_map_using_cell_symm_from_previous_mol('{pha_above}')",
             f"map_below = read_phs_and_make_map_using_cell_symm_from_previous_mol('{pha_below}')",
             "map_diff = difference_map(map_above, map_below, 1)",
@@ -87,7 +185,7 @@ class MetalIdWrapper(Wrapper):
         with open(coot_script_path, "w") as script_file:
             for line in coot_script:
                 script_file.write(line + "\n")
-        self.log.info(f"Running coot script {coot_script_path}")
+        self.log.info(f"Running coot script {coot_script_path} to create diff.map")
         coot_command = f"coot --no-guano --no-graphics -s {coot_script_path}"
         result = subprocess.run(
             coot_command, shell=True, capture_output=True, text=True
@@ -123,7 +221,7 @@ class MetalIdWrapper(Wrapper):
         # Use regex to get the protein centre of mass coordinates from find-blobs output
         # N.B. Despite the name, find-blobs is not being used to find blobs here.
         self.log.info("Finding protein centre")
-        find_blobs_command = f"find-blobs -c {pdb}"
+        find_blobs_command = f"find-blobs -c {pdb_file}"
         result = subprocess.run(
             find_blobs_command, shell=True, capture_output=True, text=True
         )
@@ -138,7 +236,7 @@ class MetalIdWrapper(Wrapper):
             "#!/usr/bin/env coot",
             "# python script for coot - generated by metal_ID",
             "set_nomenclature_errors_on_read('ignore')",
-            f"read_pdb('{pdb}')",
+            f"read_pdb('{pdb_file}')",
             f"read_ccp4_map('{map_out}', 1)",
             f"set_contour_level_in_sigma(1, {map_sig_thresh})",
         ]

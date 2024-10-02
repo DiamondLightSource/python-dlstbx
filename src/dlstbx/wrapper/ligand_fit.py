968c9b6c083a1603a9066afdbb17fe599914a3d9
rowanwalker96 <rowan.walker-gibbons@diamond.ac.uk>
2024-10-02 10:08:54 +0100

    Add software commands to wrapper

--- src/dlstbx/wrapper/ligand_fit.py
+++ src/dlstbx/wrapper/ligand_fit.py
@@ -19,12 +19,45 @@ class LigandFitWrapper(Wrapper):
         self.log.debug(
             f"Running recipewrap file {self.recwrap.recipe_step['parameters']['recipewrapper']}"
         )
-        # Get parameters from the recipe file
-        params = self.recwrap.recipe_step["job_parameters"] #dictionary
-        pdb_file = params["pdb"]
 
-        phenix_command = f"phenix.ligandfit -xx "
-        result = subprocess.run(
-        phenix_command, shell=True, capture_output=True, text=True
-        )
+        # get params
+        params = self.recwrap.recipe_step["job_parameters"] #dictionary
+        print(params)
+
+        pdb_file = params["pdb_file"]
+        if not pdb_file:
+            self.log.error(
+                "Aborting ligand fit processing. PDB file not provided."
+            )
+            return False
+
+        #pdb_file = params["pdb_file"]
+        mtz_file = params["mtz_file"]
+        ligand_code = params["ligand_code"]
+        smiles_string = params["smiles_string"] #sometimes does not work so well
+        working_directory = pathlib.Path(params["working_directory"])
+        pipeline = params["pipeline"]
+
+        pipelines = ['phenix', 'phenix_pipeline']
+        if pipeline not in pipelines:
+            self.log.error(
+                    "Aborting ligand fit processing. Pipeline not recognised"
+                )
+            return False
+
+        if pipeline == 'phenix':
+            phenix_command = f"phenix.ligandfit data={mtz_file}  model={pdb_file} ligand_smiles={smiles_string}" #ligand={ligand_code}
+            result = subprocess.run(
+            phenix_command, shell=True, capture_output=True, text=True, cwd=working_directory
+            )
+
+        elif pipeline == 'phenix_pipeline':
+            phenix_command = f"phenix.ligand_pipeline {pdb_file} {mtz_file} ligand_code={ligand_code}"
+            result = subprocess.run(
+            phenix_command, shell=True, capture_output=True, text=True, cwd=working_directory
+            )
+
+        with open(working_directory / "ligand_fit.log", "w") as log_file:
+            log_file.write(result.stdout)
+
         return True

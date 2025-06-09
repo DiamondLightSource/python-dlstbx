736b4c61506a6908d1004d4ed940ab3ccf231675
Irakli Sikharulidze <irakli.sikharulidze@diamond.ac.uk>
2025-06-09 11:44:35 +0100

    Disable parallel MinIO upload to S3 Echo

--- src/dlstbx/util/iris.py
+++ src/dlstbx/util/iris.py
@@ -204,7 +204,7 @@ def get_presigned_urls(minio_client, bucket_name, pid, files, logger):
                 filename,
                 filepath,
                 part_size=20 * 1024 * 1024,
-                num_parallel_uploads=5,
+                num_parallel_uploads=1,
             )
             timestamp = time.perf_counter() - timestamp
             logger.info(

7b542dc7dc2419dde990f641d747e952a5fe3d97
Irakli Sikharulidze <irakli.sikharulidze@diamond.ac.uk>
2025-08-06 04:58:58 +0100

    Increase number of parallel MinIO uploads to 2

--- src/dlstbx/util/iris.py
+++ src/dlstbx/util/iris.py
@@ -210,7 +210,7 @@ def get_presigned_urls(minio_client, bucket_name, pid, files, logger):
                 filename,
                 filepath,
                 part_size=20 * 1024 * 1024,
-                num_parallel_uploads=1,
+                num_parallel_uploads=2,
             )
             timestamp = time.perf_counter() - timestamp
             logger.info(

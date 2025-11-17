410ba5ed5d82b1f1237cabe81f6de3f12a6905fe
rowanwalker96 <rowan.walker-gibbons@diamond.ac.uk>
2025-11-17 16:25:40 +0000

    Add rhofit ligandfit wrapper for pandda

---
+++ src/dlstbx/wrapper/pandda_rhofit.py
@@ -0,0 +1,220 @@
+from __future__ import annotations
+
+import json
+import os
+import subprocess
+from pathlib import Path
+
+import gemmi
+import numpy as np
+import yaml
+
+from dlstbx.wrapper import Wrapper
+
+
+class PanDDARhofitWrapper(Wrapper):
+    _logger_name = "dlstbx.wrap.pandda_rhofit"
+
+    def run(self):
+        assert hasattr(self, "recwrap"), "No recipewrapper object found"
+        self.log.info(
+            f"Running recipewrap file {self.recwrap.recipe_step['parameters']['recipewrapper']}"
+        )
+
+        EVENT_MAP_PATTERN = "{dtag}-event_{event_idx}_1-BDC_{bdc}_map.native.ccp4"
+        GROUND_STATE_PATTERN = "{dtag}-ground-state-average-map.native.ccp4"
+        PANDDA_2_DIR = "/dls_sw/i04-1/software/PanDDA2"
+
+        slurm_task_id = os.environ.get("SLURM_ARRAY_TASK_ID")
+        self.log.info((f"SLURM_ARRAY_TASK_ID: {slurm_task_id}"))
+
+        params = self.recwrap.recipe_step["job_parameters"]
+        processing_dir = Path(params.get("processing_directory"))
+        analysis_dir = processing_dir / "analysis"
+        auto_panddas_dir = analysis_dir / "auto_pandda2"
+
+        datasets = json.loads(params.get("datasets"))
+        dtag = datasets[int(slurm_task_id) - 1]
+        self.log.info(f"Processing dtag: {dtag}")
+        # -------------------------------------------------------
+
+        dataset_dir = auto_panddas_dir / dtag
+        event_yaml = dataset_dir / "events.yaml"
+
+        with open(event_yaml, "r") as file:
+            data = yaml.load(file, Loader=yaml.SafeLoader)
+
+        # Determine which builds to perform. More than one binder is unlikely and score ranks
+        # well so build the best scoring event of each dataset.
+        best_key = max(data, key=lambda k: data[k]["Score"])
+        best_entry = data[best_key]
+
+        event_idx = best_key
+        bdc = best_entry["BDC"]
+        coord = best_entry["Centroid"]
+        self.log.info(f"{dtag} : {event_idx}")
+
+        dataset_dir = auto_panddas_dir / "processed_datasets" / dtag
+        ligand_dir = dataset_dir / "ligand_files"
+        build_dmap = dataset_dir / f"{dtag}-z_map.native.ccp4"
+        restricted_build_dmap = dataset_dir / "build.ccp4"
+        pdb_file = dataset_dir / f"{dtag}-pandda-input.pdb"
+        restricted_pdb_file = dataset_dir / "build.pdb"
+
+        dmap_cut = 2.0
+        # This is usually quite a good contour for building and consistent
+        # (usually) with the cutoffs PanDDA 2 uses for event finding
+
+        # Rhofit can be confused by hunting non-binding site density. This can be avoided
+        # by truncating the map to near the binding site
+        dmap = self.read_pandda_map(build_dmap)
+        dmap = self.mask_map(dmap, coord)
+        self.save_xmap(dmap, restricted_build_dmap)
+
+        # Rhofit masks the protein before building. If the original protein
+        # model clips the event then this results in autobuilding becoming impossible.
+        # To address tis residues within a 10A neighbourhood of the binding event
+        # are removed.
+        print("# # Remove nearby atoms to make room for autobuilding")
+        self.remove_nearby_atoms(
+            pdb_file,
+            coord,
+            10.0,
+            restricted_pdb_file,
+        )
+
+        # Really all the cifs should be tried and the best used, or it should try the best
+        # cif from PanDDA
+        # This is a temporary fix that will get 90% of situations that can be improved upon
+        cifs = [x for x in ligand_dir.glob("*.cif")]
+        if len(cifs) == 0:
+            self.log.error("No .cif files found!")
+
+        # -------------------------------------------------------
+        # rhofit_command = f"module load buster; source /dls_sw/i04-1/software/PanDDA2/venv/bin/activate; \
+        # {pandda_2_dir}/scripts/pandda_rhofit.sh -pdb '$PDB' -map '$MAP' -mtz '$MTZ' -cif '$CIF' -out '$OUT' -cut {cut}; \
+        # cp "$MODELLED_STRUCTURES_DIR"/{dtag}-pandda-model.pdb "$MODELLED_STRUCTURES_DIR"/pandda-internal-fitted.pdb; \
+        # python -u {merge_script} --dataset_dir="$DATASET_DIR""
+
+        self.log.info("Running rhofit command: {rhofit_command}")
+
+        try:
+            result = subprocess.run(
+                rhofit_command,
+                shell=True,
+                capture_output=True,
+                text=True,
+                cwd=auto_panddas_dir,
+                check=True,
+                timeout=params.get("timeout-minutes") * 60,
+            )
+
+        except subprocess.CalledProcessError as e:
+            self.log.error(f"Rhofit command: '{rhofit_command}' failed")
+            self.log.info(e.stdout)
+            self.log.error(e.stderr)
+            return False
+
+        # -------------------------------------------------------
+
+        self.log.info("Auto PanDDA2-Rhofit finished successfully")
+        return True
+
+    def save_xmap(xmap, xmap_file):
+        """Convenience script for saving ccp4 files."""
+        ccp4 = gemmi.Ccp4Map()
+        ccp4.grid = xmap
+        ccp4.update_ccp4_header()
+        ccp4.write_ccp4_map(str(xmap_file))
+
+    def read_pandda_map(xmap_file):
+        """PanDDA 2 maps are often truncated, and PanDDA 1 maps can have misasigned spacegroups.
+        This method handles both."""
+        dmap_ccp4 = gemmi.read_ccp4_map(str(xmap_file), setup=False)
+        dmap_ccp4.grid.spacegroup = gemmi.find_spacegroup_by_name("P1")
+        dmap_ccp4.setup(0.0)
+        dmap = dmap_ccp4.grid
+        return dmap
+
+    def expand_event_map(bdc, ground_state_file, xmap_file, coord, out_file):
+        """DEPRECATED. A method for recalculating event maps over the full cell."""
+        ground_state_ccp4 = gemmi.read_ccp4_map(str(ground_state_file), setup=False)
+        ground_state_ccp4.grid.spacegroup = gemmi.find_spacegroup_by_name("P1")
+        ground_state_ccp4.setup(0.0)
+        ground_state = ground_state_ccp4.grid
+
+        xmap_ccp4 = gemmi.read_ccp4_map(str(xmap_file), setup=False)
+        xmap_ccp4.grid.spacegroup = gemmi.find_spacegroup_by_name("P1")
+        xmap_ccp4.setup(0.0)
+        xmap = xmap_ccp4.grid
+
+        mask = gemmi.FloatGrid(xmap.nu, xmap.nv, xmap.nw)
+        mask.set_unit_cell(xmap.unit_cell)
+        mask.set_points_around(
+            gemmi.Position(coord[0], coord[1], coord[2]), radius=10.0, value=1.0
+        )
+
+        event_map = gemmi.FloatGrid(xmap.nu, xmap.nv, xmap.nw)
+        event_map.set_unit_cell(xmap.unit_cell)
+        event_map_array = np.array(event_map, copy=False)
+        event_map_array[:, :, :] = np.array(xmap)[:, :, :] - (
+            bdc * np.array(ground_state)[:, :, :]
+        )
+        event_map_array[:, :, :] = event_map_array[:, :, :] * np.array(mask)[:, :, :]
+
+        event_map_non_zero = event_map_array[event_map_array != 0.0]
+        cut = np.std(event_map_non_zero)
+
+        return cut
+
+    def mask_map(dmap, coord, radius=10.0):
+        """Simple routine to mask density to region around a specified point."""
+        mask = gemmi.FloatGrid(dmap.nu, dmap.nv, dmap.nw)
+        mask.set_unit_cell(dmap.unit_cell)
+        mask.set_points_around(
+            gemmi.Position(coord[0], coord[1], coord[2]), radius=radius, value=1.0
+        )
+
+        dmap_array = np.array(dmap, copy=False)
+        dmap_array[:, :, :] = dmap_array[:, :, :] * np.array(mask)[:, :, :]
+
+        return dmap
+
+    def remove_nearby_atoms(pdb_file, coord, radius, output_file):
+        """An inelegant method for removing residues near the event centroid and creating
+        a new, truncated pdb file. GEMMI doesn't have a super nice way to remove
+        residues according to a specific criteria."""
+        st = gemmi.read_structure(str(pdb_file))
+        new_st = st.clone()  # Clone to keep metadata
+
+        coord_array = np.array([coord[0], coord[1], coord[2]])
+
+        # Delete all residues for a clean chain. Yes this is an arcane way to do it.
+        chains_to_delete = []
+        for model in st:
+            for chain in model:
+                chains_to_delete.append((model.name, chain.name))  # model.num
+
+        for model in new_st:
+            for chain in model:
+                for res in chain:
+                    del chain[-1]
+
+        # Add non-rejected residues to a new structure
+        for j, model in enumerate(st):
+            for k, chain in enumerate(model):
+                for res in chain:
+                    add_res = True
+                    for atom in res:
+                        pos = atom.pos
+                        distance = np.linalg.norm(
+                            coord_array - np.array([pos.x, pos.y, pos.z])
+                        )
+                        if distance < radius:
+                            add_res = False
+
+                    if add_res:
+                        new_st[j][k].add_residue(res)
+                    else:
+                        print(f"Dropped residue: {j} {k} {res.name}")
+        new_st.write_pdb(str(output_file))

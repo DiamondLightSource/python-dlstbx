ae25331c2f47423135c1c6986d21586cddf9524a
Paul Naughton <paul.naughton@diamond.ac.uk>
2026-05-13 16:06:34 +0100

    Using percentile pixel

--- src/dlstbx/wrapper/estimate_transmission.py
+++ src/dlstbx/wrapper/estimate_transmission.py
@@ -21,8 +21,8 @@ class EstimateTransmissionWrapper(Wrapper):
         results_directory = Path(params["results_directory"])
 
         beamline = params["beamline"]
-        trusted_countrate_pct = params["trusted_countrate_pct"].get(beamline, 100) / 100
-        target_countrate_pct = params["target_max_pixel_countrate_pct"].get(beamline, 50) / 100
+        pixel_percentile = params["pixel_percentile"].get(beamline, 100) / 100
+        target_countrate_pct = params["target_countrate_pct"].get(beamline, 50) / 100
         transmission = float(params["transmission"])
         file = params["input_file"]
 
@@ -45,19 +45,35 @@ class EstimateTransmissionWrapper(Wrapper):
                 self.log.debug(f"From command: {script}")
                 return False
 
-        experiment_file = results_directory / "imported.expt"
+        experiment_file = working_directory / "imported.expt"
         with experiment_file.open("r") as f:
             experiment = json.load(f)
             trusted_range = experiment["detector"][0]["panels"][0]["trusted_range"][1]
 
-        reflection_file = results_directory / "strong.refl"
+        reflection_file = working_directory / "strong.refl"
         reflections = flex.reflection_table.from_file(reflection_file)
         counts_hist = self.build_hist_from_reflections(reflections)
-        self.save_hist_to_json(counts_hist, trusted_range, results_directory)
+
+        num_counts = list(counts_hist.keys())
+        num_pixels = list(counts_hist.values())
+
+        index_of_pixel_percentile = self.get_percentile_index(num_pixels, pixel_percentile)
+        counts_at_percentile = int(num_counts[index_of_pixel_percentile])
+        
+        pixel_countrate_pct = counts_at_percentile / trusted_range 
+        self.log.info(f"The countrate percentage of the {pixel_percentile}% most intense pixel is {pixel_countrate_pct}")
+        scale_factor =  target_countrate_pct / pixel_countrate_pct
+
+        scaled_transmission = min(1, (transmission * scale_factor) / 100)
+        self.log.info(f"Scaled transmission is : {scaled_transmission}")
+
+        self.recwrap.send_to(
+            "strategy",
+            {"parameters": {"scaled_transmission": float(scaled_transmission)}},
+        )
         
         results_directory.mkdir(parents=True, exist_ok=True)
         output_files = ["dials.find_spots.log"]
-
         for output_file in output_files:
             source_file = working_directory / output_file
             destination = results_directory / output_file
@@ -69,32 +85,7 @@ class EstimateTransmissionWrapper(Wrapper):
             self.log.info(f"Copying {str(source_file)} to {str(destination)}")
             shutil.copy(source_file, destination)
 
-
-        num_counts, num_spots = counts_hist
-        max_counts = num_counts[-1]  
-
-        max_pixel_countrate_pct = max_counts / trusted_range 
-        if max_pixel_countrate_pct < trusted_countrate_pct: 
-            self.log.info(f"Max pixel is less intense than the trusted value of {trusted_countrate_pct}. Pixel intensity is {max_pixel_countrate_pct}")
-            self.log.info(f"Recommended transmission will not be scaled")
-
-            self.recwrap.send_to(
-                "strategy",
-                {"parameters": {"scaled_transmission": transmission}},
-            )
-
-            self.log.info("Done.")
-            return True
-
-        scale_factor =  target_countrate_pct / max_pixel_countrate_pct
-
-        scaled_transmission = min(1, (transmission * scale_factor) / 100)
-        self.log.info(f"Scaled transmission is : {scaled_transmission}")
-
-        self.recwrap.send_to(
-            "strategy",
-            {"parameters": {"scaled_transmission": float(scaled_transmission)}},
-        )
+        self.save_hist_to_json(counts_hist, trusted_range, results_directory)
 
         self.log.info("Done.")
         return True
@@ -108,15 +99,22 @@ class EstimateTransmissionWrapper(Wrapper):
             counter.update(sbox.data.as_numpy_array().ravel())
 
         sorted_counter = sorted(counter.items())
-        return zip(*sorted_counter)
+        return {str(int(k)): v for k, v in sorted_counter}
+
+    def get_percentile_index(self, num_pixels, percentile):
+        threshold = sum(num_pixels) * percentile
+
+        for i, cum_sum in enumerate(accumulate(num_pixels)):
+            if cum_sum >= threshold:
+                return i
+
+        return len(num_pixels)
 
     def save_hist_to_json(self, hist, max_trusted_value, results_dir):
-        counts_pixel_data = dict(hist)
-
         results_path = results_dir / "overload.json"
-        self.log.info("Saving counts histogram to", results_path)
+        self.log.info(f"Saving counts histogram to {str(results_path)}")
         with open(results_path, 'w') as f:
-            json.dump({ "counts": counts_pixel_data,
-                        "overload_limit": max_trusted_value}, f)
+            json.dump({ "counts": hist,
+                        "overload_limit": max_trusted_value}, f, indent=2)
         
         self.log.info("Saved.")

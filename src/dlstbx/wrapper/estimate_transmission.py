31b77d803228e04a3b72ec183785d5446d0f13c7
Paul Naughton <paul.naughton@diamond.ac.uk>
2026-05-20 14:30:11 +0100

    Add estimate transmission wrapper (#366)
    Add a wrapper to estimate the transmission needed to achieve pixel count-rates that can be accurately measured by the detector.
    
    Dectris detectors apply a count-rate coorection to convert from the measured count-rate to the estimated true count-rate at the pixel, to account for dead time in the detector. The detector output lists a trusted range, which is the count-rate to which count-rate corrections are applied by the detector. Tests on I04 indicated that the quality of these count-rate corrections deteriorate at >70% of the reported trusted range.
    
    This wrapper imports diffraction data and finds spots using dials commands, then constructs a histogram of the pixels that comprise the strong reflections and compares the counts to the trusted range of the detector. A percentile of pixels can be set, along with a target percentage of the trusted range and the wrapper will determine the transmission requred to achieve the target count-rate for the specified percentile via simple linear scaling.
    
    The intended use for this wrapper is to read in screening data as part of a strategy pipeline to advise the transmission that should be used for subsequent full collections on the sample.

---
+++ src/dlstbx/wrapper/estimate_transmission.py
@@ -0,0 +1,191 @@
+from __future__ import annotations
+
+import json
+import shutil
+import subprocess
+from collections import Counter
+from itertools import accumulate
+from pathlib import Path
+
+import matplotlib.pyplot as plt
+from dials.array_family import flex
+
+from dlstbx.wrapper import Wrapper
+
+
+class EstimateTransmissionWrapper(Wrapper):
+    """Wrapper to estimate the transmission to be used in a UDC data collection.
+    It uses the dials.import and dials.find_spots to collect pixel intensities
+    and scale them to the target count rate."""
+
+    _logger_name = "dlstbx.wrap.estimate_transmission"
+
+    def run(self):
+        """Entrypoint for the estimate_transmission wrapper.
+
+        :param beamline: The beamline to use for the scaling parameters.
+        :type beamline: str
+        :param pixel_percentile: The percentile of pixel intensities to use for scaling, for each beamline.
+        :type pixel_percentile: dict
+        :param target_countrate_pct: The target count rate percentage to scale the pixel_percentile to, for each beamline.
+        :type target_countrate_pct: dict
+        :param transmission: The transmission value used in the screening.
+        :type transmission: float
+        :param input_file: The path to the input file.
+        :type input_file: str
+        """
+        assert hasattr(self, "recwrap"), "No recipewrapper object found"
+
+        params = self.recwrap.recipe_step["job_parameters"]
+        working_directory = Path(params["working_directory"])
+        results_directory = Path(params["results_directory"])
+
+        beamline = params["beamline"]
+        pixel_percentile = params["pixel_percentile"][beamline] / 100
+        target_countrate_pct = params["target_countrate_pct"][beamline] / 100
+        transmission = float(params["transmission"])
+        file = params["input_file"]
+
+        commands = [
+            ("dials.import", ["dials.import", file]),
+            (
+                "dials.find_spots",
+                ["dials.find_spots", "imported.expt", "ice_rings.filter=True"],
+            ),
+        ]
+
+        for command, script in commands:
+            result = subprocess.run(script, cwd=working_directory, check=True)
+
+            if result.returncode:
+                self.log.info(f"{command} failed with return code {result.returncode}")
+                self.log.info(result.stderr)
+
+                self.log.debug(f"Command output:\n{result.stdout}")
+                self.log.debug(f"From command: {script}")
+                return False
+
+        experiment_file = working_directory / "imported.expt"
+        with experiment_file.open("r") as f:
+            experiment = json.load(f)
+            trusted_range = experiment["detector"][0]["panels"][0]["trusted_range"][1]
+
+        reflection_file = working_directory / "strong.refl"
+        reflections = flex.reflection_table.from_file(reflection_file)
+        counts_hist = self.collect_counts_from_reflections(reflections)
+
+        num_counts = list(counts_hist.keys())
+        num_pixels = list(counts_hist.values())
+
+        index_of_pixel_percentile = self.get_percentile_index(
+            num_pixels, pixel_percentile
+        )
+        counts_at_percentile = int(num_counts[index_of_pixel_percentile])
+        pixel_countrate_pct = counts_at_percentile / trusted_range
+        self.log.info(
+            f"The countrate percentage of the {pixel_percentile * 100}% most intense pixel is {pixel_countrate_pct * 100}% of the trusted value"
+        )
+        scale_factor = target_countrate_pct / pixel_countrate_pct
+
+        scaled_transmission = min(1, (transmission * scale_factor) / 100)
+        self.log.info(f"Scaled transmission is : {scaled_transmission}")
+
+        self.recwrap.send_to(
+            "strategy",
+            {"parameters": {"scaled_transmission": float(scaled_transmission)}},
+        )
+
+        max_pixel_count_pct = num_counts[-1] / trusted_range
+        if max_pixel_count_pct < 0.7:
+            warning_level = 0
+        elif max_pixel_count_pct < 0.85:
+            warning_level = 1
+        else:
+            warning_level = 2
+
+        warning_message = {
+            0: "Diffraction spots are unlikely to have detector count rate issues",
+            1: "Some diffraction spots may have detector count rate issues",
+            2: "Some diffraction spots are likely to have detector count rate issues",
+        }.get(warning_level)
+
+        warning_description = f"The most intense pixel is {max_pixel_count_pct * 100}% of the detector's limit"
+        warning_severity = ({0: "INFO", 1: "WARNING", 2: "ERROR"}.get(warning_level),)
+
+        ispyb_command_list = [
+            {
+                "ispyb_command": "add_program_message",
+                "program_id": "$ispyb_autoprocprogram_id",
+                "message": warning_message,
+                "description": warning_description,
+                "severity": warning_severity,
+            }
+        ]
+
+        self.log.info("Sending %s", str(ispyb_command_list))
+        self.recwrap.send_to("ispyb", {"ispyb_command_list": ispyb_command_list})
+
+        results_directory.mkdir(parents=True, exist_ok=True)
+        output_file = "dials.find_spots.log"
+        source_file = working_directory / output_file
+        destination = results_directory / output_file
+
+        if not source_file.exists():
+            self.log.info(f"{source_file=} does not exsist")
+            return False
+
+        self.log.info(f"Copying {str(source_file)} to {str(destination)}")
+        shutil.copy(source_file, destination)
+
+        self.save_plot(num_counts, num_pixels, results_directory)
+        self.save_hist_to_json(counts_hist, trusted_range, results_directory)
+
+        self.log.info("Done.")
+        return True
+
+    def collect_counts_from_reflections(self, reflections):
+        "Iterate through the shoeboxes to a reflection and generate a pixel histogram"
+
+        shoeboxes = reflections["shoebox"]
+        counter = Counter()
+        for sbox in shoeboxes:
+            counter.update(sbox.data.as_numpy_array().ravel())
+
+        sorted_counter = sorted(counter.items())
+        return {str(int(k)): v for k, v in sorted_counter}
+
+    def get_percentile_index(self, num_pixels, percentile):
+        threshold = sum(num_pixels) * percentile
+
+        for i, cum_sum in enumerate(accumulate(num_pixels)):
+            if cum_sum >= threshold:
+                return i
+
+        return len(num_pixels)
+
+    def save_hist_to_json(self, hist, max_trusted_value, results_dir):
+        results_path = results_dir / "pixel_counts.json"
+        self.log.info(f"Saving counts histogram to {str(results_path)}")
+        with open(results_path, "w") as f:
+            json.dump(
+                {"counts": hist, "overload_limit": max_trusted_value}, f, indent=2
+            )
+
+        self.log.info("Saved.")
+
+    def save_plot(self, counts, pixels, dir):
+        """Save the plot as png"""
+
+        self.log.info("Plotting pixel intensities...")
+
+        xlabel = "Num counts"
+        ylabel = "Counts"
+
+        fig = plt.subplot()
+        fig.scatter(counts, pixels)
+        fig.set_xlabel(xlabel)
+        fig.set_ylabel(ylabel)
+        fig.set_xscale("log")
+        fig.set_yscale("log")
+
+        plt.savefig(dir / "pixel_intensities.png")

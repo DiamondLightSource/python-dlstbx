689266e990d0c7bdbedc55ca6e633061bcab26f0
Nicholas Devenish <ndevenish@gmail.com>
2026-09-16 15:55:36 +0100

    Cluster: Centralise writing of recipe files

--- src/dlstbx/services/cluster.py
+++ src/dlstbx/services/cluster.py
@@ -178,15 +178,32 @@ class DLSCluster(CommonService):
             log_extender=self.extend_log,
         )
 
-    @staticmethod
-    def _recursive_mkdir(path):
+    def _write_job_file(
+        self, header, path: str, description: str, content: str
+    ) -> bool:
+        """Write a file needed by the cluster job, creating its directory.
+
+        Returns False if the message was rejected and processing should stop."""
+        self.log.debug("Writing %s to %s", description, path)
         try:
-            os.makedirs(path)
-        except OSError as exc:
-            if exc.errno == errno.EEXIST and os.path.isdir(path):
-                pass
-            else:
+            os.makedirs(os.path.dirname(path), exist_ok=True)
+            with open(path, "w") as fh:
+                fh.write(content)
+        except PermissionError as e:
+            # Attempted to write somewhere we don't have permissions
+            self.log.error(f"Could not write {description}: {e}", exc_info=True)
+            self.transport.nack(header, requeue=False)
+            return False
+        except OSError as e:
+            if e.errno != errno.ENOENT:
                 raise
+            self.log.error(
+                f"Error in underlying filesystem writing {description}: {e}",
+                exc_info=True,
+            )
+            self.transport.nack(header)
+            return False
+        return True
 
     def run_submit_job(self, rw, header, message):
         """Submit cluster job according to message."""
@@ -217,112 +234,42 @@ class DLSCluster(CommonService):
 
         if "recipefile" in parameters:
             recipefile = parameters["recipefile"]
-            try:
-                self._recursive_mkdir(os.path.dirname(recipefile))
-            except PermissionError as e:
-                # Attempted to create a recipefile directory somewhere we don't have permissions
-                self.log.error(
-                    f"Could not create recipe file directory: {e}", exc_info=True
-                )
-                self.transport.nack(header, requeue=False)
-                return
-            except OSError as e:
-                if e.errno == errno.ENOENT:
-                    self.log.error(
-                        "Error in underlying filesystem: %s", str(e), exc_info=True
-                    )
-                    self.transport.nack(header)
-                    return
-                raise
-            self.log.debug("Writing recipe to %s", recipefile)
             params.commands = params.commands.replace("$RECIPEFILE", recipefile)
-            try:
-                with open(recipefile, "w") as fh:
-                    fh.write(rw.recipe.pretty())
-            except PermissionError as e:
-                # Attempted to write a recipefile somewhere we don't have permissions
-                self.log.error(f"Could not write recipe file: {e}", exc_info=True)
-                self.transport.nack(header, requeue=False)
+            if not self._write_job_file(
+                header, recipefile, "recipe file", rw.recipe.pretty()
+            ):
                 return
         if "recipeenvironment" in parameters:
             recipeenvironment = parameters["recipeenvironment"]
-            try:
-                self._recursive_mkdir(os.path.dirname(recipeenvironment))
-            except PermissionError as e:
-                # Attempted to create a recipeenvironment directory somewhere we don't have permissions
-                self.log.error(
-                    f"Could not create recipe environment directory: {e}", exc_info=True
-                )
-                self.transport.nack(header, requeue=False)
-                return
-            except OSError as e:
-                if e.errno == errno.ENOENT:
-                    self.log.error(
-                        "Error in underlying filesystem: %s", str(e), exc_info=True
-                    )
-                    self.transport.nack(header)
-                    return
-                raise
-            self.log.debug("Writing recipe environment to %s", recipeenvironment)
             params.commands = params.commands.replace("$RECIPEENV", recipeenvironment)
-            try:
-                with open(recipeenvironment, "w") as fh:
-                    json.dump(
-                        rw.environment,
-                        fh,
-                        sort_keys=True,
-                        indent=2,
-                        separators=(",", ": "),
-                    )
-            except PermissionError as e:
-                # Attempted to write a recipeenvironment somewhere we don't have permissions
-                self.log.error(
-                    f"Could not write recipe environment: {e}", exc_info=True
-                )
-                self.transport.nack(header, requeue=False)
+            if not self._write_job_file(
+                header,
+                recipeenvironment,
+                "recipe environment",
+                json.dumps(
+                    rw.environment, sort_keys=True, indent=2, separators=(",", ": ")
+                ),
+            ):
                 return
         if "recipewrapper" in parameters:
             recipewrapper = parameters["recipewrapper"]
-            try:
-                self._recursive_mkdir(os.path.dirname(recipewrapper))
-            except PermissionError as e:
-                # Attempted to create a recipewrapper directory somewhere we don't have permissions
-                self.log.error(
-                    f"Could not create recipe wrapper directory: {e}", exc_info=True
-                )
-                self.transport.nack(header, requeue=False)
-                return
-            except OSError as e:
-                if e.errno == errno.ENOENT:
-                    self.log.error(
-                        "Error in underlying filesystem: %s", str(e), exc_info=True
-                    )
-                else:
-                    self.log.error(
-                        "Could not create working directory: %s", str(e), exc_info=True
-                    )
-                self.transport.nack(header)
-                return
-            self.log.debug("Storing serialized recipe wrapper in %s", recipewrapper)
             params.commands = params.commands.replace("$RECIPEWRAP", recipewrapper)
-            try:
-                with open(recipewrapper, "w") as fh:
-                    json.dump(
-                        {
-                            "recipe": rw.recipe.recipe,
-                            "recipe-pointer": rw.recipe_pointer,
-                            "environment": rw.environment,
-                            "recipe-path": rw.recipe_path,
-                            "payload": rw.payload,
-                        },
-                        fh,
-                        indent=2,
-                        separators=(",", ": "),
-                    )
-            except PermissionError as e:
-                # Attempted to write a recipewrapper somewhere we don't have permissions
-                self.log.error(f"Could not write recipe wrapper: {e}", exc_info=True)
-                self.transport.nack(header, requeue=False)
+            if not self._write_job_file(
+                header,
+                recipewrapper,
+                "recipe wrapper",
+                json.dumps(
+                    {
+                        "recipe": rw.recipe.recipe,
+                        "recipe-pointer": rw.recipe_pointer,
+                        "environment": rw.environment,
+                        "recipe-path": rw.recipe_path,
+                        "payload": rw.payload,
+                    },
+                    indent=2,
+                    separators=(",", ": "),
+                ),
+            ):
                 return
 
         if "workingdir" not in parameters or not parameters["workingdir"].startswith(
